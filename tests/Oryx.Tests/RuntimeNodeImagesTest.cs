ce9275aa9f3240075e04b9512d622b0c78edc40e
Elena Lakhno <elenla@microsoft.com>
2018-08-31 19:23:08 +0000

    Merged PR 139504: Node runtime images test
    Node runtime images test. This depends on https://devdiv.visualstudio.com/DevDiv/_git/Oryx/pullrequest/139008?_a=overview to build and tag runtime images locally and assumes runtime image name as node-<node_version>: latest.

---
+++ tests/Oryx.Tests/RuntimeNodeImagesTest.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using Oryx.Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Oryx.Tests
+{
+    public class RuntimeNodeImagesTest
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly DockerCli _dockerCli;
+
+        public RuntimeNodeImagesTest(ITestOutputHelper output)
+        {
+            _output = output;
+            _dockerCli = new DockerCli();
+        }
+
+        [Theory]
+        [InlineData("4.4.7")]
+        [InlineData("4.5.0")]
+        [InlineData("4.8.3")]
+        [InlineData("4.8.4")]
+        [InlineData("6.2.2")]
+        [InlineData("6.6.0")]
+        [InlineData("6.9.3")]
+        [InlineData("6.10.3")]
+        [InlineData("6.11.0")]
+        [InlineData("6.11.1")]
+        [InlineData("6.11.5")]
+        [InlineData("8.0.0")]
+        [InlineData("8.1.2")]
+        [InlineData("8.1.3")]
+        [InlineData("8.1.4")]
+        [InlineData("8.2.1")]
+        [InlineData("8.8.0")]
+        [InlineData("8.8.1")]
+        [InlineData("8.9.4")]
+        [InlineData("8.11.2")]
+        [InlineData("9.4.0")]
+        [InlineData("10.1.0")]
+        public void NodeVersionMatchesImageName(string nodeVersion)
+        {
+            // Arrange & Act
+            var expectedNodeVersion = "v" + nodeVersion;
+            var result = _dockerCli.Run(
+                "node-" + nodeVersion + ":latest",
+                commandToExecuteOnRun: "node",
+                commandArguments: new[] { "--version" });
+
+            // Assert
+            var actualOutput = result.ReplaceNewLine();
+            RunAsserts(
+                () =>
+                {
+                    Assert.True(result.IsSuccess);
+                    Assert.Equal(expectedNodeVersion, actualOutput);
+                },
+                result.GetDebugInfo());
+        }
+
+        private void RunAsserts(Action action, string message)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception)
+            {
+                _output.WriteLine(message);
+                throw;
+            }
+        }
+    }
+}
