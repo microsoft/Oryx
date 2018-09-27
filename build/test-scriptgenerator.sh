5298514918ad71cfe511dc8b57026c6832a9906c
Kiran Challa <kichalla@microsoft.com>
2018-09-27 17:53:30 +0000

    Merged PR 143392: Added BuildScriptGenerator.Tests to CI process
    Added BuildScriptGenerator.Tests to CI process
    
    Related work items: #690612

--- build/test-scriptgenerator.sh
+++ build/test-scriptgenerator.sh
@@ -2,12 +2,16 @@
 set -e
 
 declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
-declare -r testProjectName="BuildScriptGenerator.Tests"
 
 # Load all variables
 source $REPO_DIR/build/__variables.sh
 
 echo
 echo "Building and running tests..."
+testProjectName="BuildScriptGenerator.Tests"
+cd "$TESTS_SRC_DIR/$testProjectName"
+dotnet test --test-adapter-path:. --logger:"xunit;LogFilePath=$ARTIFACTS_DIR\testResults\\$testProjectName.xml" -c $BUILD_CONFIGURATION
+
+testProjectName="BuildScriptGeneratorCli.Tests"
 cd "$TESTS_SRC_DIR/$testProjectName"
 dotnet test --test-adapter-path:. --logger:"xunit;LogFilePath=$ARTIFACTS_DIR\testResults\\$testProjectName.xml" -c $BUILD_CONFIGURATION
