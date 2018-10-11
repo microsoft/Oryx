a7e295baccc1b21b73a1d4aaa1a8686e2874b964
Elena Lakhno <elenla@microsoft.com>
2018-10-11 19:27:48 +0000

    Merged PR 145778: Fixing node sample app
    Fixing node sample app
    
    Related work items: #703662

--- tests/SampleApps/nodejs/linx_node_express/server.js
+++ tests/SampleApps/nodejs/linx_node_express/server.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World from express!');
 });
 
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port 8080!');
+var port = process.env.PORT || 80;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
