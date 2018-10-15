11c566993beb2f8ee4ce19028b01bbfa4a86d642
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2018-10-15 17:54:30 +0000

    Merged PR 146036: Migrating  to MCR
    Also I have created supplementary build and release definitions
    
    Oryx-BuildImage-MCR
    Oryx-Runtime-MCR
    Oryx Release RuntimeImage-MCR
    Oryx Release BuildImage-MCR
    
    Related work items: #694685, #699925

---
+++ build/build-buildimagesMCR.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
+declare -r GIT_COMMIT=$(git rev-parse HEAD)
+
+# Load all variables
+source $REPO_DIR/build/__variablesMCR.sh
+
+cd "$BUILD_IMAGES_BUILD_CONTEXT_DIR"
+
+function BuildAndTagStage(){
+	local stageName="$1"
+	local stageTagName="oryxdevms/$1"
+
+	echo
+	echo
+	echo "Building stage '$stageName' with tag '$stageTagName' ..."
+	docker build --target $stageName -t $stageTagName -f "$BUILD_IMAGES_DOCKERFILE" .
+}
+
+# Tag stages to avoid creating dangling images.
+# NOTE:
+# These images are not written to artifacts file because they are not expected
+# to be pushed. This is just a workaround to prevent having dangling images so that
+# when a cleanup operation is being done on a build agent, a valuable dangling image
+# is not removed.
+BuildAndTagStage python-build-prereqs
+BuildAndTagStage python2.7.15-build
+BuildAndTagStage python3.5.6-build
+BuildAndTagStage python3.6.6-build
+BuildAndTagStage openssl1.1.1-build
+BuildAndTagStage python3.7.0-build
+BuildAndTagStage buildscriptbuilder
+
+tags="-t $DOCKER_BUILD_IMAGES_REPO:latest"
+labels="--label com.microsoft.oryx.git-commit=$GIT_COMMIT --label com.microsoft.oryx.build-number=$BUILD_NUMBER"
+
+if [ -n "$BUILD_NUMBER" ]
+then
+    tags="$tags -t $DOCKER_BUILD_IMAGES_REPO:$BUILD_NUMBER"
+fi
+
+if [ -n "$BUILD_BUILDIMAGES_USING_NOCACHE" ]
+then
+	echo
+	echo "Building build image(s) with NO cache..."
+	docker build --no-cache $tags $labels . -f "$BUILD_IMAGES_DOCKERFILE"
+else
+	echo
+	echo "Building build image(s)..."
+	docker build $tags $labels . -f "$BUILD_IMAGES_DOCKERFILE"
+fi
+
+# Write the list of images that were built to artifacts folder
+echo
+echo "Writing the list of build images built to artifacts folder..."
+mkdir -p "$ARTIFACTS_DIR/images"
+
+# Write image list to artifacts file
+echo "$DOCKER_BUILD_IMAGES_REPO:latest" > $BUILD_IMAGES_ARTIFACTS_FILE
+
+if [ -n "$BUILD_NUMBER" ]
+then
+	echo "$DOCKER_BUILD_IMAGES_REPO:$BUILD_NUMBER" >> $BUILD_IMAGES_ARTIFACTS_FILE
+fi
+
+echo
+echo "List of images built (from '$BUILD_IMAGES_ARTIFACTS_FILE'):"
+cat $BUILD_IMAGES_ARTIFACTS_FILE
+
+echo
+echo "Cleanup: Running 'docker system prune' ..."
+#docker system prune -f
---
+++ build/test-buildimagesMCR.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
+declare -r buildBuildImagesScript="$REPO_DIR/build/build-buildimagesMCR.sh"
+declare -r testProjectName="Oryx.BuildImage.Tests"
+
+# Load all variables
+source $REPO_DIR/build/__variablesMCR.sh
+
+if [ "$1" = "skipBuildingImages" ]
+then
+    echo
+    echo "Skipping building build images as argument '$1' was passed..."
+else
+    echo
+    echo "Invoking script '$buildBuildImagesScript'..."
+    $buildBuildImagesScript "$@"
+fi
+
+echo
+echo "Building and running tests..."
+cd "$TESTS_SRC_DIR/$testProjectName"
+dotnet test --test-adapter-path:. --logger:"xunit;LogFilePath=$ARTIFACTS_DIR\testResults\\$testProjectName.xml" -c $BUILD_CONFIGURATION
