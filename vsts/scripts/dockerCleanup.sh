c986a5a6b1b6ef15914762cc9d29dc11b02c6702
Kiran Challa <kichalla@microsoft.com>
2018-12-10 18:36:20 +0000

    Merged PR 155041: Fixed vsts docker artifact cleanup
    Fixed vsts docker artifact cleanup
    
    Related work items: #746408

--- vsts/scripts/dockerCleanup.sh
+++ vsts/scripts/dockerCleanup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+set -e
 
 declare -r BUILD_NUMBER="$BUILD_BUILDNUMBER"
 declare -r DOCKER_SYSTEM_PRUNE="${ORYX_DOCKER_SYSTEM_PRUNE:-true}"
 
+# The following code snippet is a hack to workaround the following issue:
+# Our CI agents run under a non-root account, but docker containers run under the 'root' account.
+# Since a non-root account cannot delete files created by a 'root' account, we try to run a
+# docker container and run the clean up command from there using the volume mounted directory trick.
+# 
+# This is required for end-to-end tests to work, where a host's directory is volume mounted to a build
+# image and the same host directory is volume mounted to a runtime image too.
+#
+# Even though Docker has a way to run a container as a non-root (for example, in our case the CI agent account),
+# this causes problems in build image where we try to install packages and stuff and we run into permission problems.
+# Since that is too intrusive and error prone, this hack allows us to not worry about those permission issues.
 mountedDirs="/tmp/OryxTestsMountedDirs"
 if [ -d "$mountedDirs" ]; then
 	echo
 	echo "Cleaning up files created by tests' docker containers ..."
-	docker run -v $mountedDirs:/tempDirs oryxdevms/build rm -rf /tempDirs/*
+	docker run -v $mountedDirs:/tempDirs oryxdevms/build /bin/bash -c "rm -rf /tempDirs/* && ls /tempDirs"
 fi
 
 echo
@@ -29,11 +41,6 @@ function UntagImages(){
 		docker rmi -f $imagesToUntag
 	fi
 }
-
-echo
-echo "Docker size:"
-echo
-docker system df
 
 echo
 echo "Current list of docker images:"
@@ -66,8 +73,3 @@ then
 	echo
 	docker images
 fi
-
-echo
-echo "Docker size:"
-echo
-docker system df
