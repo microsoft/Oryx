70335f058294e744c9478b6a40b3c46f50d87779
Dor Fire <dorfire@microsoft.com>
2019-01-09 23:25:46 +0000

    Merged PR 158465: Changed Dockerfile generation scripts to dynamically detect Debian/Alpine
    Related work items: #761173

--- images/runtime/node/generateDockerfiles.sh
+++ images/runtime/node/generateDockerfiles.sh
@@ -5,16 +5,29 @@ set -e
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 declare -r VERSIONS_FILE="$DIR/nodeVersions.txt"
 declare -r DOCKERFILE_TEMPLATE="$DIR/Dockerfile.template"
-declare -r NODE_VERSION_PLACEHOLDER="%NODE_BASE_IMAGE%"
+declare -r IMAGE_NAME_PLACEHOLDER="%NODE_BASE_IMAGE%"
+declare -r ALPINE_OR_STRETCH_PLACEHOLDER="%NODE_BASE_IMAGE_ALPINE_OR_STRETCH%"
 
-while IFS= read -r NODE_VERSION || [[ -n $NODE_VERSION ]]
+# Example line:
+# 8.11.4-alpine
+while IFS= read -r NODE_IMAGE_NAME || [[ -n $NODE_IMAGE_NAME ]]
 do
-    IFS='.' read -ra SPLIT_VERSION <<< "$NODE_VERSION"
-    VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
-    echo "Processing version $NODE_VERSION in directory $VERSION_DIRECTORY"
-    mkdir -p "$DIR/$VERSION_DIRECTORY/"
-    TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/Dockerfile"
-    cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
-    sed -i "s/$NODE_VERSION_PLACEHOLDER/$NODE_VERSION/g" "$TARGET_DOCKERFILE"
+	IFS='.' read -ra SPLIT_VERSION <<< "$NODE_IMAGE_NAME"
+	VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
+	echo "Generating Dockerfile for image '$NODE_IMAGE_NAME' in directory '$VERSION_DIRECTORY'..."
+
+	GO_IMAGE_TYPE="stretch"
+	# Figure out if the final image is Alpine based
+	if [[ $NODE_IMAGE_NAME == *"alpine"* ]]; then
+		GO_IMAGE_TYPE="alpine"
+	fi
+
+	mkdir -p "$DIR/$VERSION_DIRECTORY/"
+	TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/Dockerfile"
+	cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
+
+	# Replace placeholders
+	sed -i "s|$IMAGE_NAME_PLACEHOLDER|$NODE_IMAGE_NAME|g" "$TARGET_DOCKERFILE"
+	sed -i "s|$ALPINE_OR_STRETCH_PLACEHOLDER|$GO_IMAGE_TYPE|g" "$TARGET_DOCKERFILE"
 
 done < "$VERSIONS_FILE"
