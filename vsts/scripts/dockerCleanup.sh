b49c5f2b66cb4372a10318dc35ffc03a7469ca95
Kiran Challa <kichalla@microsoft.com>
2019-01-14 18:12:51 +0000

    Merged PR 159065: Untag ACR tags in CI to save disk space
    Untag ACR tags in CI to save disk space
    
    Related work items: #765993

--- vsts/scripts/dockerCleanup.sh
+++ vsts/scripts/dockerCleanup.sh
@@ -28,15 +28,24 @@ echo "Current list of docker images:"
 echo
 docker images
 
-# An image that is built in our pipelines is tagged with 'latest' and '<build number>'.
-# The following is to untag an image with the build number tag so that when the next time
+# An image that is built in our pipelines is tagged with 'latest' and 'build number'.
+# The following is to untag an image with the 'build number' tag so that when the next time
 # images are built, the older images can become dangled which can later be cleaned up.
 #
-# Note that we should untag these images only after they have been pushed to a remote repository.
+# **NOTE**
+# - We still keep the tags of the following pattern because we still need some cache so that next builds are faster
+#	a. oryxdevms/build:latest
+#	b. oryxdevms/python-<major.minor>:latest
+#	b. oryxdevms/node-<major.minor>:latest
+#	b. oryxdevms/dotnetcore-<major.minor>:latest
+# - We should untag these images only after they have been pushed to a remote repository.
 UntagImages "oryxdevms/*:*.*"
 UntagImages "oryxprod/*:latest"
 UntagImages "oryxprod/*:*.*"
-UntagImages "oryxprod/*:Release*"
+UntagImages "oryxdevmcr.azurecr.io/public/oryx/*:latest"
+UntagImages "oryxdevmcr.azurecr.io/public/oryx/*:*.*"
+UntagImages "oryxmcr.azurecr.io/public/oryx/*:latest"
+UntagImages "oryxmcr.azurecr.io/public/oryx/*:*.*"
 
 echo
 echo "Updated list of docker images:"
