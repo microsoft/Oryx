33fb2d05f828d7b1aa5936f2492119d4b095cb50
Dor Fire <dorfire@microsoft.com>
2019-01-24 18:23:10 +0000

    Merged PR 160717: Added pip output workaround
    Related work items: #763927

--- images/build/python-build.sh
+++ images/build/python-build.sh
@@ -20,15 +20,17 @@ done;
 gpg --batch --verify /python.tar.xz.asc /python.tar.xz
 tar -xJf /python.tar.xz --strip-components=1 -C .
 
+INSTALLATION_PREFIX=/opt/python/$PYTHON_VERSION
+
 if [ "${PYTHON_VERSION::1}" == "2" ]; then
     ./configure \
-        --prefix=/opt/python/$PYTHON_VERSION \
+        --prefix=$INSTALLATION_PREFIX \
         --build=$(dpkg-architecture --query DEB_BUILD_GNU_TYPE) \
         --enable-shared \
         --enable-unicode=ucs4
 else
     ./configure \
-        --prefix=/opt/python/$PYTHON_VERSION \
+        --prefix=$INSTALLATION_PREFIX \
         --build=$(dpkg-architecture --query DEB_BUILD_GNU_TYPE) \
         --enable-loadable-sqlite-extensions \
         --enable-shared \
@@ -41,20 +43,19 @@ make -j $(nproc)
 
 make install
 
-PYTHON_PATH=/opt/python/$PYTHON_VERSION
-
+# Install pip
 wget https://bootstrap.pypa.io/get-pip.py -O /get-pip.py
 LD_LIBRARY_PATH=/usr/src/python \
 /usr/src/python/python /get-pip.py \
-    --prefix $PYTHON_PATH \
+    --prefix $INSTALLATION_PREFIX \
     --disable-pip-version-check \
     --no-cache-dir \
     --no-warn-script-location \
     pip==$PIP_VERSION
 
 if [ "${PYTHON_VERSION::1}" == "2" ]; then
-    LD_LIBRARY_PATH=$PYTHON_PATH/lib \
-    $PYTHON_PATH/bin/pip install --no-cache-dir virtualenv
+    LD_LIBRARY_PATH=$INSTALLATION_PREFIX/lib \
+    $INSTALLATION_PREFIX/bin/pip install --no-cache-dir virtualenv
 fi
 
 # Currently only for version '2' of Python, the alias 'python' exists in the 'bin'
@@ -63,10 +64,14 @@ fi
 # expects the alias 'python' to point to '3.7' rather than '2'. In cases where benv is
 # not passed as an explicit python version, the version '2' is used by default. This is
 # done in the Dockerfile.
-pythonBinDir="$PYTHON_PATH/bin"
+pythonBinDir="$INSTALLATION_PREFIX/bin"
 pythonAliasFile="$pythonBinDir/python"
 if [ ! -e "$pythonAliasFile" ]; then
     IFS='.' read -ra SPLIT_VERSION <<< "$PYTHON_VERSION"
     majorAndMinorParts="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
     ln -s $pythonBinDir/python$majorAndMinorParts $pythonBinDir/python
 fi
+
+# Replace log level in pip's code as a workaround for https://github.com/pypa/pip/issues/6189
+pipReqSetPath=`find $INSTALLATION_PREFIX/lib -path "*site-packages/pip/_internal/req/req_set.py"`
+sed -i 's|logger\.debug('\''Cleaning up\.\.\.'\'')|logger\.info('\''Cleaning up\.\.\.'\'')|' "$pipReqSetPath"
