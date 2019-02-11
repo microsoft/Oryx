547ed6509680b5e295eaa6f900c193aa149469e1
Gabriel Pedro de Castro <gadecast@microsoft.com>
2019-02-11 22:39:12 +0000

    Merged PR 163916: Remove tailwindtraders app
    Remove tailwindtraders app
    This app doesn't add new scenario in particular, so I'm not adding a new app to replace it.
    
    Related work items: #786193

--- tests/SampleApps/nodejs/TailwindTraders-Opener/client/src/routes/checkout.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import CheckoutContainer from '../containers/checkout-container';
-
-ReactDOM.render(<CheckoutContainer />, document.getElementById('content'));
