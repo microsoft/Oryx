d9825b269016186d14c206ea2a932455be19869d
Dor Fire <dorfire@microsoft.com>
2019-02-14 18:00:03 -0800

    Added build image tester scaffold

---
+++ tests/Oryx.BuildImage.Tests/PhpSampleAppsTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Oryx.BuildImage.Tests
+{
+    class PhpSampleAppsTest : SampleAppsTestBase
+    {
+        public override void Builds_AndCopiesContentToOutputDirectory_Recursively()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Build_CopiesOutput_ToNestedOutputDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Build_ReplacesContentInDestinationDir_WhenDestinationDirIsNotEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CanBuild_UsingScriptGeneratedBy_ScriptOnlyOption()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ErrorDuringBuild_ResultsIn_NonSuccessfulExitCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GeneratesScriptAndBuilds_WhenDestination_IsSubDirectoryOfSource()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GeneratesScriptAndBuilds_WhenSourceAndDestinationFolders_AreSame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GeneratesScript_AndBuilds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GeneratesScript_AndBuilds_UsingSuppliedIntermediateDir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GeneratesScript_AndBuilds_WhenExplicitLanguageAndVersion_AreProvided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
