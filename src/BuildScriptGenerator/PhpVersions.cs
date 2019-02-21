453e3d63a835825edb0dbeb3bcc61c95d9a71aaa
Dor Fire <dorfire@microsoft.com>
2019-02-20 16:15:01 -0800

    Added PHP version info consts

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -5,6 +5,10 @@ namespace Microsoft.Oryx.Common
     public static class PhpVersions
     {
         public const string Php73Version = "7.3.2";
-        public const string Php7Version = "7.3.2";
+        public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
+        public const string Php73TarSha256 = "010b868b4456644ae227d05ad236c8b0a1f57dc6320e7e5ad75e86c5baf0a9a8";
+        public const string Php72Version = "7.2.15";
+        public const string Php72Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 B1B44D8F021E4E2D6021E995DC9FF8D3EE5AF27F";
+        public const string Php72TarSha256 = "75e90012faef700dffb29311f3d24fa25f1a5e0f70254a9b8d5c794e25e938ce";
     }
 }
