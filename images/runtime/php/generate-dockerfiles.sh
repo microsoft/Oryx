a2dff1939d126dedb239c1812ce415c71ff11b38
Dor Fire <dorfire@microsoft.com>
2019-03-07 10:47:38 -0800

    Removed separate image tag file

--- images/runtime/php/generate-dockerfiles.sh
+++ images/runtime/php/generate-dockerfiles.sh
@@ -7,13 +7,16 @@
 set -e
 
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-declare -r TAG_FILE="$DIR/image-tags.txt"
 declare -r DOCKERFILE_TEMPLATE="$DIR/Dockerfile.template"
 declare -r IMAGE_NAME_PLACEHOLDER="%PHP_BASE_IMAGE%"
+. $DIR/../../../build/__php-versions.sh
+declare -r VERSION_ARRAY=($PHP73_VERSION $PHP72_VERSION $PHP70_VERSION $PHP56_VERSION)
 
-while IFS= read -r PHP_IMAGE_NAME || [[ -n $PHP_IMAGE_NAME ]]
+for PHP_VERSION in "${VERSION_ARRAY[@]}"
 do
-	IFS='.' read -ra SPLIT_VERSION <<< "$PHP_IMAGE_NAME"
+	PHP_IMAGE_NAME="$PHP_VERSION-apache"
+
+	IFS='.' read -ra SPLIT_VERSION <<< "$PHP_VERSION"
 	VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
 	echo "Generating Dockerfile for image '$PHP_IMAGE_NAME' in directory '$VERSION_DIRECTORY'..."
 
@@ -23,4 +26,4 @@ do
 
 	# Replace placeholders
 	sed -i "s|$IMAGE_NAME_PLACEHOLDER|$PHP_IMAGE_NAME|g" "$TARGET_DOCKERFILE"
-done < "$TAG_FILE"
+done
