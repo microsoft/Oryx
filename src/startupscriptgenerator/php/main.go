425bc2e69a4771f9b68eff7c9b45f15f53b472f3
Dor Fire <dorfire@microsoft.com>
2019-03-11 15:37:35 -0700

    Fixed Go build error

--- src/startupscriptgenerator/php/main.go
+++ src/startupscriptgenerator/php/main.go
@@ -24,7 +24,7 @@ func main() {
 
 	entrypointGenerator := PhpStartupScriptGenerator{
 		SourcePath: fullAppPath,
-		StartupCmd: startupCmdPtr,
+		StartupCmd: *startupCmdPtr,
 	}
 
 	command := entrypointGenerator.GenerateEntrypointScript()
