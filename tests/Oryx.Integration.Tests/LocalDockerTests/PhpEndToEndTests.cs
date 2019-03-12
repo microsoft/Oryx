9b3bd7376d82b0cd27f7fb4a7a5b58928e2f79df
Dor Fire <dorfire@microsoft.com>
2019-03-12 10:48:29 -0700

    Added PHP E2E test

---
+++ tests/Oryx.Integration.Tests/LocalDockerTests/PhpEndToEndTests.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using Microsoft.Oryx.Common;
+using Microsoft.Oryx.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Oryx.Integration.Tests.LocalDockerTests
+{
+    public class PhpEndToEndTests
+    {
+        private const int HostPort = 8000;
+        private const string RunScriptPath = "/tmp/startup.sh";
+        private readonly ITestOutputHelper _output;
+        private readonly string _hostSamplesDir;
+        private readonly HttpClient _httpClient;
+
+        public PhpEndToEndTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _hostSamplesDir = Path.Combine(Directory.GetCurrentDirectory(), "SampleApps");
+            _httpClient = new HttpClient();
+        }
+
+        [Theory]
+        [InlineData(PhpVersions.Php73Version)]
+        [InlineData(PhpVersions.Php72Version)]
+        [InlineData(PhpVersions.Php70Version)]
+        // Twig does not support PHP < 7
+        public async Task TwigExample(string phpVersion)
+        {
+            // Arrange
+            phpVersion = RemovePatchVersion(phpVersion);
+            var appName = "twig-example";
+            var hostDir = Path.Combine(_hostSamplesDir, "php", appName);
+            var volume = DockerVolume.Create(hostDir);
+            var appDir = volume.ContainerDir;
+            var portMapping = $"{HostPort}:80";
+            var script = new ShellScriptBuilder()
+                .AddCommand($"cd {appDir}")
+                .AddCommand($"oryx -appPath {appDir} -output {RunScriptPath}")
+                .AddCommand(RunScriptPath)
+                .ToString();
+
+            await EndToEndTestHelper.BuildRunAndAssertAppAsync(
+                appName, _output, volume,
+                "oryx", new[] { "build", appDir, "-l", "php", "--language-version", phpVersion },
+                $"oryxdevms/php-{phpVersion}",
+                portMapping,
+                "/bin/sh", new[] { "-c", script },
+                async () =>
+                {
+                    var data = await _httpClient.GetStringAsync($"http://localhost:{HostPort}/");
+                    Assert.Contains("<h1>Hello World!</h1>", data);
+                });
+        }
+
+        private static string RemovePatchVersion(string fullVersion)
+        {
+            var lastDot = fullVersion.LastIndexOf('.');
+            return fullVersion.Substring(0, lastDot);
+        }
+    }
+}
