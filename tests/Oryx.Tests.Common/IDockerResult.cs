60a8f05672b56885da3b2db7f4e5902af94927c5
Dor Fire <dorfire@microsoft.com>
2019-03-13 12:38:20 -0700

    Introduced IDockerResult

---
+++ tests/Oryx.Tests.Common/IDockerResult.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Oryx.Tests.Common
+{
+    interface IDockerResult
+    {
+        Exception Exception { get; }
+
+        string ExecutedCommand { get; }
+
+        int ExitCode { get; }
+
+        string StdOut { get; }
+
+        string StdErr { get; }
+
+        string GetDebugInfo(IDictionary<string, string> extraDefs = null);
+    }
+}
