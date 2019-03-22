6c085d7943e8176345412e15109edcba5a90b1ec
Dor Fire <dorfire@microsoft.com>
2019-03-22 15:16:36 -0700

    Updated local dev script

--- build/build-buildimage-platforms.sh
+++ build/build-buildimage-platforms.sh
@@ -14,8 +14,8 @@ declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
 declare -r ACR_NAME='oryxdevmcr.azurecr.io'
 
 # Build Python
-docker build -f $REPO_DIR/images/build/python/prereqs/Dockerfile -t "$ACR_NAME/python-build-prereqs" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/2.7/Dockerfile -t "$ACR_NAME/python27-build" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.5/Dockerfile -t "$ACR_NAME/python35-build" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.6/Dockerfile -t "$ACR_NAME/python36-build" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.7/Dockerfile -t "$ACR_NAME/python37-build" $REPO_DIR
+docker build -f $REPO_DIR/images/build/python/prereqs/Dockerfile -t "python-build-prereqs" $REPO_DIR
+docker build -f $REPO_DIR/images/build/python/2.7/Dockerfile -t "mcr.microsoft.com/oryx/python-build-2.7" $REPO_DIR
+docker build -f $REPO_DIR/images/build/python/3.5/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.5" $REPO_DIR
+docker build -f $REPO_DIR/images/build/python/3.6/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.6" $REPO_DIR
+docker build -f $REPO_DIR/images/build/python/3.7/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.7" $REPO_DIR
