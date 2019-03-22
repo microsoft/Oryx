bc2e29ccbe88d0bcc55d9bcf02ffbe2950c6b86b
Dor Fire <dorfire@microsoft.com>
2019-03-22 15:27:37 -0700

    Prepared script for DevOps task

--- build/build-buildimage-bases.sh
+++ build/build-buildimage-bases.sh
@@ -12,9 +12,25 @@ set -e
 
 declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
 
+source $REPO_DIR/build/__variables.sh
+
 # Build Python
 docker build -f $REPO_DIR/images/build/python/prereqs/Dockerfile -t "python-build-prereqs" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/2.7/Dockerfile -t "mcr.microsoft.com/oryx/python-build-2.7" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.5/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.5" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.6/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.6" $REPO_DIR
-docker build -f $REPO_DIR/images/build/python/3.7/Dockerfile -t "mcr.microsoft.com/oryx/python-build-3.7" $REPO_DIR
+
+docker build -f $REPO_DIR/images/build/python/2.7/Dockerfile -t "$ACR_DEV_NAME/public/oryx/python-build-2.7:$BUILD_NUMBER" $REPO_DIR
+echo "$ACR_DEV_NAME/public/oryx/python-build-2.7:$BUILD_NUMBER" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
+
+docker build -f $REPO_DIR/images/build/python/3.5/Dockerfile -t "$ACR_DEV_NAME/public/oryx/python-build-3.5:$BUILD_NUMBER" $REPO_DIR
+echo "$ACR_DEV_NAME/public/oryx/python-build-3.5:$BUILD_NUMBER" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
+
+docker build -f $REPO_DIR/images/build/python/3.6/Dockerfile -t "$ACR_DEV_NAME/public/oryx/python-build-3.6:$BUILD_NUMBER" $REPO_DIR
+echo "$ACR_DEV_NAME/public/oryx/python-build-3.6:$BUILD_NUMBER" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
+
+docker build -f $REPO_DIR/images/build/python/3.7/Dockerfile -t "$ACR_DEV_NAME/public/oryx/python-build-3.7:$BUILD_NUMBER" $REPO_DIR
+echo "$ACR_DEV_NAME/public/oryx/python-build-3.7:$BUILD_NUMBER" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
+
+
+echo
+echo "List of images built (from '$RUNTIME_IMAGES_ARTIFACTS_FILE'):"
+cat $BUILD_IMAGE_BASES_ARTIFACTS_FILE
+echo
