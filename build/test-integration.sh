6a4c684dfce438177a657c902af84cd6eeda6387
Dor Fire <dorfire@microsoft.com>
2019-03-26 15:35:08 -0700

    Removed unnecessary pulls

--- build/test-integration.sh
+++ build/test-integration.sh
@@ -6,20 +6,11 @@
 
 set -e
 
-declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
+source __variables.sh
 
 if [ -z "$STORAGEACCOUNTKEY" ]; then # If STORAGEACCOUNTKEY is unset or empty
 	export STORAGEACCOUNTKEY=`az.cmd storage account keys list -n oryxautomation | grep key1 | awk '{print $NF}'`
 fi
-
-# Load all variables
-source $REPO_DIR/build/__variables.sh
-
-echo
-echo Pulling docker images required for running tests against databases ...
-docker pull mysql/mysql-server:5.7
-docker pull postgres:9.5
-docker pull microsoft/mssql-server-linux:2017-CU12
 
 echo
 echo "Building and running tests..."
