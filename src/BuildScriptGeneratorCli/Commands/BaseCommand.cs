959c7aef867da43970d3285aca1ab9d326b2eee8
Dor Fire <dorfire@microsoft.com>
2019-03-28 10:12:09 -0700

    Added cmdline log

--- src/BuildScriptGeneratorCli/Commands/BaseCommand.cs
+++ src/BuildScriptGeneratorCli/Commands/BaseCommand.cs
@@ -33,6 +33,7 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
             try
             {
                 _serviceProvider = GetServiceProvider();
+                _serviceProvider?.GetRequiredService<ILogger<BaseCommand>>()?.LogInformation("Oryx command line: {cmdLine}", Environment.CommandLine);
                 if (!IsValidInput(_serviceProvider, console))
                 {
                     return ProcessConstants.ExitFailure;
