ebc253668d19331f58a7eec57732eb682a6d5938
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-03-28 12:53:16 -0700

    initial experiment on parallel builds

--- build/build-runtimeimages.sh
+++ build/build-runtimeimages.sh
@@ -79,7 +79,7 @@ for dockerFile in $dockerFiles; do
 
     if [ -n "$BUILD_NUMBER" ]
     then
-        tags="$tags -t $runtimeImageTagName:$BUILD_NUMBER"
+        tags="$tags -t $runtimeImageTagName:$BUILD_DEFINITIONNAME.$BUILD_NUMBER"
     fi
     
     echo
@@ -101,7 +101,7 @@ for dockerFile in $dockerFiles; do
 
     if [ -n "$BUILD_NUMBER" ]
     then
-        docker tag "$runtimeImageTagName:latest" "$runtimeImageACRTagName:$BUILD_NUMBER"
+        docker tag "$runtimeImageTagName:latest" "$runtimeImageACRTagName:$BUILD_DEFINITIONNAME.$BUILD_NUMBER"
     fi
 
     if [ $clearedOutput = "false" ]
@@ -115,12 +115,13 @@ for dockerFile in $dockerFiles; do
     # add new content
     echo
     echo "Updating artifacts file with the built runtime image information..."
-    echo "$runtimeImageTagName:latest" >> $RUNTIME_IMAGES_ARTIFACTS_FILE
-    echo "$runtimeImageACRTagName:latest" >> $ACR_RUNTIME_IMAGES_ARTIFACTS_FILE
+	echo "$runtimeImageTagName:latest" >> $RUNTIME_IMAGES_ARTIFACTS_FILE
+	echo "$runtimeImageACRTagName:latest" >> $ACR_RUNTIME_IMAGES_ARTIFACTS_FILE
+
     if [ -n "$BUILD_NUMBER" ]
     then
-    	echo "$runtimeImageTagName:$BUILD_NUMBER" >> $RUNTIME_IMAGES_ARTIFACTS_FILE
-        echo "$runtimeImageACRTagName:$BUILD_NUMBER" >> $ACR_RUNTIME_IMAGES_ARTIFACTS_FILE
+    	echo "$runtimeImageTagName:$BUILD_DEFINITIONNAME.$BUILD_NUMBER" >> $RUNTIME_IMAGES_ARTIFACTS_FILE
+        echo "$runtimeImageACRTagName:$BUILD_DEFINITIONNAME.$BUILD_NUMBER" >> $ACR_RUNTIME_IMAGES_ARTIFACTS_FILE
     fi
 
     cd $RUNTIME_IMAGES_SRC_DIR
