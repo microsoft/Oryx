e1bf2002db607c871f38014e2f9734ba7e018b48
Gabriel Castro <16232766+gpcastro@users.noreply.github.com>
2019-04-02 15:24:25 -0700

    Remove Python 3.5 (#40)

--- tests/BuildScriptGenerator.Tests/Settings.cs
+++ tests/BuildScriptGenerator.Tests/Settings.cs
@@ -8,7 +8,6 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Tests
     internal class Settings
     {
         public const string Python27Version = "2.7.16";
-        public const string Python35Version = "3.5.6";
         public const string Python36Version = "3.6.8";
     }
 }
