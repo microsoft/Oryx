2ae10dfa3d9f965675e11af2af944a06acbb72bb
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-04-02 15:00:14 -0700

    implementing parallel builds for validation and nightly builds
    implementing parallel builds for validation and nightly  builds for oryx

--- vsts/scripts/dockerCleanup.sh
+++ vsts/scripts/dockerCleanup.sh
@@ -10,7 +10,7 @@ declare -r DOCKER_SYSTEM_PRUNE="${ORYX_DOCKER_SYSTEM_PRUNE:-false}"
 echo
 echo "Stop running containers:"
 echo
-docker ps -q | docker stop
+docker stop $(docker ps -a -q)
 
 function UntagImages() {
 	local imagePattern=$1
@@ -61,28 +61,10 @@ echo
 echo "Cleanup: Run 'docker system prune': $DOCKER_SYSTEM_PRUNE"
 if [ "$DOCKER_SYSTEM_PRUNE" == "true" ]
 then
-	docker system prune -f
+    docker system prune -f
 
-	echo
-	echo "Updated list of docker images:"
-	echo
-	docker images
-fi
-
-# The following code snippet is a hack to workaround the following issue:
-# Our CI agents run under a non-root account, but docker containers run under the 'root' account.
-# Since a non-root account cannot delete files created by a 'root' account, we try to run a
-# docker container and run the clean up command from there using the volume mounted directory trick.
-# 
-# This is required for end-to-end tests to work, where a host's directory is volume mounted to a build
-# image and the same host directory is volume mounted to a runtime image too.
-#
-# Even though Docker has a way to run a container as a non-root (for example, in our case the CI agent account),
-# this causes problems in build image where we try to install packages and stuff and we run into permission problems.
-# Since that is too intrusive and error prone, this hack allows us to not worry about those permission issues.
-mountedDirs="/tmp/OryxTestsMountedDirs"
-if [ -d "$mountedDirs" ]; then
-	echo
-	echo "Cleaning up files created by tests' docker containers ..."
-	docker run -v $mountedDirs:/tempDirs oryxdevms/build /bin/bash -c "rm -rf /tempDirs/* && ls /tempDirs"
+    echo
+    echo "Updated list of docker images:"
+    echo
+    docker images
 fi
