67fa1e3b2e78863d631aca5cac3ffccba6a43aca
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-04-02 12:26:48 -0700

    add sudo to remove command

--- vsts/scripts/dockerArtifactsRemoval.sh
+++ vsts/scripts/dockerArtifactsRemoval.sh
@@ -8,6 +8,7 @@ declare -r BUILD_NUMBER="$BUILD_BUILDNUMBER"
 declare -r DOCKER_SYSTEM_PRUNE="${ORYX_DOCKER_SYSTEM_PRUNE:-true}"
 
 mountedDirs="/tmp/OryxTestsMountedDirs"
+whoami
 docker ps -a -q
 docker volume ls
 
@@ -16,7 +17,7 @@ echo "Mounted Directory: "
 echo
 
 if [ -d "$mountedDirs" ]; then
-	echo
-	echo "Cleaning up files created by tests' docker containers ..."
-	rm -rf $mountedDirs && ls /tmp
+    echo
+    echo "Cleaning up files created by tests' docker containers ..."
+    sudo rm -rf $mountedDirs && ls /tmp
 fi
