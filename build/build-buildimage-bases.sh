13a28fc3d0f69aaab68bd5608e9ad86f1613349d
Dor Fire <dorfire@microsoft.com>
2019-04-04 13:38:37 -0700

    Added informational messages

--- build/build-buildimage-bases.sh
+++ build/build-buildimage-bases.sh
@@ -23,7 +23,9 @@ mkdir -p `dirname $BUILD_IMAGE_BASES_ARTIFACTS_FILE`
 
 case $1 in
 	'python')
-		# Build Python
+		echo "Building Python base images"
+		echo
+
 		docker build -f $BUILD_IMAGE_PREFIX/python/prereqs/Dockerfile -t "python-build-prereqs" $__REPO_DIR
 
 		declare -r PYTHON_IMAGE_PREFIX="$ACR_DEV_NAME/public/oryx/python-build"
@@ -41,7 +43,9 @@ case $1 in
 		echo "$PYTHON_IMAGE_PREFIX-3.7:$IMAGE_TAG" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
 		;;
 	'php')
-		# Build PHP
+		echo "Building PHP base images"
+		echo
+
 		docker build -f $BUILD_IMAGE_PREFIX/php/prereqs/Dockerfile -t "php-build-prereqs" $__REPO_DIR
 
 		declare -r PHP_IMAGE_PREFIX="$ACR_DEV_NAME/public/oryx/php-build"
@@ -59,7 +63,9 @@ case $1 in
 		echo "$PHP_IMAGE_PREFIX-7.3:$BUILD_NUMBER" >> $BUILD_IMAGE_BASES_ARTIFACTS_FILE
 		;;            
 	'yarn-cache')
-		# Build Yarn cache
+		echo "Building Yarn package cache base image"
+		echo
+
 		YARN_CACHE_IMAGE_BASE="$ACR_DEV_NAME/public/oryx/build-yarn-cache"
 		YARN_CACHE_IMAGE_NAME=$YARN_CACHE_IMAGE_BASE:$IMAGE_TAG
 
