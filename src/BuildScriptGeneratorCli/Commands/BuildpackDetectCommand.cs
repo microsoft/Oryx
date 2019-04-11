9da38b8b9b26a9083b7f086d3398c6f3f3a2dfe9
Dor Fire <dorfire@microsoft.com>
2019-04-11 14:20:11 -0700

    Removed obsolete assembly directive

--- src/BuildScriptGeneratorCli/Commands/BuildpackDetectCommand.cs
+++ src/BuildScriptGeneratorCli/Commands/BuildpackDetectCommand.cs
@@ -6,15 +6,12 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Oryx.BuildScriptGenerator;
 using Microsoft.Oryx.Common;
-
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 
 namespace Microsoft.Oryx.BuildScriptGeneratorCli
 {
