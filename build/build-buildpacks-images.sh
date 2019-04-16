c29e11333374e310498c15912cb8c292c531dfa8
Dor <dorfire@microsoft.com>
2019-04-16 13:41:30 +0300

    Moved folder

--- build/build-buildpacks-images.sh
+++ build/build-buildpacks-images.sh
@@ -9,28 +9,35 @@ set -e
 declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
 source $REPO_DIR/build/__variables.sh
 
-# Build base image for builder
+echo "-> Building stack base image: $DOCKER_PACK_STACK_BASE_IMAGE_REPO"
+echo
 cd "$BUILD_IMAGES_BUILD_CONTEXT_DIR"
-docker build -f "$BUILDER_BASE_IMAGE_DOCKERFILE" -t $DOCKER_BUILDER_BASE_IMAGE_REPO:latest .
+docker build -f "$PACK_STACK_BASE_IMAGE_DOCKERFILE" \
+			 -t $DOCKER_PACK_STACK_BASE_IMAGE_REPO:latest \
+			 -t $DOCKER_PACK_BUILDER_IMAGE_REPO:latest \
+			 .
 
 cd /tmp
 
-$REPO_DIR/images/buildpack-builder/install-pack.sh
+$REPO_DIR/images/pack-builder/install-pack.sh
 
-# Create builder
-builderName="$DOCKER_DEV_REPO_BASE/buildpack-builder"
-./pack create-builder $builderName \
-	--builder-config $REPO_DIR/images/buildpack-builder/builder.toml \
+echo "-> Creating builder image: $DOCKER_PACK_BUILDER_IMAGE_REPO"
+echo
+./pack create-builder $DOCKER_PACK_BUILDER_IMAGE_REPO:latest \
+	--builder-config $REPO_DIR/images/pack-builder/builder.toml \
 	--no-pull
+echo "-> Created builder"
 
-# Remove pack everything that was added by it
+# Remove pack & everything that was added by it
 rm -f   ./pack 
 rm -rf ~/.pack
 
 # Build an image that runs `pack`
+echo "-> Building pack runner image: $DOCKER_PACK_IMAGE_REPO"
+echo
 cd "$BUILD_IMAGES_BUILD_CONTEXT_DIR"
 docker build -f "$PACK_IMAGE_DOCKERFILE" \
 	--build-arg BUILD_NUMBER='0.2.0' \
-	--build-arg BUILDPACK_BUILDER_NAME="$builderName" \
+	--build-arg BUILDPACK_BUILDER_NAME="$builderImageName" \
 	-t $DOCKER_PACK_IMAGE_REPO:latest \
 	.
--- images/pack-builder/pack-runner.Dockerfile
+++ images/pack-builder/pack-runner.Dockerfile
@@ -3,7 +3,7 @@ FROM buildpack-deps:stable
 WORKDIR /tmp
 
 COPY images/buildpack-builder/install-pack.sh install-pack.sh
-RUN ./install-pack.sh && mv pack /usr/local/bin
+RUN ./install-pack.sh && mv pack /usr/local/bin && rm install-pack.sh
 
 ARG BUILDPACK_BUILDER_NAME
 RUN pack set-default-builder $BUILDPACK_BUILDER_NAME
