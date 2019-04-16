5d72f384cd742bb0bf0a6e5ef357a6721e53e0fc
Dor <dorfire@microsoft.com>
2019-04-16 17:13:18 +0300

    Fixed vars

--- build/build-buildpacks-images.sh
+++ build/build-buildpacks-images.sh
@@ -13,20 +13,19 @@ echo "-> Building stack base image: $DOCKER_PACK_STACK_BASE_IMAGE_REPO"
 echo
 cd "$BUILD_IMAGES_BUILD_CONTEXT_DIR"
 docker build -f "$PACK_STACK_BASE_IMAGE_DOCKERFILE" \
-			 -t $DOCKER_PACK_STACK_BASE_IMAGE_REPO:latest \
-			 -t $DOCKER_PACK_BUILDER_IMAGE_REPO:latest \
+			 -t "$DOCKER_PACK_STACK_BASE_IMAGE_REPO:latest" \
+			 -t "mcr.microsoft.com/oryx/$DOCKER_PACK_STACK_BASE_IMAGE_NAME" \
 			 .
 
 cd /tmp
 
 $REPO_DIR/images/pack-builder/install-pack.sh
 
-echo "-> Creating builder image: $DOCKER_PACK_BUILDER_IMAGE_REPO"
+echo "-> Creating builder image: $DOCKER_PACK_STACK_BASE_IMAGE_REPO"
 echo
-./pack create-builder $DOCKER_PACK_BUILDER_IMAGE_REPO:latest \
+./pack create-builder $DOCKER_PACK_BUILDER_IMAGE_REPO \
 	--builder-config $REPO_DIR/images/pack-builder/builder.toml \
 	--no-pull
-echo "-> Created builder"
 
 # Remove pack & everything that was added by it
 rm -f   ./pack 
@@ -38,6 +37,6 @@ echo
 cd "$BUILD_IMAGES_BUILD_CONTEXT_DIR"
 docker build -f "$PACK_IMAGE_DOCKERFILE" \
 	--build-arg BUILD_NUMBER="$BUILD_NUMBER" \
-	--build-arg BUILDPACK_BUILDER_NAME="$builderImageName" \
+	--build-arg DEFAULT_BUILDER_NAME="mcr.microsoft.com/oryx/$DOCKER_PACK_BUILDER_IMAGE_NAME" \
 	-t $DOCKER_PACK_IMAGE_REPO:latest \
 	.
--- images/pack-builder/pack-runner.Dockerfile
+++ images/pack-builder/pack-runner.Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /tmp
 COPY images/pack-builder/install-pack.sh install-pack.sh
 RUN ./install-pack.sh && mv pack /usr/local/bin && rm install-pack.sh
 
-ARG BUILDPACK_BUILDER_NAME
-RUN pack set-default-builder $BUILDPACK_BUILDER_NAME
+ARG DEFAULT_BUILDER_NAME
+RUN pack set-default-builder $DEFAULT_BUILDER_NAME
 
 ENTRYPOINT ["/usr/local/bin/pack"]
