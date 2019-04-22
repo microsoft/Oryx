5df80cd1e6f419358118a695629192bab8d3d695
Dor <dorfire@microsoft.com>
2019-04-22 18:08:13 +0300

    Styling

--- src/startupscriptgenerator/common/configuration.go
+++ src/startupscriptgenerator/common/configuration.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Order of precedence for value of settings: Explicit command like argument => Environment Variable
-func SetEnvironmentVariableInScript(scriptBuilder *strings.Builder, environmentVariableName string, explicitValue string, defaultValue string){
+func SetEnvironmentVariableInScript(scriptBuilder *strings.Builder, environmentVariableName string, explicitValue string, defaultValue string) {
 	if explicitValue != "" {
 		scriptBuilder.WriteString("export " + environmentVariableName + "=" + explicitValue)
 	} else {
