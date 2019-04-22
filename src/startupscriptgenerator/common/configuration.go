37f749ebae7eb2cbddbe2d58dc66212c111ed89f
Dor <dorfire@microsoft.com>
2019-04-22 15:50:09 +0300

    Added consolidated final newlines

--- src/startupscriptgenerator/common/configuration.go
+++ src/startupscriptgenerator/common/configuration.go
@@ -12,10 +12,11 @@ import (
 // Order of precedence for value of settings: Explicit command like argument => Environment Variable
 func SetEnvironmentVariableInScript(scriptBuilder *strings.Builder, environmentVariableName string, explicitValue string, defaultValue string){
 	if explicitValue != "" {
-		scriptBuilder.WriteString("export " + environmentVariableName + "=" + explicitValue + "\n\n")
+		scriptBuilder.WriteString("export " + environmentVariableName + "=" + explicitValue)
 	} else {
 		scriptBuilder.WriteString("if [ -z \"$" + environmentVariableName + "\" ]; then" + "\n");
 		scriptBuilder.WriteString("		export " + environmentVariableName + "=" + defaultValue + "\n")
-		scriptBuilder.WriteString("fi\n\n")
+		scriptBuilder.WriteString("fi")
 	}
+	scriptBuilder.WriteString("\n\n")
 }
