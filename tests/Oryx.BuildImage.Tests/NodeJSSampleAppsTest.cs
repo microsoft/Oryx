b34ecd74a79e6426b1ecc3333793950dc4a5ae2d
Kiran Challa <kichalla@users.noreply.github.com>
2019-04-23 10:27:53 -0700

    Test clean-up: clean ShellScriptBuilder (#92)

--- tests/Oryx.BuildImage.Tests/NodeJSSampleAppsTest.cs
+++ tests/Oryx.BuildImage.Tests/NodeJSSampleAppsTest.cs
@@ -205,7 +205,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
             var logFile = "/tmp/directory.log";
 
             var script = SetupEnvironment_ErrorDetectingNodeTest(appDir, appOutputDir, logFile)
-                .AddFileExistsCheck(logFile, true)
+                .AddFileExistsCheck(logFile)
                 .AddStringExistsInFileCheck("idea.js", logFile)
                 .AddStringExistsInFileCheck("app2", logFile)
                 .AddStringExistsInFileCheck("app3", logFile)
@@ -243,8 +243,8 @@ namespace Microsoft.Oryx.BuildImage.Tests
 
             var script = SetupEnvironment_ErrorDetectingNodeTest(appDir, appOutputDir, logFile)
                 .AddBuildCommand($"{appDir} -i /tmp/int -o {appOutputDir} --log-file {logFile}")
-                .AddStringNotExistsInFileCheck("app4", logFile, true)
-                .AddStringNotExistsInFileCheck("3.log", logFile)
+                .AddStringDoesNotExistInFileCheck("app4", logFile)
+                .AddStringDoesNotExistInFileCheck("3.log", logFile)
                 .ToString();
 
             // Act
