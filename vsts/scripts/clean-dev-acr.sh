484650768913d7e34f4c6299bec51f3facabfd91
Dor <dorfire@microsoft.com>
2019-04-24 15:43:01 +0300

    Added yes flag

--- vsts/scripts/clean-dev-acr.sh
+++ vsts/scripts/clean-dev-acr.sh
@@ -9,6 +9,10 @@ set -e
 declare -r ACR_NAME='oryxdevmcr'
 declare -r AZ_NAME_OUTPUT_PARAMS="--name $ACR_NAME --output tsv"
 
+if [[ "$1" == "--yes" ]]; then
+	YES="$1"
+fi
+
 # Prepare an array with all repository names in the registry
 REPOS=(`az acr repository list $AZ_NAME_OUTPUT_PARAMS`)
 echo "Found ${#REPOS[@]} repositories in ACR instance '$ACR_NAME'"
@@ -17,7 +21,7 @@ echo
 datecmd='date'
 if [[ "$OSTYPE" == "darwin"* ]]; then datecmd='gdate'; fi
 
-tsLimit=`$datecmd --iso-8601=seconds -d 'month ago'`
+tsLimit=`$datecmd --iso-8601=seconds -d '2 months ago'`
 declare -r azQuery="[?timestamp<=\`$tsLimit\`].digest"
 
 for repo in "${REPOS[@]}"
@@ -27,7 +31,7 @@ do
 	echo "Deleting ${#digests[@]} images created before '$tsLimit' in repository '$repo'..."
 	for manifest in "${MANIFESTS[@]}"
 	do
-		az acr repository delete --name $ACR_NAME --yes --image $manifest
+		az acr repository delete --name $ACR_NAME $YES --image $manifest
 		echo "> Deleted $manifest"
 	done
 	echo
