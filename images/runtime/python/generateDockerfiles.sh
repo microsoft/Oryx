ebe95d5392601a8bd910e2274bfc7311346bb20c
Kiran Challa <kichalla@microsoft.com>
2019-05-02 06:28:11 -0700

    Create base images for php and node runtime images

--- images/runtime/python/generateDockerfiles.sh
+++ images/runtime/python/generateDockerfiles.sh
@@ -11,7 +11,6 @@ declare -r PYTHON_VERSIONS_PATH=$DIR/../../../build/__python-versions.sh
 declare -r VERSIONS_FILE="$DIR/pythonVersions.txt"
 declare -r DOCKERFILE_TEMPLATE="$DIR/Dockerfile.template"
 declare -r IMAGE_NAME_PLACEHOLDER="%PYTHON_BASE_IMAGE%"
-declare -r ALPINE_OR_STRETCH_PLACEHOLDER="%PYTHON_BASE_IMAGE_ALPINE_OR_STRETCH%"
 declare -r IMAGE_SUFFIX="-slim-stretch"
 
 source "$PYTHON_VERSIONS_PATH"
@@ -23,17 +22,10 @@ do
 	VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
 	echo "Generating Dockerfile for image '$PYTHON_IMAGE_NAME' in directory '$VERSION_DIRECTORY'..."
 
-	GO_IMAGE_TYPE="stretch"
-	# Figure out if the final image is Alpine based
-	if [[ $PYTHON_IMAGE_NAME == *"alpine"* ]]; then
-		GO_IMAGE_TYPE="alpine"
-	fi
-
 	mkdir -p "$DIR/$VERSION_DIRECTORY/"
 	TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/Dockerfile"
 	cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
 
 	# Replace placeholders
 	sed -i "s|$IMAGE_NAME_PLACEHOLDER|$PYTHON_IMAGE_NAME|g" "$TARGET_DOCKERFILE"
-	sed -i "s|$ALPINE_OR_STRETCH_PLACEHOLDER|$GO_IMAGE_TYPE|g" "$TARGET_DOCKERFILE"
 done < <(compgen -A variable | grep 'PYTHON[0-9]\{2,\}_VERSION')
