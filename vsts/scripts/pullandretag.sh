f19aec1abd7f8956af43c30c126790da76f2964f
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-05-03 14:47:20 -0700

    add few more changes to pull and retag to optimize

--- vsts/scripts/pullandretag.sh
+++ vsts/scripts/pullandretag.sh
@@ -6,21 +6,37 @@
 
 set -o pipefail
 
-echo "upper: "$TESTINTEGRATIONCASEFILTER
-echo "lower: "$TestIntegrationCaseFilter
-
 declare integrationtestfilter="oryxdevmcr.azurecr.io/public/oryx/"
+
+echo "Build image filter is set"
+while read sourceImage; do
+  # Always use specific build number based tag and then use the same tag to create a 'latest' tag and push it
+  if [[ $sourceImage != *:latest ]]; then
+	if [[ $sourceImage == *"build"* ]]; then
+		echo "Pulling the source image $sourceImage ..."
+		docker pull "$sourceImage" | sed 's/^/     /'
+        
+		# Trim the build number tag and append the '':latest' to end of it
+		newtag="${sourceImage%:*}:latest"
+
+		# Replace the ACR registry repository name with a name that the tests know about
+		newtag=$(echo "$newtag" | sed 's,oryxdevmcr.azurecr.io/public/oryx,oryxdevms,g')
+
+		echo
+		echo "Tagging the source image with tag $newtag ..."
+		docker tag "$sourceImage" "$newtag" | sed 's/^/     /'
+		echo
+		echo -------------------------------------------------------------------------------
+	fi
+  fi
+done <"$1"
 
 if [ -n "$TESTINTEGRATIONCASEFILTER" ];then
 	integrationtestfilter=$TESTINTEGRATIONCASEFILTER
-elif [ -n "$TestIntegrationCaseFilter" ];then
-	integrationtestfilter="$TestIntegrationCaseFilter"
 fi
 
 integrationtestfilter=$(echo "$integrationtestfilter" | sed 's/.*/\L&/')
-echo "filter is set for: "$integrationtestfilter
-
-docker ps -a 
+echo "Runtime image filter is set for "$integrationtestfilter
 
 while read sourceImage; do
   # Always use specific build number based tag and then use the same tag to create a 'latest' tag and push it
@@ -42,4 +58,4 @@ while read sourceImage; do
 		echo -------------------------------------------------------------------------------
 	fi
   fi
-done <"$1"
+done <"$2"
