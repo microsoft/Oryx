545c8074f5cec0854619f18d99c19b2f69ba4c4c
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-05-03 11:36:06 -0700

    filter images for pulling and retagging

--- vsts/scripts/pullandretag.sh
+++ vsts/scripts/pullandretag.sh
@@ -6,22 +6,40 @@
 
 set -o pipefail
 
+echo "upper: "$TESTINTEGRATIONCASEFILTER
+echo "lower: "$TestIntegrationCaseFilter
+
+declare integrationtestfilter="oryxdevmcr.azurecr.io/public/oryx/"
+
+if [ -n "$TESTINTEGRATIONCASEFILTER" ];then
+	integrationtestfilter=$TESTINTEGRATIONCASEFILTER
+elif [ -n "$TestIntegrationCaseFilter" ]
+	integrationtestfilter="$TestIntegrationCaseFilter"
+fi
+
+integrationtestfilter=$(echo "$integrationtestfilter" | sed 's/.*/\L&/')
+echo "filter is set for: "$integrationtestfilter
+
+docker ps -a 
+
 while read sourceImage; do
   # Always use specific build number based tag and then use the same tag to create a 'latest' tag and push it
   if [[ $sourceImage != *:latest ]]; then
-    echo "Pulling the source image $sourceImage ..."
-    docker pull "$sourceImage" | sed 's/^/     /'
+	if [[ $sourceImage == *"$integrationtestfilter"* ]]; then
+		echo "Pulling the source image $sourceImage ..."
+		docker pull "$sourceImage" | sed 's/^/     /'
         
-    # Trim the build number tag and append the '':latest' to end of it
-    newtag="${sourceImage%:*}:latest"
+		# Trim the build number tag and append the '':latest' to end of it
+		newtag="${sourceImage%:*}:latest"
 
-    # Replace the ACR registry repository name with a name that the tests know about
-    newtag=$(echo "$newtag" | sed 's,oryxdevmcr.azurecr.io/public/oryx,oryxdevms,g')
+		# Replace the ACR registry repository name with a name that the tests know about
+		newtag=$(echo "$newtag" | sed 's,oryxdevmcr.azurecr.io/public/oryx,oryxdevms,g')
 
-    echo
-    echo "Tagging the source image with tag $newtag ..."
-    docker tag "$sourceImage" "$newtag" | sed 's/^/     /'
-    echo
-    echo -------------------------------------------------------------------------------
+		echo
+		echo "Tagging the source image with tag $newtag ..."
+		docker tag "$sourceImage" "$newtag" | sed 's/^/     /'
+		echo
+		echo -------------------------------------------------------------------------------
+	fi
   fi
 done <"$1"
