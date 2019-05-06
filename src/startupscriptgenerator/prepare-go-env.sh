e4fe37e48de029548d417aa59e32c3ed65aa8c07
Kiran Challa <kichalla@users.noreply.github.com>
2019-05-06 16:42:18 -0700

    Create base images for php and node runtime images (#119)

--- src/startupscriptgenerator/prepare-go-env.sh
+++ src/startupscriptgenerator/prepare-go-env.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-# If we're running on Alpine (i.e. apk is available), then most likely these two packages are missing
-if type "apk" > /dev/null; then
-	apk add --no-cache git
-	apk add --no-cache build-base
-fi
-
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
