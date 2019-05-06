5ec9d9f183ee8e14e29da1264dd8c6c0743c1cde
Dor <dorfire@microsoft.com>
2019-05-06 13:41:37 -0700

    Added a server for PHP keys (#117)

--- images/build/php/prereqs/build.sh
+++ images/build/php/prereqs/build.sh
@@ -50,7 +50,9 @@ if [ -n "$PHP_ASC_URL" ]; then
 	export GNUPGHOME="$(mktemp -d)";
 	for i in {1..5}; do
 		gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys "${GPG_KEYS[0]}" || \
-			gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys "${GPG_KEYS[1]}";
+		gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys "${GPG_KEYS[1]}" || \
+		gpg --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "${GPG_KEYS[0]}" || \
+		gpg --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "${GPG_KEYS[1]}";
 		if [ $? -eq 0 ]; then break; fi
 	done
 	gpg --batch --verify php.tar.xz.asc php.tar.xz;
