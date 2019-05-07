c21e0ce743d452a7bde72f1414bb8217a7c5ca5d
Dor Fire <dorfire@microsoft.com>
2019-05-07 13:49:14 -0700

    Added test

---
+++ tests/Oryx.Common.Test/StringExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.Oryx.Common.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void ReplaceUrlPasswords_Sanity()
+        {
+            Assert.Equal("https://bla:***@example.com/", "https://bla:blabla@example.com/".ReplaceUrlPasswords());
+            Assert.Equal("https://bla:***@example.com/", "https://bla:123456@example.com/".ReplaceUrlPasswords());
+            Assert.Equal("https://bla:***@example.com/", "https://bla:123bla@example.com/".ReplaceUrlPasswords());
+            Assert.Equal("https://bla:*@example.com/", "https://bla:bla123@example.com/".ReplaceUrlPasswords("*"));
+
+            Assert.Equal("https://bla:***@example.com/ and then ftp://user:***@example.org/subdir",
+                "https://bla:123bla@example.com/ and then ftp://user:pass@example.org/subdir".ReplaceUrlPasswords());
+        }
+    }
+}
