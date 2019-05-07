4b406e996924f10071e11dd1a99dfee60973cad9
Dor Fire <dorfire@microsoft.com>
2019-05-07 14:28:43 -0700

    Added method

--- tests/Oryx.Common.Test/StringExtensionsTest.cs
+++ tests/Oryx.Common.Test/StringExtensionsTest.cs
@@ -13,12 +13,24 @@ namespace Microsoft.Oryx.Common.Extensions
         public void ReplaceUrlPasswords_Sanity()
         {
             Assert.Equal("https://bla:***@example.com/", "https://bla:blabla@example.com/".ReplaceUrlPasswords());
+
             Assert.Equal("https://bla:***@example.com/", "https://bla:123456@example.com/".ReplaceUrlPasswords());
+
             Assert.Equal("https://bla:***@example.com/", "https://bla:123bla@example.com/".ReplaceUrlPasswords());
+
             Assert.Equal("https://bla:*@example.com/", "https://bla:bla123@example.com/".ReplaceUrlPasswords("*"));
 
-            Assert.Equal("https://bla:***@example.com/ and then ftp://user:***@example.org/subdir",
-                "https://bla:123bla@example.com/ and then ftp://user:pass@example.org/subdir".ReplaceUrlPasswords());
+            Assert.Equal(
+                "just before https://root:***@sub.example.net",
+                "just before https://root:to!1or@sub.example.net".ReplaceUrlPasswords());
+
+            Assert.Equal(
+                "https://bla:***@example.com/ only after",
+                "https://bla:blabla@example.com/ only after".ReplaceUrlPasswords());
+
+            Assert.Equal(
+                "before https://bla:***@example.com/ and then ftp://user:***@example.org/subdir",
+                "before https://bla:123bla@example.com/ and then ftp://user:pass@example.org/subdir".ReplaceUrlPasswords());
         }
     }
 }
