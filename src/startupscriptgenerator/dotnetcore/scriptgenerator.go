77d951432c05c6f4f9de7ed84457a7855702e9e6
Kiran Challa <kichalla@microsoft.com>
2019-05-15 17:07:23 -0700

    Fixed indentation

--- src/startupscriptgenerator/dotnetcore/scriptgenerator.go
+++ src/startupscriptgenerator/dotnetcore/scriptgenerator.go
@@ -75,46 +75,46 @@ fi
 if [ -z "$startUpCommand" ]; then
 	echo Finding the startup file name...
 	for file in *; do 
-	if [ -f "$file" ]; then 
-		case $file in
-		*.runtimeconfig.json)
-			startupDllFileNamePrefix=${file%%.runtimeconfig.json}
-			startupExecutableFileName="$startupDllFileNamePrefix"
-			startupDllFileName="$startupDllFileNamePrefix.dll"
-			break
-		;;
-		esac
-	fi
+		if [ -f "$file" ]; then 
+			case $file in
+			*.runtimeconfig.json)
+				startupDllFileNamePrefix=${file%%.runtimeconfig.json}
+				startupExecutableFileName="$startupDllFileNamePrefix"
+				startupDllFileName="$startupDllFileNamePrefix.dll"
+				break
+			;;
+			esac
+		fi
 	done
 
 	if [ -f "$startupExecutableFileName" ]; then
-	# Starting ASP.NET Core 3.0, an executable is created based on the platform where it is published from,
-	# so for example, if a user does a publish (not self-contained) on Mac, there would be files like 'todoApp'
-	# and 'todoApp.dll'. In this scenario the 'todoApp' executable is actually meant for Mac and not for Linux.
-	# So here we check for the file type and fall back to using 'dotnet todoApp.dll'.
-	
-	isLinuxExecutable $startupExecutableFileName
-	if [ "$isLinuxExecutableResult" == "true" ]; then
-	  echo "Found the startup executable file '$startupExecutableFileName'"
-	  startUpCommand="./$startupExecutableFileName"
-	else
-	  echo "Cannot use executable '$startupExecutableFileName' as startup file as it is not meant for Linux"
-	fi
+		# Starting ASP.NET Core 3.0, an executable is created based on the platform where it is published from,
+		# so for example, if a user does a publish (not self-contained) on Mac, there would be files like 'todoApp'
+		# and 'todoApp.dll'. In this scenario the 'todoApp' executable is actually meant for Mac and not for Linux.
+		# So here we check for the file type and fall back to using 'dotnet todoApp.dll'.
+
+		isLinuxExecutable $startupExecutableFileName
+		if [ "$isLinuxExecutableResult" == "true" ]; then
+			echo "Found the startup executable file '$startupExecutableFileName'"
+			startUpCommand="./$startupExecutableFileName"
+		else
+			echo "Cannot use executable '$startupExecutableFileName' as startup file as it is not meant for Linux"
+		fi
 	fi
 
 	if [ -z "$startUpCommand" ] && [ -f "$startupDllFileName" ]; then
-	  echo "Found the startup file '$startupDllFileName'"
-	  startUpCommand="dotnet '$startupDllFileName'"
+			echo "Found the startup file '$startupDllFileName'"
+			startUpCommand="dotnet '$startupDllFileName'"
 	fi
 fi
 
 if [ -z "$startUpCommand" ]; then
 	if [ -f "$defaultAppFilePath" ]; then
-	  cd "$defaultAppFileDir"
-	  startUpCommand="dotnet '$defaultAppFilePath'"
+		cd "$defaultAppFileDir"
+		startUpCommand="dotnet '$defaultAppFilePath'"
 	else
-	  echo Unable to start the application.
-	  exit 1
+		echo Unable to start the application.
+		exit 1
 	fi
 fi
 
