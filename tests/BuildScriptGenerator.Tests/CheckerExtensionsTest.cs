0846f4e7f0d968f21daaf66c32c776257cbe418d
Dor Fire <dorfire@microsoft.com>
2019-05-17 15:09:55 -0700

    Added tester scaffold

---
+++ tests/BuildScriptGenerator.Tests/CheckerExtensionsTest.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Tests
+{
+    public class CheckerExtensionsTest
+    {
+
+    }
+}
