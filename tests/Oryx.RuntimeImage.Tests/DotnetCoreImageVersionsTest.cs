2516b08577f2c6b427612723cb96baacd876a063
Kiran Challa <kichalla@microsoft.com>
2019-05-20 13:18:52 -0700

    Fixed test

--- tests/Oryx.RuntimeImage.Tests/DotnetCoreImageVersionsTest.cs
+++ tests/Oryx.RuntimeImage.Tests/DotnetCoreImageVersionsTest.cs
@@ -82,8 +82,8 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
 
         [Theory]
         [InlineData("2.0", "Version  : 2.0.9")]
-        [InlineData("2.1", "Version: 2.1.10")]
-        [InlineData("2.2", "Version: 2.2.4")]
+        [InlineData("2.1", "Version: 2.1.11")]
+        [InlineData("2.2", "Version: 2.2.5")]
         [InlineData("3.0", "Version: 3.0.0-preview5-27626-15")]
         public void RuntimeImage_HasExecptedDotnetVersion(string version, string expectedOutput)
         {
