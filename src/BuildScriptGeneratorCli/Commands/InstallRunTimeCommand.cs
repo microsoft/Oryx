871831ef45dd345cad8b92f4dc21b14e73fd4dc2
Dor Fire <dorfire@microsoft.com>
2019-05-20 17:51:17 -0700

    Added install-runtime command

---
+++ src/BuildScriptGeneratorCli/Commands/InstallRunTimeCommand.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Oryx.BuildScriptGenerator;
+using Microsoft.Oryx.Common;
+
+namespace Microsoft.Oryx.BuildScriptGeneratorCli.Commands
+{
+    [Command("install-runtime", Description = "Install the required runtime components for a platform.")]
+    class InstallRunTimeCommand : CommandBase
+    {
+        [Option(
+            "-p|--platform <name>",
+            CommandOptionType.SingleValue,
+            Description = "The name of the platform for which the runtime components should be installed.")]
+        public string Platform { get; set; }
+
+        [Option(
+            "--platform-version <version>",
+            CommandOptionType.SingleValue,
+            Description = "The version of the platform for which the runtime components should be installed.")]
+        public string PlatformVersion { get; set; }
+
+        internal override int Execute(IServiceProvider serviceProvider, IConsole console)
+        {
+            var scriptGenerator = new RunTimeInstallationScriptGenerator(serviceProvider, console);
+
+            if (!scriptGenerator.TryGenerateScript(out var generatedScript))
+            {
+                return ProcessConstants.ExitFailure;
+            }
+
+            console.WriteLine(generatedScript);
+
+            return ProcessConstants.ExitSuccess;
+        }
+    }
+}
