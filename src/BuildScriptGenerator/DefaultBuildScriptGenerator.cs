5ae93f7cb83f4f13b4fa88bac55904088f621c01
Dor <dorfire@microsoft.com>
2019-05-21 15:05:08 -0700

    Reverted

--- src/BuildScriptGenerator/DefaultBuildScriptGenerator.cs
+++ src/BuildScriptGenerator/DefaultBuildScriptGenerator.cs
@@ -215,7 +215,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator
             IDictionary<string, string> tools,
             [NotNull] List<ICheckerMessage> checkerMessageSink)
         {
-            var checkers = _checkers.WhereApplicable(tools, _logger).ToArray();
+            var checkers = _checkers.WhereApplicable(tools).ToArray();
 
             _logger.LogInformation("Running {checkerCount} applicable checkers for {toolCount} tools: {toolNames}",
                 checkers.Length, tools.Keys.Count, string.Join(',', tools.Keys));
