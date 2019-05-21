564bbbbf0933206042f7a20badaa24851d9b7640
Dor Fire <dorfire@microsoft.com>
2019-05-20 17:21:04 -0700

    Added RunTimeInstallationScriptGeneratorOptions

---
+++ src/BuildScriptGenerator/Options/RunTimeInstallationScriptGeneratorOptions.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Oryx.BuildScriptGenerator
+{
+    public class RunTimeInstallationScriptGeneratorOptions
+    {
+        public string DestinationDir { get; set; }
+
+        public string Language { get; set; }
+
+        public string LanguageVersion { get; set; }
+    }
+}
