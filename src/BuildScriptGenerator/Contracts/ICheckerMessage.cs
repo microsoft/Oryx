9879403deb7871c4035d051b87bb4cbdf3432252
Dor Fire <dorfire@microsoft.com>
2019-05-22 10:01:30 -0700

    Improved doc

--- src/BuildScriptGenerator/Contracts/ICheckerMessage.cs
+++ src/BuildScriptGenerator/Contracts/ICheckerMessage.cs
@@ -7,10 +7,19 @@ using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Oryx.BuildScriptGenerator
 {
+    /// <summary>
+    /// Defines the interface of a message that can be emitted from an <see cref="IChecker"/>.
+    /// </summary>
     public interface ICheckerMessage
     {
+        /// <summary>
+        /// Severity level.
+        /// </summary>
         LogLevel Level { get; }
 
+        /// <summary>
+        /// Body of message. Can span multiple lines.
+        /// </summary>
         string Content { get; }
     }
 }
