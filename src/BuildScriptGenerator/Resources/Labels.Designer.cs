d3307e5512d987e690bab0aafe2e0969d58592b1
Dor Fire <dorfire@microsoft.com>
2019-05-23 11:16:39 -0700

    Moved value to a label

--- src/BuildScriptGenerator/Resources/Labels.Designer.cs
+++ src/BuildScriptGenerator/Resources/Labels.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Labels {
@@ -57,6 +57,15 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Resources {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to An outdated version of Node.js was detected ({0}). Consider updating.\nVersions supported by Oryx: {1}.
+        /// </summary>
+        internal static string NodeVersionCheckerMessageFormat {
+            get {
+                return ResourceManager.GetString("NodeVersionCheckerMessageFormat", resourceCulture);
             }
         }
         
