4e852cdd19f5613c61c466dcebffe7a98a7d0e3e
Kiran Challa <kichalla@users.noreply.github.com>
2019-05-23 11:55:42 -0700

    Tests: Added tests for .NET Core 1.0 and 2.0 apps (#167)

---
+++ tests/SampleApps/DotNetCore/aspnetcore10/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
