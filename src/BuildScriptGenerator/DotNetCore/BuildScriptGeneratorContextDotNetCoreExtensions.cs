c60175f4a0824e31b866f369096bbd39cd29b0c2
Dor Fire <dorfire@microsoft.com>
2019-05-29 16:08:55 -0700

    Moved platform-specific members to platform dirs

--- src/BuildScriptGenerator/DotNetCore/BuildScriptGeneratorContextDotNetCoreExtensions.cs
+++ src/BuildScriptGenerator/DotNetCore/BuildScriptGeneratorContextDotNetCoreExtensions.cs
@@ -8,15 +8,15 @@ namespace Microsoft.Oryx.BuildScriptGenerator
     public partial class BuildScriptGeneratorContext
     {
         /// <summary>
-        /// Gets or sets a value indicating whether the detection and build of PHP code
-        /// in the repo should be enabled.
+        /// Gets or sets a value indicating whether the detection and build of .NET core
+        /// code in the repo should be enabled.
         /// Defaults to true.
         /// </summary>
-        public bool EnablePhp { get; set; } = true;
+        public bool EnableDotNetCore { get; set; } = true;
 
         /// <summary>
-        /// Gets or sets the version of PHP used in the repo.
+        /// Gets or sets the version of .NET Core used in the repo.
         /// </summary>
-        public string PhpVersion { get; set; }
+        public string DotnetCoreVersion { get; set; }
     }
 }
