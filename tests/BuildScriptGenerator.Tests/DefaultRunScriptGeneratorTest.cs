af859134f9b206f1e730333a02392e17f59f45a3
Dor Fire <dorfire@microsoft.com>
2019-06-05 16:09:53 -0700

    Added tester for DefaultRunScriptGenerator

---
+++ tests/BuildScriptGenerator.Tests/DefaultRunScriptGeneratorTest.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Oryx.Tests.Common;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Tests
+{
+    public class DefaultRunScriptGeneratorTest
+    {
+        [Fact(Skip = "WIP")]
+        public void GenerateBashScript_ReturnsScript()
+        {
+            // Arrange
+            var platform = new TestProgrammingPlatform(
+                "test",
+                new[] { "1.0.0" },
+                canGenerateScript: true,
+                scriptContent: "script-content");
+            var gen = new DefaultRunScriptGenerator(new[] { platform }, NullLogger<DefaultRunScriptGenerator>.Instance);
+            var ctx = new RunScriptGeneratorOptions { SourceRepo = new MemorySourceRepo() };
+
+            // Act
+            var generatedScript = gen.GenerateBashScript(platform.Name, ctx);
+
+            // Assert
+            Assert.Contains("script-content", generatedScript);
+        }
+    }
+}
