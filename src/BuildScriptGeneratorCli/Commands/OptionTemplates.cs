a1b17fa30c8352796791dd5782c8993676f7f792
Dor <dorfire@gmail.com>
2019-06-07 15:38:39 -0700

    Moved option templates to consts & added --platform (#199)

---
+++ src/BuildScriptGeneratorCli/Commands/OptionTemplates.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+namespace Microsoft.Oryx.BuildScriptGeneratorCli
+{
+    internal static class OptionTemplates
+    {
+        public const string Platform = "--platform <name>|-l|--language <name>";
+        public const string PlatformVersion = "--platform-version <version>|--language-version <version>";
+        public const string Property = "-p|--property <key-value>";
+    }
+}
