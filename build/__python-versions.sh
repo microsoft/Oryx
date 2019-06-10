d52dce4abb4ddb8489366e82f91ca25bc6dd80de
Dor Fire <dorfire@microsoft.com>
2019-06-10 13:55:31 -0700

    Reverted tag update

--- build/__python-versions.sh
+++ build/__python-versions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'build-constants.yaml'. Changes may be overridden.
 
-PYTHON_RUNTIME_BASE_TAG='20190530.1'
+PYTHON_RUNTIME_BASE_TAG='20190402.1'
 PIP_VERSION='19.0.3'
 PYTHON27_VERSION='2.7.16'
 PYTHON36_VERSION='3.6.8'
