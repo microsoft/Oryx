83975dc5722e745071aa8051539533eba22c1c82
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-06-14 12:10:15 -0700

    add package updates

--- tests/Oryx.Common.Tests/StringExtensionsTest.cs
+++ tests/Oryx.Common.Tests/StringExtensionsTest.cs
@@ -63,7 +63,7 @@ namespace Microsoft.Oryx.Common.Extensions
 
             Assert.Equal(
                 "before https://***@example.com/ and then ftp://***@example.org/subdir",
-                "before https://bla:123bla@example.com/ and " +
+                "before https://bla:123bla@example.com/ and ".ReplaceUrlUserInfo() +
                 "then ftp://user:pass@example.org/subdir".ReplaceUrlUserInfo());
         }
     }
