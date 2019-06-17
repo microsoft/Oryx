0ae319e473546753bd8d8178893e34ec92d12b45
Dor <dorfire@gmail.com>
2019-06-17 12:24:04 -0700

    Improved upgradability for Python (#202)

--- build/__python-versions.sh
+++ build/__python-versions.sh
@@ -1,5 +1,6 @@
 # This file was auto-generated from 'build-constants.yaml'. Changes may be overridden.
 
+PYTHON_BASE_TAG='20190610.2'
 PIP_VERSION='19.0.3'
 PYTHON27_VERSION='2.7.16'
 PYTHON36_VERSION='3.6.8'
