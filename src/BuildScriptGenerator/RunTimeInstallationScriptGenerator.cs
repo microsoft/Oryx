619350eecf2fead177c34b4ab3687d058d82abc9
Dor <dorfire@microsoft.com>
2019-06-17 12:55:48 -0700

    Added forgotten using statement

--- src/BuildScriptGenerator/RunTimeInstallationScriptGenerator.cs
+++ src/BuildScriptGenerator/RunTimeInstallationScriptGenerator.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Oryx.BuildScriptGenerator.Exceptions;
+using Microsoft.Oryx.Common.Extensions;
 
 namespace Microsoft.Oryx.BuildScriptGenerator
 {
