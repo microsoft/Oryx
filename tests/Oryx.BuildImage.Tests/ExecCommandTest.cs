85724a383ca7eb31e48f113a65778339fb14b9e7
Dor Fire <dorfire@microsoft.com>
2019-06-24 16:59:43 -0700

    Added test

---
+++ tests/Oryx.BuildImage.Tests/ExecCommandTest.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Oryx.Tests.Common;
+using Microsoft.Oryx.BuildScriptGeneratorCli;
+using Xunit;
+
+namespace Oryx.BuildImage.Tests
+{
+    public class ExecCommandTest
+    {
+        protected readonly DockerCli _dockerCli = new DockerCli();
+
+        [Fact(Skip = "WIP")]
+        public void Exec_Sanity()
+        {
+            // Arrange
+            var appPath = "/tmp/app";
+            var script = new ShellScriptBuilder()
+                .AddCommand($"mkdir {appPath}")
+                .CreateFile($"{appPath}/package.json", "{}")
+                .AddCommand($"oryx exec {appPath} 'node --version' --debug")
+                .ToString();
+
+            // Act
+            var result = _dockerCli.Run(
+                new DockerRunArguments(Settings.BuildImageName, "/bin/bash", new[] { "-c", script }));
+
+            // Assert
+        }
+    }
+}
