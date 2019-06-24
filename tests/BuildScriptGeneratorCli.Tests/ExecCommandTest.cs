4f2d60fc18c92bb22b9ddebbb766cbec2453d3c9
Dor Fire <dorfire@microsoft.com>
2019-06-24 15:56:30 -0700

    Added tester

---
+++ tests/BuildScriptGeneratorCli.Tests/ExecCommandTest.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Oryx.BuildScriptGenerator;
+using Microsoft.Oryx.Tests.Common;
+using Xunit;
+
+namespace Microsoft.Oryx.BuildScriptGeneratorCli.Tests
+{
+    public class ExecCommandTest : IClassFixture<TestTempDirTestFixture>
+    {
+        private readonly TestTempDirTestFixture _testDir;
+
+        public ExecCommandTest(TestTempDirTestFixture testFixture)
+        {
+            _testDir = testFixture;
+        }
+
+        [Fact]
+        public void OnExecute_ShowsErrorAndExits_WhenSourceDirectoryDoesNotExist()
+        {
+            // Arrange
+            var cmd = new ExecCommand
+            {
+                SourceDir = _testDir.GenerateRandomChildDirPath(),
+                Command = "bla",
+            };
+            var testConsole = new TestConsole();
+
+            // Act
+            var exitCode = cmd.OnExecute(new CommandLineApplication(testConsole), testConsole);
+
+            // Assert
+            Assert.NotEqual(0, exitCode);
+            var error = testConsole.StdError;
+            Assert.DoesNotContain("Usage:", error);
+            Assert.Contains("Could not find the source directory", error);
+        }
+    }
+}
