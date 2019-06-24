27776d9d1db81243314b81a3c12cc9ad7422d473
Dor Fire <dorfire@microsoft.com>
2019-06-24 16:42:41 -0700

    Regen'd consts

---
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
@@ -0,0 +1,15 @@
+// This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
+{
+    public static class DotNetCoreRunTimeVersions
+    {
+        public const string NetCoreApp10 = "1.0.16";
+        public const string NetCoreApp11 = "1.1.14";
+        public const string NetCoreApp20 = "2.0.9";
+        public const string NetCoreApp21 = "2.1.11";
+        public const string NetCoreApp22 = "2.2.5";
+        public const string NetCoreApp30 = "3.0.0";
+        public const string NetCoreApp30PreviewName = "3.0.0-preview5-27626-15";
+    }
+}
