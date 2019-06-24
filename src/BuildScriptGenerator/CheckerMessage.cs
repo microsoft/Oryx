b3ac28096728d444aa3836d58f3bbc24f4308cc1
Dor <dorfire@microsoft.com>
2019-06-24 10:41:25 -0700

    Reverted

--- src/BuildScriptGenerator/CheckerMessage.cs
+++ src/BuildScriptGenerator/CheckerMessage.cs
@@ -4,19 +4,24 @@
 // --------------------------------------------------------------------------------------------
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Oryx.BuildScriptGenerator;
 
 namespace Microsoft.Oryx.BuildScriptGenerator
 {
     public class CheckerMessage : ICheckerMessage
     {
+        private readonly LogLevel _level;
+
+        private readonly string _content;
+
         public CheckerMessage(string content, LogLevel level = LogLevel.Warning)
         {
-            Level = level;
-            Content = content;
+            _level = level;
+            _content = content;
         }
 
-        public LogLevel Level { get; }
+        public LogLevel Level => _level;
 
-        public string Content { get; }
+        public string Content => _content;
     }
 }
