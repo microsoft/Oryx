72811ca546957d3d844ed4dcff6e4ba704731a5b
Kiran Challa <kichalla@microsoft.com>
2019-06-25 06:35:10 -0700

    .NET Core: Updated 3.0 preview version

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreRuntimeVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRuntimeVersions.cs
@@ -10,6 +10,6 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string NetCoreApp21 = "2.1.11";
         public const string NetCoreApp22 = "2.2.5";
         public const string NetCoreApp30 = "3.0.0";
-        public const string NetCoreApp30PreviewName = "3.0.0-preview5-27626-15";
+        public const string NetCoreApp30PreviewName = "3.0.0-preview6-27804-01";
     }
 }
