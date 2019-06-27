5b5c67afcf697cfae919ae4e14640c9ad970d43a
Kiran Challa <kichalla@users.noreply.github.com>
2019-06-27 12:35:46 -0700

    Embedded commit information: Fix the commit id used to embed into binaries and images (#235)

--- tests/Oryx.RuntimeImage.Tests/DotnetCoreImageVersionsTest.cs
+++ tests/Oryx.RuntimeImage.Tests/DotnetCoreImageVersionsTest.cs
@@ -26,15 +26,16 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
         [InlineData("3.0")]
         public void DotNetCoreRuntimeImage_Contains_VersionAndCommit_Information(string version)
         {
-            var agentOS = Environment.GetEnvironmentVariable("AGENT_OS");
-            var gitCommitID = Environment.GetEnvironmentVariable("BUILD_SOURCEVERSION");
-            var buildNumber = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
-            var expectedOryxVersion = string.Concat(Settings.OryxVersion, buildNumber);
-
             // we cant always rely on gitcommitid as env variable in case build context is not correctly passed
             // so we should check agent_os environment variable to know if the build is happening in azure devops agent 
             // or locally, locally we need to skip this test
+            var agentOS = Environment.GetEnvironmentVariable("AGENT_OS");
             Skip.If(string.IsNullOrEmpty(agentOS));
+
+            // Arrange
+            var buildNumber = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
+            var expectedOryxVersion = string.Concat(Settings.OryxVersion, buildNumber);
+            var gitCommitID = GitHelper.GetCommitID();
 
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
