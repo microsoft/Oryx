7a7ccabce8513435ae301e651462edf8af497369
Kiran Challa <kichalla@microsoft.com>
2019-06-27 11:21:05 -0700

    Updated to use git cli to get the commit id

--- tests/Oryx.RuntimeImage.Tests/NodeImagesTest.cs
+++ tests/Oryx.RuntimeImage.Tests/NodeImagesTest.cs
@@ -462,15 +462,16 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
         [MemberData(nameof(TestValueGenerator.GetNodeVersions), MemberType = typeof(TestValueGenerator))]
         public void NodeImage_Contains_VersionAndCommit_Information(string version)
         {
-            var agentOS = Environment.GetEnvironmentVariable("AGENT_OS");
-            var gitCommitID = Environment.GetEnvironmentVariable("BUILD_SOURCEVERSION");
-            var buildNumber = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
-            var expectedOryxVersion = string.Concat(Settings.OryxVersion, buildNumber);
-
             // We can't always rely on git commit ID as env variable in case build context is not correctly passed
             // so we should check agent_os environment variable to know if the build is happening in azure devops agent 
             // or locally, locally we need to skip this test
+            var agentOS = Environment.GetEnvironmentVariable("AGENT_OS");
             Skip.If(string.IsNullOrEmpty(agentOS));
+
+            // Arrange
+            var gitCommitID = GitHelper.GetCommitID();
+            var buildNumber = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
+            var expectedOryxVersion = string.Concat(Settings.OryxVersion, buildNumber);
 
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
