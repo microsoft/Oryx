0f5be2bab74baa201c9bcf620d55fe750d159aaa
Dor <dorfire@microsoft.com>
2019-07-09 08:42:45 -0700

    Updated base tags

--- build/__pythonVersions.sh
+++ build/__pythonVersions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-PYTHON_BASE_TAG='20190705.1'
+PYTHON_BASE_TAG='20190709.1'
 PIP_VERSION='19.0.3'
 PYTHON27_VERSION='2.7.16'
 PYTHON36_VERSION='3.6.9'
