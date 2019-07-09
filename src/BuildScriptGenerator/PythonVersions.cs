c86a25e4fbe5ae6f345217d8eefdebf7d1d467cf
Dor <dorfire@microsoft.com>
2019-07-09 07:46:09 -0700

    Regenerated consts

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Oryx.Common
         public const string PipVersion = "19.0.3";
         public const string Python27Version = "2.7.16";
         public const string Python36Version = "3.6.9";
-        public const string Python37Version = "3.7.3";
+        public const string Python37Version = "3.7.4";
         public const string Python38Version = "3.8.0b2";
     }
 }
