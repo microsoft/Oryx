434fd3d5c4064622ee74c2d1ad7f54e4777aee39
Dor <dorfire@microsoft.com>
2019-07-09 15:49:02 -0700

    Fixed build

--- src/BuildScriptGeneratorCli/PipDownloadEventLogger.cs
+++ src/BuildScriptGeneratorCli/PipDownloadEventLogger.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using Microsoft.Oryx.Common;
+using Microsoft.Oryx.Common.Extensions;
 
 namespace Microsoft.Oryx.BuildScriptGenerator
 {
