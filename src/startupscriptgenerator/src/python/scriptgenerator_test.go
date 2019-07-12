31e38ba9e735b9ed68683dd805a84d65c17474e8
Kiran Challa <kichalla@users.noreply.github.com>
2019-07-11 17:09:27 -0700

    Python runtime startup script: Assign 'workers' to gunicorn based on cpu cores dynamically (#274)

--- src/startupscriptgenerator/src/python/scriptgenerator_test.go
+++ src/startupscriptgenerator/src/python/scriptgenerator_test.go
@@ -6,41 +6,69 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func ExamplePythonStartupScriptGenerator_getCommandFromModule_onlyModule() {
+func Test_ExamplePythonStartupScriptGenerator_getCommandFromModule_onlyModule(t *testing.T) {
 	// Arrange
+	workerCount := getWorkerCount()
+	expected := "GUNICORN_CMD_ARGS=\"--timeout 600 --access-logfile '-' --error-logfile '-' --workers=" +
+		workerCount + "\" gunicorn module.py"
 	gen := PythonStartupScriptGenerator{
 		BindPort: "",
 	}
+
 	// Act
-	command := gen.getCommandFromModule("module.py", "")
-	fmt.Println(command)
-	// Output:
-	// GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-'" gunicorn module.py
+	actual := gen.getCommandFromModule("module.py", "")
+
+	// Assert
+	assert.Equal(t, expected, actual)
 }
 
-func ExamplePythonStartupScriptGenerator_getCommandFromModule_moduleAndPath() {
+func ExamplePythonStartupScriptGenerator_getCommandFromModule_moduleAndPath(t *testing.T) {
 	// Arrange
+	workerCount := getWorkerCount()
+	expected := "GUNICORN_CMD_ARGS=\"--timeout 600 --access-logfile '-' --error-logfile '-' --workders=" +
+		workerCount + " --chdir=/a/b/c\" gunicorn module.py"
 	gen := PythonStartupScriptGenerator{
 		BindPort: "",
 	}
+
 	// Act
-	command := gen.getCommandFromModule("module.py", "/a/b/c")
-	fmt.Println(command)
-	// Output:
-	// GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --chdir=/a/b/c" gunicorn module.py
+	actual := gen.getCommandFromModule("module.py", "/a/b/c")
+
+	// Assert
+	assert.Equal(t, expected, actual)
 }
 
-func ExamplePythonStartupScriptGenerator_getCommandFromModule_moduleAndPathAndHost() {
+func ExamplePythonStartupScriptGenerator_getCommandFromModule_moduleAndPathAndHost(t *testing.T) {
 	// Arrange
+	workerCount := getWorkerCount()
+	expected := "GUNICORN_CMD_ARGS=\"--timeout 600 --access-logfile '-' --error-logfile '-' --workers=" +
+		workerCount + "--bind=0.0.0.0:12345 --chdir=/a/b/c\" gunicorn module.py"
 	gen := PythonStartupScriptGenerator{
 		BindPort: "12345",
 	}
+
 	// Act
-	command := gen.getCommandFromModule("module.py", "/a/b/c")
-	fmt.Println(command)
-	// Output:
-	// GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:12345 --chdir=/a/b/c" gunicorn module.py
+	actual := gen.getCommandFromModule("module.py", "/a/b/c")
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
+
+func Test_GetsWorkerCountBasedOnNumberOfCores(t *testing.T) {
+	// Arrange
+	cpuCount := runtime.NumCPU()
+	expected := strconv.Itoa((2 * cpuCount) + 1)
+
+	// Act
+	actual := getWorkerCount()
+
+	// Assert
+	assert.Equal(t, expected, actual)
 }
