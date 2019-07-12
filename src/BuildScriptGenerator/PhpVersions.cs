780df9d3422d0cf54cc1bb6e31ee223dfac8b227
Dor <dorfire@microsoft.com>
2019-07-12 13:38:57 -0700

    Gen'd consts

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -4,9 +4,9 @@ namespace Microsoft.Oryx.Common
 {
     public static class PhpVersions
     {
-        public const string PhpBuildBaseTag = "20190530.1";
-        public const string PhpRuntimeBaseTag = "20190530.1";
-        public const string ComposerVersion = "1.8.4";
+        public const string PhpBuildBaseTag = "20190710.4";
+        public const string PhpRuntimeBaseTag = "20190710.4";
+        public const string ComposerVersion = "1.8.6";
         public const string ComposerSha384 = "48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5";
         public const string Php73Version = "7.3.7";
         public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
