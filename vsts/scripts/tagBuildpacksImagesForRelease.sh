4febaeccab6ad0801e57b2721972c2f4018f757d
Dor Fire <dorfire@microsoft.com>
2019-07-16 16:35:58 -0700

    Updated pipeline

--- vsts/scripts/tagBuildpacksImagesForRelease.sh
+++ vsts/scripts/tagBuildpacksImagesForRelease.sh
@@ -13,20 +13,6 @@ declare -r prodImageRepo="oryxmcr.azurecr.io/public/oryx"
 buildNumber=$BUILD_BUILDNUMBER
 sourceBranchName=$BUILD_SOURCEBRANCHNAME
 
-echo "Pulling pack-builder image..."
-packBuilderImage=$sourceImageRepo/pack-builder:Oryx-CI.$buildNumber
-docker pull "$packBuilderImage"
-echo "Retagging pack-builder image 'builderNumber'..."
-echo "$prodImageRepo/pack-builder:$buildNumber">>"$outFile"
-docker tag "$packBuilderImage" "$prodImageRepo/pack-builder:$buildNumber"
-
-echo "Pulling pack-stack-base..."
-packStackBaseImage=$sourceImageRepo/pack-stack-base:Oryx-CI.$buildNumber
-docker pull "$packStackBaseImage"
-echo "Retagging pack-stack-base image 'builderNumber'..."
-echo "$prodImageRepo/pack-stack-base:$buildNumber">>"$outFile"
-docker tag "$packStackBaseImage" "$prodImageRepo/pack-stack-base:$buildNumber"
-
 echo "Pulling pack image..."
 packImage=$sourceImageRepo/pack:Oryx-CI.$buildNumber
 docker pull "$packImage"
@@ -35,14 +21,6 @@ echo "$prodImageRepo/pack:$buildNumber">>"$outFile"
 docker tag "$packImage" "$prodImageRepo/pack:$buildNumber"
 
 if [ "$sourceBranchName" == "master" ]; then
-    echo "Retagging pack-builder image with 'stable'..."
-    docker tag "$packBuilderImage" "$prodImageRepo/pack-builder:stable"
-    echo "$prodImageRepo/pack-builder:stable">>"$outFile"
-
-    echo "Retagging pack-stack-base image with 'latest'..."
-    docker tag "$packStackBaseImage" "$prodImageRepo/pack-stack-base:latest"
-    echo "$prodImageRepo/pack-stack-base:latest">>"$outFile"
-
     echo "Retagging pack image with 'stable'..."
     docker tag "$packImage" "$prodImageRepo/pack:stable"
     echo "$prodImageRepo/pack:stable">>"$outFile"
