6872fc1c3e0df8fb5f2c49d42cfcd48f9212ff76
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-07-25 12:31:56 -0700

    update few more tests and scripts

--- tests/Oryx.RuntimeImage.Tests/Node/NodeRuntimeImageOtherTests.cs
+++ tests/Oryx.RuntimeImage.Tests/Node/NodeRuntimeImageOtherTests.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
         [InlineData("10.10", "10.10.0")]
         [InlineData("10.12", "10.12.0")]
         [InlineData("10.14", "10.14.2")]
-        [InlineData("12", "12.5.0")]
+        [InlineData("12", "12.7.0")]
         public void NodeVersionMatchesImageName(string nodeTag, string nodeVersion)
         {
             // Arrange & Act
