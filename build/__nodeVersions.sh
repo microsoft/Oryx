cedd42486c1bd1ee893f30641b4d180d65b42482
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-07-26 12:45:58 -0700

    update yarn to 1.17.3 (#297)

--- build/__nodeVersions.sh
+++ build/__nodeVersions.sh
@@ -1,7 +1,7 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-YARN_VERSION='1.13.0'
-YARN_MINOR_VERSION='1.13'
+YARN_VERSION='1.17.3'
+YARN_MINOR_VERSION='1.17'
 YARN_MAJOR_VERSION='1'
 NODE6_VERSION='6.17.1'
 NODE6_MAJOR_MINOR_VERSION='6.17'
