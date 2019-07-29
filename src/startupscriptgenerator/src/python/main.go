5ba72f1a435d6516ecc2f41919a784860bf5cd7d
Dor <dorfire@microsoft.com>
2019-07-29 08:28:41 -0700

    Added debug flags

--- src/startupscriptgenerator/src/python/main.go
+++ src/startupscriptgenerator/src/python/main.go
@@ -14,20 +14,42 @@ func main() {
 	common.PrintVersionInfo()
 
 	appPathPtr := flag.String("appPath", ".", "The path to the application folder, e.g. '/home/site/wwwroot/'.")
+	
 	manifestDirPtr := common.ManifestDirFlag
-	userStartupCommandPtr := flag.String("userStartupCommand", "", "[Optional] Command that will be executed to start the application up.")
-	defaultAppFilePathPtr := flag.String("defaultApp", "", "[Optional] Path to a default file that will be executed if the entrypoint is not found. Ex: '/opt/defaultsite'")
-	defaultAppModulePtr := flag.String("defaultAppModule", "application:app", "Module of the default application, e.g. 'application:app'.")
-	defaultAppDebugCmdPtr := flag.String("defaultAppDebugCmd", "application.py", "Python command to run if debugging the app in debug mode, e.g. 'application.py start_dev_server'.")
+	
+	userStartupCommandPtr := flag.String("userStartupCommand", "", "[Optional] Command that will be executed " +
+		"to start the application up.")
+	
+	defaultAppFilePathPtr := flag.String("defaultApp", "", "[Optional] Path to a default file that will be " +
+		"executed if the entrypoint is not found. Ex: '/opt/defaultsite'")
+	
+	defaultAppModulePtr := flag.String("defaultAppModule", "application:app", "Module of the default application," +
+		" e.g. 'application:app'.")
+	
+	defaultAppDebugCmdPtr := flag.String("defaultAppDebugCmd", "application.py", "Python command to run if " +
+		"debugging the app in debug mode, e.g. 'application.py start_dev_server'.")
+	
+	debugAdapterPtr := flag.String("debugAdapter", "", "Python debugger adapter. Currently, only 'ptvsd' is " +
+		"supported.")
+
+	debugWaitPtr := flag.Bool("debugWait", false, "Whether the debugger adapter should pause and wait for a " +
+		"client connection before running the app.")
+	
 	virtualEnvNamePtr := flag.String("virtualEnvName", "", "Name of the virtual environment for the app")
-	packagesFolderPtr := flag.String("packagedir", "", "Directory where the python packages were installed, if no virtual environment was used.")
+	
+	packagesFolderPtr := flag.String("packagedir", "", "Directory where the python packages were installed, if " +
+		"no virtual environment was used.")
+	
 	bindPortPtr := flag.String("bindPort", "", "[Optional] Port where the application will bind to. Default is 80")
+	
 	outputPathPtr := flag.String("output", "run.sh", "Path to the script to be generated.")
+	
 	skipVirtualEnvExtraction := flag.Bool(
 		"skipVirtualEnvExtraction",
 		false,
-		"Disables the extraction of the compressed virtual environment file. If used, some external tool will have to extract it - "+
-			"otherwise the application might not work.")
+		"Disables the extraction of the compressed virtual environment file. If used, some external tool will " +
+			"have to extract it - otherwise the application might not work.")
+	
 	flag.Parse()
 
 	fullAppPath := common.GetValidatedFullPath(*appPathPtr)
@@ -44,6 +66,8 @@ func main() {
 		DefaultAppPath:           defaultAppFullPath,
 		DefaultAppModule:         *defaultAppModulePtr,
 		DefaultAppDebugCommand:   *defaultAppDebugCmdPtr,
+		DebugAdapter:             *debugAdapterPtr,
+		DebugWait:                *debugWaitPtr,
 		PackageDirectory:         *packagesFolderPtr,
 		SkipVirtualEnvExtraction: *skipVirtualEnvExtraction,
 		Manifest:                 buildManifest,
