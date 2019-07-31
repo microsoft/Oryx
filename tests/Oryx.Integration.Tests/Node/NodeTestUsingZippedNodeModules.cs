bda0da380e3756ca1ddc0f4f5ac84e5d06ae02eb
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-07-30 19:29:25 -0700

    add some tests and sample apps for angular

--- tests/Oryx.Integration.Tests/Node/NodeTestUsingZippedNodeModules.cs
+++ tests/Oryx.Integration.Tests/Node/NodeTestUsingZippedNodeModules.cs
@@ -76,5 +76,60 @@ namespace Microsoft.Oryx.Integration.Tests
                     Assert.Equal("Hello World from express!", data);
                 });
         }
+
+        [Theory]
+        [MemberData(nameof(TestValueGenerator.GetZipOptions_NodeVersions), MemberType = typeof(TestValueGenerator))]
+        public async Task CanBuildAndRunAngularApp_UsingZippedNodeModules(string compressFormat, string nodeVersion)
+        {
+            // NOTE:
+            // 1. Use intermediate directory(which here is local to container) to avoid errors like
+            //      "tar: node_modules/form-data: file changed as we read it"
+            //    related to zipping files on a folder which is volume mounted.
+            // 2. Use output directory within the container due to 'rsync'
+            //    having issues with volume mounted directories
+
+            // Arrange
+            var appOutputDirPath = Directory.CreateDirectory(Path.Combine(_tempRootDir, Guid.NewGuid().ToString("N")))
+                .FullName;
+            var appOutputDirVolume = DockerVolume.CreateMirror(appOutputDirPath);
+            var appOutputDir = appOutputDirVolume.ContainerDir;
+            var appName = "angular-app";
+            var volume = CreateAppVolume(appName);
+            var appDir = volume.ContainerDir;
+            var runAppScript = new ShellScriptBuilder()
+                .AddCommand($"oryx -appPath {appOutputDir} -bindPort {ContainerPort}")
+                .AddCommand(DefaultStartupFilePath)
+                .ToString();
+            var buildScript = new ShellScriptBuilder()
+                .AddCommand(
+                $"oryx build {appDir} -i /tmp/int -o /tmp/out --platform nodejs " +
+                $"--platform-version {nodeVersion} -p compress_node_modules={compressFormat}")
+                .AddCommand($"cp -rf /tmp/out/* {appOutputDir}")
+                .ToString();
+
+            await EndToEndTestHelper.BuildRunAndAssertAppAsync(
+                appName,
+                _output,
+                new List<DockerVolume> { appOutputDirVolume, volume },
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    buildScript
+                },
+                $"oryxdevmcr.azurecr.io/public/oryx/node-{nodeVersion}",
+                ContainerPort,
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    runAppScript
+                },
+                async (hostPort) =>
+                {
+                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
+                    Assert.Equal("Welcome to test-angular!", data);
+                });
+        }
     }
 }
