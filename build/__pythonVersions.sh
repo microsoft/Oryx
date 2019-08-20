38a067abafa017931ac1cdfd576dba1de3bb466b
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-08-20 07:55:05 -0700

    update python 3.8 to 3.8.0b3 (#316)
    * update python 3.8 to 3.8.0b3
    
    * update python base version

--- build/__pythonVersions.sh
+++ build/__pythonVersions.sh
@@ -1,8 +1,8 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-PYTHON_BASE_TAG='20190802.1'
+PYTHON_BASE_TAG='20190814.1'
 PIP_VERSION='19.1.1'
 PYTHON27_VERSION='2.7.16'
 PYTHON36_VERSION='3.6.9'
 PYTHON37_VERSION='3.7.4'
-PYTHON38_VERSION='3.8.0b2'
+PYTHON38_VERSION='3.8.0b3'
