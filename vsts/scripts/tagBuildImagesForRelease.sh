f1582781c626d51adde090f67b09486c0bdb1b99
Kiran Challa <kichalla@microsoft.com>
2019-08-26 04:54:02 -0700

    updated

--- vsts/scripts/tagBuildImagesForRelease.sh
+++ vsts/scripts/tagBuildImagesForRelease.sh
@@ -9,25 +9,27 @@ set -o pipefail
 buildNumber=$BUILD_BUILDNUMBER
 
 function tagBuildImage() {
-    local prodRegistryLatestTagName="$1"
-    local prodRegistrySpecificTagName="$2"
+    local devRegistryImageName="$1"
+    local prodRegistryLatestTagName="$2"
+    local prodRegistrySpecificTagName="$3"
     local prodRegistryRepoName="oryxmcr.azurecr.io/public/oryx/build"
     sourceBranchName=$BUILD_SOURCEBRANCHNAME
-    buildImageName="oryxdevmcr.azurecr.io/public/oryx/build:Oryx-CI.$buildNumber"
     outFileMCR="$BUILD_ARTIFACTSTAGINGDIRECTORY/drop/images/build-images-mcr.txt"
 
-    echo "Pulling the source image $buildImageName ..."
-    docker pull "$buildImageName" | sed 's/^/     /'
+    echo "Pulling the source image $devRegistryImageName..."
+    docker pull "$devRegistryImageName" | sed 's/^/     /'
 
     echo
     echo "Tagging the source image with tag $prodRegistrySpecificTagName..."
-    docker tag "$buildImageName" "$prodRegistryRepoName:$prodRegistrySpecificTagName"
-    echo "$prodRegistryRepoName:$prodRegistrySpecificTagName">>"$outFileMCR"
+    prodRegistryImageName="$prodRegistryRepoName:$prodRegistrySpecificTagName"
+    docker tag "$devRegistryImageName" "$prodRegistryImageName"
+    echo "$prodRegistryImageName">>"$outFileMCR"
 
     if [ "$sourceBranchName" == "master" ]; then
         echo "Tagging the source image with tag $prodRegistryLatestTagName..."
-        docker tag "$buildImageName" "$prodRegistryRepoName:$prodRegistryLatestTagName"
-        echo "$prodRegistryRepoName:$prodRegistryLatestTagName">>"$outFileMCR"
+        prodRegistryImageName="$prodRegistryRepoName:$prodRegistryLatestTagName"
+        docker tag "$devRegistryImageName" "$prodRegistryRepoName:$prodRegistryLatestTagName"
+        echo "$prodRegistryImageName">>"$outFileMCR"
     else
         echo "Not creating 'latest' tag as source branch is not 'master'. Current branch is $sourceBranchName"
     fi
@@ -35,5 +37,5 @@ function tagBuildImage() {
     echo -------------------------------------------------------------------------------
 }
 
-tagBuildImage "latest" "$buildNumber"
-tagBuildImage "slim" "slim-$buildNumber"
+tagBuildImage "oryxdevmcr.azurecr.io/public/oryx/build:Oryx-CI.$buildNumber" "latest" "$buildNumber"
+tagBuildImage "oryxdevmcr.azurecr.io/public/oryx/build-slim:Oryx-CI.$buildNumber" "slim" "slim-$buildNumber"
