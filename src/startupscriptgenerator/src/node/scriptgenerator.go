212d74b8b3b64a959215197eedd4af82e4b072ca
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-08-27 07:37:47 -0700

    update test and node_path fix for global node_modules

--- src/startupscriptgenerator/src/node/scriptgenerator.go
+++ src/startupscriptgenerator/src/node/scriptgenerator.go
@@ -98,6 +98,7 @@ func (gen *NodeStartupScriptGenerator) GenerateEntrypointScript() string {
 	scriptBuilder.WriteString("#!/bin/sh\n")
 	scriptBuilder.WriteString("\n# Enter the source directory to make sure the script runs where the user expects\n")
 	scriptBuilder.WriteString("cd \"" + gen.SourcePath + "\"\n\n")
+	scriptBuilder.WriteString("export NODE_PATH=$(npm root --quiet -g):$NODE_PATH\n")
 
 	// Expose the port so that a custom command can use it if needed.
 	common.SetEnvironmentVariableInScript(&scriptBuilder, "PORT", gen.BindPort, DefaultBindPort)
---
+++ tests/SampleApps/nodejs/linxnodeexpress-appinsights/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var appInsights = require('applicationinsights');
+
+console.log(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+console.log(process.env.APPLICATIONINSIGHTSAGENT_EXTENSION_ENABLED)
+
+if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY && process.env.APPLICATIONINSIGHTSAGENT_EXTENSION_ENABLED.toLowerCase() === "true") {
+	console.log("hello world here")
+			appInsights
+				.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+				.setSendLiveMetrics(true)
+				.start();
+}
+
+let client = appInsights.defaultClient;
+client.trackTrace({message: "trace message"});
+client.trackMetric({name: "custom metric", value: 3});
+
+app.get('/', function (req, res) {
+  res.send('Hello World from express!');
+});
+
+var port = process.env.PORT || 80;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
