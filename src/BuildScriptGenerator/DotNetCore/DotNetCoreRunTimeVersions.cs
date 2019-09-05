6b6d9b5663aa392edc744fc13d3269f2e27feb87
Kiran Challa <kichalla@microsoft.com>
2019-09-05 12:17:47 -0700

    Upgraded .NET Core 3 from Preview8 to Preview9

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
@@ -10,6 +10,6 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string NetCoreApp21 = "2.1.12";
         public const string NetCoreApp22 = "2.2.6";
         public const string NetCoreApp30 = "3.0.0";
-        public const string NetCoreApp30PreviewName = "3.0.0-preview8-28405-07";
+        public const string NetCoreApp30PreviewName = "3.0.0-preview9-19423-09";
     }
 }
