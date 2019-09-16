47f77254674918bd8d66331351c9aedf177e8ab6
Kiran Challa <kichalla@microsoft.com>
2019-09-16 13:40:41 -0700

    Upgraded .NET Core to latest versions

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
@@ -7,9 +7,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string NetCoreApp10 = "1.0.16";
         public const string NetCoreApp11 = "1.1.14";
         public const string NetCoreApp20 = "2.0.9";
-        public const string NetCoreApp21 = "2.1.12";
-        public const string NetCoreApp22 = "2.2.6";
+        public const string NetCoreApp21 = "2.1.13";
+        public const string NetCoreApp22 = "2.2.7";
         public const string NetCoreApp30 = "3.0.0";
-        public const string NetCoreApp30PreviewName = "3.0.0-preview9-19423-09";
+        public const string NetCoreApp30PreviewName = "3.0.0-rc1-19456-20";
     }
 }
