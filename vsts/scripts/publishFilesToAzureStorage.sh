ca6feccd6b626d1e0d3ca182251ee924aa4e331e
Kiran Challa <kichalla@microsoft.com>
2019-09-18 12:50:06 -0700

    updated

--- vsts/scripts/publishFilesToAzureStorage.sh
+++ vsts/scripts/publishFilesToAzureStorage.sh
@@ -11,11 +11,13 @@ storageAccount="$1"
 uploadFiles() {
     local platform="$1"
     local artifactsDir="$BUILD_ARTIFACTSTAGINGDIRECTORY/drop/platformSdks/$platform"
-    if ls "$artifactsDir/$platform-*.tar.gz" 1> /dev/null 2>&1; then
+    if ls "$artifactsDir/$platform-*.tar.gz"; then
+        echo "Uploading files under $artifactsDir..."
         az storage blob upload-batch \
             -s "$artifactsDir" \
             -d $platform \
-            --account-name $storageAccount
+            --account-name $storageAccount \
+            --pattern '*.*'
     fi
 }
 
