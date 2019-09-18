e45934299947b4fe6b70465d9de2aa46f2ac7fdd
Kiran Challa <kichalla@microsoft.com>
2019-09-18 15:39:29 -0700

    Upgraded PHP versions

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -8,12 +8,12 @@ namespace Microsoft.Oryx.Common
         public const string PhpRuntimeBaseTag = "20190917.1";
         public const string ComposerVersion = "1.8.6";
         public const string ComposerSha384 = "a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1";
-        public const string Php73Version = "7.3.5";
+        public const string Php73Version = "7.3.9";
         public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
-        public const string Php73TarSha256 = "e1011838a46fd4a195c8453b333916622d7ff5bce4aca2d9d99afac142db2472";
-        public const string Php72Version = "7.2.18";
+        public const string Php73TarSha256 = "4007f24a39822bef2805b75c625551d30be9eeed329d52eb0838fa5c1b91c1fd";
+        public const string Php72Version = "7.2.22";
         public const string Php72Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 B1B44D8F021E4E2D6021E995DC9FF8D3EE5AF27F";
-        public const string Php72TarSha256 = "9970dbb3ab1298c9e6aac54bebfa841c8ad14b18eead65594a68fa841364cb8d";
+        public const string Php72TarSha256 = "eb597fcf8dc0a6211a42a6346de4f63ee166829a6df6d8ed767fe14be8d1c3a3";
         public const string Php70Version = "7.0.33";
         public const string Php70Keys = "1A4E8B7277C42E53DBA9C7B9BCAA30EA9C0D5763 6E4F6AB321FDC07F2C332E3AC2BF0BC433CFC8B3";
         public const string Php70TarSha256 = "ab8c5be6e32b1f8d032909dedaaaa4bbb1a209e519abb01a52ce3914f9a13d96";
