d1b08877abe7be8fbd9c073c41f5ecea6d97d57e
Kiran Challa <kichalla@users.noreply.github.com>
2019-10-16 14:36:04 -0700

    Added .NET Core 3.1 preview1 version (#391)

--- tests/BuildScriptGenerator.Tests/DotnetCore/DotnetCoreLanguageDetectorTest.cs
+++ tests/BuildScriptGenerator.Tests/DotnetCore/DotnetCoreLanguageDetectorTest.cs
@@ -99,6 +99,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Tests.DotNetCore
         [InlineData("netcoreapp2.1", DotNetCoreRunTimeVersions.NetCoreApp21)]
         [InlineData("netcoreapp2.2", DotNetCoreRunTimeVersions.NetCoreApp22)]
         [InlineData("netcoreapp3.0", DotNetCoreRunTimeVersions.NetCoreApp30)]
+        [InlineData("netcoreapp3.1", DotNetCoreRunTimeVersions.NetCoreApp31)]
         public void Detect_ReturnsExpectedLanguageVersion_ForTargetFrameworkVersions(
             string netCoreAppVersion,
             string expectedSdkVersion)
@@ -253,6 +254,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Tests.DotNetCore
                 DotNetCoreRunTimeVersions.NetCoreApp21,
                 DotNetCoreRunTimeVersions.NetCoreApp22,
                 DotNetCoreRunTimeVersions.NetCoreApp30,
+                DotNetCoreRunTimeVersions.NetCoreApp31,
             };
         }
 
