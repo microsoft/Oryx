bfee6330b98338273c341b6646cd6a662ef4b17c
Kiran Challa <kichalla@users.noreply.github.com>
2019-10-25 13:53:55 -0700

    Added back support to log build script output and with extra info as properties (#401)

--- src/BuildScriptGeneratorCli/Commands/BuildCommand.cs
+++ src/BuildScriptGeneratorCli/Commands/BuildCommand.cs
@@ -197,11 +197,13 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
                 console.WriteLine($"Build script content:\n{scriptContent}");
             }
 
+            var oryxVersion = Program.GetVersion();
+            var oryxReleaseTagName = Program.GetMetadataValue("RELEASE_TAG_NAME");
             var buildEventProps = new Dictionary<string, string>()
             {
-                { "oryxVersion", Program.GetVersion() },
+                { "oryxVersion", oryxVersion },
                 { "oryxCommitId", Program.GetMetadataValue("GitCommit") },
-                { "oryxReleaseTagName", Program.GetMetadataValue("RELEASE_TAG_NAME") },
+                { "oryxReleaseTagName", oryxReleaseTagName },
                 {
                     "oryxCommandLine",
                     string.Join(
@@ -275,7 +277,16 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
 
             if (exitCode != ProcessConstants.ExitSuccess)
             {
-                logger.LogError("Build script exited with {exitCode}", exitCode);
+                logger.LogLongMessage(
+                    LogLevel.Error,
+                    header: "Error running build script",
+                    buildScriptOutput.ToString(),
+                    new Dictionary<string, object>
+                    {
+                        ["buildExitCode"] = exitCode,
+                        ["oryxVersion"] = oryxVersion,
+                        ["oryxReleaseTagName"] = oryxReleaseTagName,
+                    });
                 return exitCode;
             }
 
