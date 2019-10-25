a8f1d4301273adeeac3108642dc937fc4eeb2ee3
Kiran Challa <kichalla@microsoft.com>
2019-10-25 14:26:20 -0700

    updated

--- tests/BuildScriptGenerator.Tests/Node/NodeScriptGeneratorOptionsSetupTest.cs
+++ tests/BuildScriptGenerator.Tests/Node/NodeScriptGeneratorOptionsSetupTest.cs
@@ -23,7 +23,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Tests.Node
             optionsSetup.Configure(options);
 
             // Assert
-            Assert.Equal(NodeVersions.Node10Version, options.NodeJsDefaultVersion);
+            Assert.Equal(NodeConstants.NodeLtsVersion, options.NodeJsDefaultVersion);
         }
 
         [Fact]
