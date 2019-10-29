09c24d60439a97ef448e2876e3ac1624b8ffdcb2
Dor Fire <dorfire@microsoft.com>
2019-10-29 16:29:31 -0700

    Fixed GD opt

--- images/build/php/prereqs/build.sh
+++ images/build/php/prereqs/build.sh
@@ -100,8 +100,8 @@ fi
 		--with-config-file-scan-dir="$PHP_INI_DIR/conf.d" \
 		--enable-option-checking=fatal \
 		--with-mhash \
+		--with-gd \
 		--enable-ftp \
-		--enable-gd \
 		--enable-exif \
 		--enable-mbstring \
 		--enable-mysqlnd \
@@ -122,7 +122,7 @@ make clean;
 
 # https://github.com/docker-library/php/issues/692 (copy default example "php.ini" files somewhere easily discoverable)
 cp -v php.ini-* "$PHP_INI_DIR/";
-cp "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+cp "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini";
 
 cd /;
 /php/docker-php-source.sh delete;
