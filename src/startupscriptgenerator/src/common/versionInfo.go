f205f06c99d16612903420878fa432afd9cec823
Kiran Challa <kichalla@microsoft.com>
2019-11-05 06:15:33 -0800

    updated

--- src/startupscriptgenerator/src/common/versionInfo.go
+++ src/startupscriptgenerator/src/common/versionInfo.go
@@ -8,11 +8,12 @@ package common
 // NOTE: The 'BuildNumber' and 'Commit' variables here are modified during build time. Look at
 // the 'go build' command in file 'startupscriptgenerator/build.sh'
 var (
-	BuildNumber = "unspecified"
-	Version     = "0.2" + "." + BuildNumber
-	Commit      = "unspecified"
+	BuildNumber    = "unspecified"
+	Version        = "0.2" + "." + BuildNumber
+	Commit         = "unspecified"
+	ReleaseTagName = "unspecified"
 )
 
 func PrintVersionInfo() {
-	println("Oryx Version : " + Version + ", Commit: " + Commit + "\n")
+	fmt.Printf("Oryx Version: %s, Commit: %s, ReleaseTagName: %s\n", Version, Commit, ReleaseTagName)
 }
