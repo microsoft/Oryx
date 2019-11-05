1fac9e3b9133321aa8511319b8f19a4f5a08ad8c
Kiran Challa <kichalla@microsoft.com>
2019-11-04 20:19:07 -0800

    Updated language detector to detect 3.1 apps too

--- src/BuildScriptGenerator/DotNetCore/DotnetCoreLanguageDetector.cs
+++ src/BuildScriptGenerator/DotNetCore/DotnetCoreLanguageDetector.cs
@@ -112,6 +112,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
 
                 case DotNetCoreConstants.NetCoreApp30:
                     return DotNetCoreRunTimeVersions.NetCoreApp30;
+
+                case DotNetCoreConstants.NetCoreApp31:
+                    return DotNetCoreRunTimeVersions.NetCoreApp31;
             }
 
             return null;
