c6771ea3672372a815ec5303bc262308537157b6
Kiran Challa <kichalla@microsoft.com>
2019-11-13 10:33:05 -0800

    Addressed feedback

--- images/build/createFlattenedDotNetCoreInstall.sh
+++ images/build/createFlattenedDotNetCoreInstall.sh
@@ -4,7 +4,7 @@
 # Licensed under the MIT license.
 # --------------------------------------------------------------------------------------------
 
-set -ex
+set -e
 
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
@@ -16,11 +16,42 @@ splitSdksDir="/opt/dotnet/sdks"
 allSdksDir="/opt/dotnet/all"
 mkdir -p "$allSdksDir"
 
-# Copy latest muxer and license files
-cp -f "$splitSdksDir/3/dotnet" "$allSdksDir"
-cp -f "$splitSdksDir/3/LICENSE.txt" "$allSdksDir"
-cp -f "$splitSdksDir/3/ThirdPartyNotices.txt" "$allSdksDir"
+# Get the latest (not 'lts') version of .NET Core SDK so that we can use the 'dotnet.exe' of that version
+# as the muxer to switch between different versions of SDKs
+currentMaxMajorVersion="0"
+currentMaxMinorVersion="0"
+cd "$splitSdksDir"
+integerRegex='^[0-9]+$'
+for sdkDir in "$splitSdksDir"/*/
+do
+    sdkDir=${sdkDir%*/}
+    sdkDirName=${sdkDir##*/}
 
+    IFS='.' read -ra SDK_VERSION_PARTS <<< "$sdkDirName"
+    majorVersion=${SDK_VERSION_PARTS[0]}
+    minorVersion=${SDK_VERSION_PARTS[1]:-0}
+
+    # Ignore strings like 'lts'
+    if ! [[ $majorVersion =~ $integerRegex ]] ; then
+        continue
+    fi
+
+    if [ "$majorVersion" -gt "$currentMaxMajorVersion" ] || \
+        ( [ "$majorVersion" -gt "$currentMaxMajorVersion" ] && \
+            [ "$minorVersion" -gt "$currentMaxMinorVersion" ] ); then
+        currentMaxMajorVersion=$majorVersion
+        currentMaxMinorVersion=$minorVersion
+    fi
+done
+
+muxerVersion="$currentMaxMajorVersion.$currentMaxMinorVersion"
+echo "Using SDK version '$muxerVersion' for getting the muxer dotnet.exe..."
+cp -f "$splitSdksDir/$muxerVersion/dotnet" "$allSdksDir"
+cp -f "$splitSdksDir/$muxerVersion/LICENSE.txt" "$allSdksDir"
+cp -f "$splitSdksDir/$muxerVersion/ThirdPartyNotices.txt" "$allSdksDir"
+
+# Creates a structure which is how a typical .NET Core SDK install needs to be. Here we create only symlinks
+# to existing split structure of SDKs.
 function createLinks() {
     local sdkVersion="$1"
     local runtimeVersion="$2"
