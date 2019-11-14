05db6519ea6340bae6ab16aa66bd89170a0f90e0
Kiran Challa <kichalla@microsoft.com>
2019-11-14 14:20:43 -0800

    Updated base image tags

--- build/__pythonVersions.sh
+++ build/__pythonVersions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-PYTHON_BASE_TAG='20191112.4'
+PYTHON_BASE_TAG='20191114.2'
 PIP_VERSION='19.3.1'
 PYTHON27_VERSION='2.7.17'
 PYTHON36_VERSION='3.6.9'
