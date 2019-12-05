b323830fd1e570a3a92b782af75135e51d9dfbf6
Kiran Challa <kichalla@microsoft.com>
2019-12-04 17:14:55 -0800

    updated

--- build/__pythonVersions.sh
+++ build/__pythonVersions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-PYTHON_BASE_TAG='20191115.2'
+PYTHON_BASE_TAG='20191205.1'
 PIP_VERSION='19.3.1'
 PYTHON27_VERSION='2.7.17'
 PYTHON36_VERSION='3.6.9'
