456fd40c32fcb693fb0ee3db29b86ae13d32b89c
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-12-16 12:56:34 -0800

    fix indentation

--- images/runtime/php-fpm/buildRunTimeImageBases_Init.sh
+++ images/runtime/php-fpm/buildRunTimeImageBases_Init.sh
@@ -11,14 +11,14 @@ docker build -t $baseImage -f "$CURRENT_DIR/runbase.Dockerfile" .
 
 for PHP_VERSION in "${VERSION_ARRAY[@]}"
 do
-	IFS='.' read -ra SPLIT_VERSION <<< "$PHP_VERSION"
-	VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
+    IFS='.' read -ra SPLIT_VERSION <<< "$PHP_VERSION"
+    VERSION_DIRECTORY="${SPLIT_VERSION[0]}.${SPLIT_VERSION[1]}"
 
-	PHP_IMAGE_NAME="php-fpm-$VERSION_DIRECTORY"
+    PHP_IMAGE_NAME="php-fpm-$VERSION_DIRECTORY"
     cd "$CURRENT_DIR/$VERSION_DIRECTORY/"
 
     echo
     echo "Building php image '$PHP_IMAGE_NAME'..."
     echo
-	docker build -t $PHP_IMAGE_NAME -f "$VERSION_DIRECTORY.Dockerfile" .
+    docker build -t $PHP_IMAGE_NAME -f "$VERSION_DIRECTORY.Dockerfile" .
 done
