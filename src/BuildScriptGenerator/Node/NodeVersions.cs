079ca9385aec4a414201bf8aa0bcd267b7a86f6f
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2019-12-19 14:26:58 -0800

    update php and node runtime images to latest (#450)
    * update node and php runtimes to latest
    
    * increase timeout
    
    * update base image tags for php and node images
    
    * update testvalue generator for new node versions
    
    * fix unit tests
    
    * update dockerfiles for php 7.2 and php 7.3
    
    * update image tags to fix unit tests

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,14 +4,14 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20191205.1";
-        public const string YarnCacheBaseTag = "20191205.1";
+        public const string NodeRuntimeBaseTag = "20191219.3";
+        public const string YarnCacheBaseTag = "20191219.3";
         public const string YarnVersion = "1.17.3";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
-        public const string Node8Version = "8.16.2";
-        public const string Node10Version = "10.17.0";
-        public const string Node12Version = "12.13.0";
+        public const string Node8Version = "8.17.0";
+        public const string Node10Version = "10.18.0";
+        public const string Node12Version = "12.14.0";
     }
 }
