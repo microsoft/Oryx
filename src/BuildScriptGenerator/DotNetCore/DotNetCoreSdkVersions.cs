a454ee5c513ac88b57de66edb43aa3a088b4ad92
Kiran Challa <kichalla@users.noreply.github.com>
2020-01-22 02:35:00 -0800

    Fixed php composer SHA and upgrade all platform versions to their latest (#459)

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
@@ -6,13 +6,13 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
     {
         public const string DotNetCore11SdkVersion = "1.1.14";
         public const string DotNetCore11SdkSha512 = "E36AA4B278B936E1E2989B5E556F4EC3AEF2CB1EBB5ABD1BD6011D511873FC3E72101D035CF4DAF1563C7A3C43C193863D38CBF24288D375DD143AB42AFAE524";
-        public const string DotNetCore21SdkVersion = "2.1.802";
-        public const string DotNetCore21SdkSha512 = "8601EC68FA8978C5A08A696CC69F5E0AA9DABF274BADE40E2FD9DED2E563F2CC7A1CB418A95C9B52E1FA9B6C4A30993BE68CB3B6BBDA4598C57B37EC9CE992AD";
+        public const string DotNetCore21SdkVersion = "2.1.803";
+        public const string DotNetCore21SdkSha512 = "57d48d6ca1bd92ac348dc05220d984811c0cf005774d7afdfbbf125a842acb0a26572146ed25a7eb26f4e0404fe840b70d1e7ec1fb7c9a5c6cfe81fefc41b363";
         public const string DotNetCore22SdkVersion = "2.2.402";
         public const string DotNetCore22SdkSha512 = "81937DE0874EE837E3B42E36D1CF9E04BD9DEFF6BA60D0162AE7CA9336A78F733E624136D27F559728DF3F681A72A669869BF91D02DB47C5331398C0CFDA9B44";
-        public const string DotNetCore30SdkVersion = "3.0.101";
-        public const string DotNetCore30SdkSha512 = "8cd6070c9df1272ac3f2a0e9bd8c1fc2cfef38907fa2e3c81384f005168f45036b5904b4f731cbe37d2b3fe558f560008c481939093b0d08ef0f1778ca97f186";
-        public const string DotNetCore31SdkVersion = "3.1.100";
-        public const string DotNetCore31SdkSha512 = "5217ae1441089a71103694be8dd5bb3437680f00e263ad28317665d819a92338a27466e7d7a2b1f6b74367dd314128db345fa8fff6e90d0c966dea7a9a43bd21";
+        public const string DotNetCore30SdkVersion = "3.0.102";
+        public const string DotNetCore30SdkSha512 = "77bc287d9c20630976ac4c0736192ba4899154c9e7cc5b87bc9d94d5d8abafdd832cfe8f385b6ba584c702d9261566109df15ab46b0d62bd218d950d3b47893e";
+        public const string DotNetCore31SdkVersion = "3.1.101";
+        public const string DotNetCore31SdkSha512 = "eeee75323be762c329176d5856ec2ecfd16f06607965614df006730ed648a5b5d12ac7fd1942fe37cfc97e3013e796ef278e7c7bc4f32b8680585c4884a8a6a1";
     }
 }
--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,14 +4,14 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20191219.3";
-        public const string YarnCacheBaseTag = "20191219.3";
+        public const string NodeRuntimeBaseTag = "20200121.9";
+        public const string YarnCacheBaseTag = "20200121.9";
         public const string YarnVersion = "1.17.3";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
-        public const string Node10Version = "10.18.0";
-        public const string Node12Version = "12.14.0";
+        public const string Node10Version = "10.18.1";
+        public const string Node12Version = "12.14.1";
     }
 }
