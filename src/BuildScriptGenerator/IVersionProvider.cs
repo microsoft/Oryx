0a175b1f2a0262877356827a91b8d608ad8950e7
Kiran Challa <kichalla@microsoft.com>
2020-02-04 15:42:54 -0800

    New version provider

---
+++ src/BuildScriptGenerator/IVersionProvider.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Oryx.BuildScriptGenerator
+{
+    public interface IVersionProvider
+    {
+        IEnumerable<VersionInfo> GetSupportedVersions(string platformName);
+    }
+
+    public class VersionInfo
+    {
+        public VersionSource VersionSource { get; set; }
+
+        public string Version { get; set; }
+    }
+
+    public enum VersionSource
+    {
+        Disk,
+        AzureStorage
+    }
+
+    public class FromDiskVersionProvider : IVersionProvider
+    {
+        public IEnumerable<VersionInfo> GetSupportedVersions(string platformName)
+        {
+           return VersionProviderHelper.GetVersionsFromDirectory()
+        }
+    }
+}
