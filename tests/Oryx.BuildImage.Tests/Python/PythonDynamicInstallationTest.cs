2ae06a5b0b276b1b142c3950837dd598c3f31164
Kiran Challa <kichalla@microsoft.com>
2020-02-25 09:29:56 -0800

    updated

--- tests/Oryx.BuildImage.Tests/Python/PythonDynamicInstallationTest.cs
+++ tests/Oryx.BuildImage.Tests/Python/PythonDynamicInstallationTest.cs
@@ -27,7 +27,6 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 var imageTestHelper = new ImageTestHelper();
                 var data = new TheoryData<string>();
                 data.Add(imageTestHelper.GetTestSlimBuildImage());
-                data.Add(imageTestHelper.GetAzureFunctionsJamStackBuildImage());
                 data.Add(imageTestHelper.GetGitHubActionsBuildImage());
                 return data;
             }
