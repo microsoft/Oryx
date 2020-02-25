0a3c61e1078e0275d6f097ba404f0a7b5f2fcc01
Kiran Challa <kichalla@microsoft.com>
2020-02-25 14:42:26 -0800

    updated

--- vsts/scripts/publishFilesToAzureStorage.sh
+++ vsts/scripts/publishFilesToAzureStorage.sh
@@ -16,7 +16,7 @@ uploadFiles() {
         return
     fi
 
-    allFiles=$(find $artifactsDir -type f -name '*.tar.gz')
+    allFiles=$(find $artifactsDir -type f -name '*.tar.gz' -name '*.txt')
     for fileToUpload in $allFiles
     do
         fileName=$(basename $fileToUpload)
