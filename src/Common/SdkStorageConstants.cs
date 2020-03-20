91e5c40a7d51b2bbc4f217583e4948b3554a7960
Kiran Challa <kichalla@microsoft.com>
2020-03-20 15:02:37 -0700

    Fixed constants file

--- src/Common/SdkStorageConstants.cs
+++ src/Common/SdkStorageConstants.cs
@@ -4,6 +4,7 @@ namespace Microsoft.Oryx.Common
 {
     public static class SdkStorageConstants
     {
+        public const string EnableDynamicInstallKey = "ENABLE_DYNAMIC_INSTALL";
         public const string SdkStorageBaseUrlKeyName = "ORYX_SDK_STORAGE_BASE_URL";
         public const string DevSdkStorageBaseUrl = "https://oryxsdksdev.blob.core.windows.net";
         public const string ProdSdkStorageBaseUrl = "https://oryxsdks.blob.core.windows.net";
