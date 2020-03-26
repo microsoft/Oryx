9ba27f876f6d4aad7fb02382f6fe9ca8aab5504d
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-03-26 12:46:26 -0700

    add an autoreload sample app integration test for php

---
+++ tests/SampleApps/php/greetings/README.md
@@ -0,0 +1,4 @@
+# greetings
+This is a repo for hello world stuff.
+
+This is the composer branch.
---
+++ tests/SampleApps/php/greetings/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// Autoload files using Composer autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+use HelloWorld\Greetings;
+
+echo Greetings::sayHelloWorld();
+echo "<br>oryx oryx oryx";
