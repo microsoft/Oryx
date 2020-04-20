574203296eb7fb7b93b3ad9b90fdf4292c29d7c7
Zixuan Qian <zqian@microsoft.com>
2020-04-20 15:22:34 -0700

    Update SDK storage Urls to production (#566)
    * change sdk storage urls to prod
    
    * apply dns url to cdn

--- src/Common/SdkStorageConstants.cs
+++ src/Common/SdkStorageConstants.cs
@@ -7,8 +7,8 @@ namespace Microsoft.Oryx.Common
         public const string EnableDynamicInstallKey = "ENABLE_DYNAMIC_INSTALL";
         public const string SdkStorageBaseUrlKeyName = "ORYX_SDK_STORAGE_BASE_URL";
         public const string DevSdkStorageBaseUrl = "https://oryxsdksdev.blob.core.windows.net";
-        public const string ProdSdkStorageBaseUrl = "https://oryxsdks.blob.core.windows.net";
-        public const string ProdSdkCdnStorageBaseUrl = "https://oryxsdk-cdn.azureedge.net";
+        public const string ProdSdkStorageBaseUrl = "https://oryxsdksprod.blob.core.windows.net";
+        public const string ProdSdkCdnStorageBaseUrl = "https://oryx-cdn.microsoft.io";
         public const string DefaultVersionFileName = "defaultVersion.txt";
         public const string VersionsToBuildFileName = "versionsToBuild.txt";
         public const string ContainerMetadataUrlFormat = "{0}/{1}?restype=container&comp=list&include=metadata";
