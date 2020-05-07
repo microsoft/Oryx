107384ae005efd58f0fe1687f73a7d5d39274473
Zixuan Qian <zqian@microsoft.com>
2020-05-07 14:31:44 -0700

    Apply multi workers strategy to Gunicorn (#565)
    * apply multi workers strategy to gunicorn

--- src/Common/ExtVarNames.cs
+++ src/Common/ExtVarNames.cs
@@ -12,5 +12,6 @@ namespace Microsoft.Oryx.Common
         public const string UserAppInsightsKeyEnv = "APPINSIGHTS_INSTRUMENTATIONKEY";
         public const string UserAppInsightsEnableEnv = "ApplicationInsightsAgent_EXTENSION_VERSION";
         public const string UserAppInsightsConnectionStringEnv = "APPLICATIONINSIGHTS_CONNECTION_STRING";
+        public const string PythonEnableGunicornMultiWorkersEnvVarName = "PYTHON_ENABLE_GUNICORN_MULTIWORKERS";
     }
 }
