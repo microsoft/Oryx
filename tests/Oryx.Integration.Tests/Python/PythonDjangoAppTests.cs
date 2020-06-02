b22f14881c7f101769e7e4536f80d7f0f7568b6c
Kiran Challa <kichalla@microsoft.com>
2020-06-02 08:54:45 -0700

    updated

--- tests/Oryx.Integration.Tests/Python/PythonDjangoAppTests.cs
+++ tests/Oryx.Integration.Tests/Python/PythonDjangoAppTests.cs
@@ -131,7 +131,7 @@ namespace Microsoft.Oryx.Integration.Tests
 
             var buildScript = new ShellScriptBuilder()
                 .AddCommand($"export {BuildScriptGeneratorCli.SettingsKeys.EnableMultiPlatformBuild}=true")
-                .AddBuildCommand($"{appDir} --platform {PythonConstants.PlatformName} --platform {PythonVersions.Python37Version}")
+                .AddBuildCommand($"{appDir} --platform {PythonConstants.PlatformName} --platform-version {PythonVersions.Python37Version}")
                 .ToString();
 
             var runAppScript = new ShellScriptBuilder()
