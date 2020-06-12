3e32361dc8088bb71052028dc310bc8b2560e9ca
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-06-12 11:26:31 -0700

    publish base images to pme staging repository (#627)
    * release base images to pme prod acr
    
    * update release base images to pme prod acr
    
    * add some more base images to release into pme prod acr
    
    * update PR
    
    * remove few more rebase conflict
    
    * remove yarn-cache buser image temporarily
    
    * remove yarn-cache buser image temporarily

---
+++ vsts/scripts/pushImagesToRegistry.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+# --------------------------------------------------------------------------------------------
+
+set -euo pipefail
+# $1 > file that has all the tags 
+# e.g. /data/agent/_work/206/a/drop/images/yarn-cache-build/oryxmcr-base-images-mcr.txt
+# e.g. /data/agent/_work/206/a/drop/images/yarn-cache-build/oryxmcr-base-images-mcr.txt
+
+busterBaseImagesFile="$1"-buster-base-images-mcr.txt
+stretchBaseImagesFile="$1"-stretch-base-images-mcr.txt
+
+echo "base image tag file is in directory: $1"
+
+if [ -f "$busterBaseImagesFile" ]; then
+    echo "$busterBaseImagesFile exists. pushing buster image tags ..."
+    while read imageName; do
+      # read the tags from file
+      echo "Pushing image $imageName ..."
+      docker push "$imageName"
+    done <"$busterBaseImagesFile"
+fi
+
+if [ -f "$stretchBaseImagesFile" ]; then
+    echo "$stretchBaseImagesFile exists. pushing stretch image tags ..."
+    while read imageName; do
+      # read the tags from file
+      echo "Pushing image $imageName ..."
+      docker push "$imageName"
+    done <"$stretchBaseImagesFile"
+fi
+
