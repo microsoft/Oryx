f8f3a3f5284026bdc4e65f476b9623cd1848b28a
Kiran Challa <kichalla@microsoft.com>
2020-06-19 12:15:08 -0700

    Addressed feedback

--- vsts/scripts/createNuGetPackages.ps1
+++ vsts/scripts/createNuGetPackages.ps1
@@ -3,6 +3,11 @@
 # Licensed under the MIT license.
 # --------------------------------------------------------------------------------------------
 
+# In this script, we extract already created nuget packages, which were created by dotnet cli automatically
+# but do not have signed dlls within them.
+# So we extract the package and update it's contents with signed dlls and repackage it.
+# The steps which signs the nuget package itself would come after this step in DevOps pipeline.
+
 function DeleteItem($pathToRemove) {
     if (Test-Path -Path $pathToRemove) {
         Remove-Item -Recurse -Force -Path "$pathToRemove"
@@ -13,6 +18,7 @@ $repoRoot="$PSScriptRoot\..\.."
 $artifactsPackagesDir="$repoRoot\artifacts\packages"
 . $repoRoot\build\__detectorNugetPackagesVersions.ps1
 $detectorName="Microsoft.Oryx.Detector"
+$commonProjectAssemblyName="Microsoft.Oryx.Common"
 cd "$artifactsPackagesDir"
 
 # Delete any existing directory and zip file. Could have been from an earlier build.
@@ -23,8 +29,12 @@ Expand-Archive -Path "$detectorName.zip" -DestinationPath "$detectorName"
 DeleteItem "$detectorName.zip"
 
 Copy-Item `
-    -Path "$repoRoot\src\Detector\bin\Release\netstandard2.0\$detectorName.dll" `
-    -Destination "$detectorName\lib\netstandard2.0\$detectorName.dll" `
+    -Path "$repoRoot\src\Detector\bin\Release\netcoreapp2.1\$detectorName.dll" `
+    -Destination "$detectorName\lib\netcoreapp2.1\$detectorName.dll" `
+    -Force
+Copy-Item `
+    -Path "$repoRoot\src\Detector\bin\Release\netcoreapp2.1\$commonProjectAssemblyName.dll" `
+    -Destination "$detectorName\lib\netcoreapp2.1\$commonProjectAssemblyName.dll" `
     -Force
 
 Compress-Archive -Path "$detectorName\*" -DestinationPath "$detectorName.zip"
