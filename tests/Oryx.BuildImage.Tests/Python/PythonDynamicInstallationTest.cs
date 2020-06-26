ba0af29a8d716510fc90866acb5d0394d11fd4ac
Zixuan Qian <zqian@microsoft.com>
2020-06-25 23:23:19 -0700

    Enable dynamic install for Python preview version (#654)

--- tests/Oryx.BuildImage.Tests/Python/PythonDynamicInstallationTest.cs
+++ tests/Oryx.BuildImage.Tests/Python/PythonDynamicInstallationTest.cs
@@ -22,24 +22,25 @@ namespace Microsoft.Oryx.BuildImage.Tests
         {
         }
 
-        public static TheoryData<string> ImageNameData
+        public static TheoryData<string, string> ImageNameData
         {
             get
             {
                 var imageTestHelper = new ImageTestHelper();
-                var data = new TheoryData<string>();
-                data.Add(imageTestHelper.GetLtsVersionsBuildImage());
-                data.Add(imageTestHelper.GetGitHubActionsBuildImage());
+                var data = new TheoryData<string, string>();
+                data.Add(imageTestHelper.GetLtsVersionsBuildImage(), "3.8.1");
+                data.Add(imageTestHelper.GetLtsVersionsBuildImage(), "3.8.3");
+                data.Add(imageTestHelper.GetGitHubActionsBuildImage(), "3.8.1");
+                data.Add(imageTestHelper.GetGitHubActionsBuildImage(), "3.8.3");
                 return data;
             }
         }
 
         [Theory]
         [MemberData(nameof(ImageNameData))]
-        public void GeneratesScript_AndBuilds(string imageName)
+        public void GeneratesScript_AndBuildsPython(string imageName, string version)
         {
             // Arrange
-            var version = "3.8.1";
             var installationDir = $"{BuildScriptGenerator.Constants.TemporaryInstallationDirectoryRoot}/python/{version}";
             var appName = "flask-app";
             var volume = CreateSampleAppVolume(appName);
@@ -76,6 +77,50 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 },
                 result.GetDebugInfo());
         }
+
+        [Theory]
+        [InlineData("3.8.0b3")]
+        [InlineData("3.9.0b1")]
+        public void GeneratesScript_AndBuildsPythonPreviewVersion(string previewVersion)
+        {
+            // Arrange
+            var installationDir = $"{BuildScriptGenerator.Constants.TemporaryInstallationDirectoryRoot}/python/{previewVersion}";
+            var appName = "flask-app";
+            var volume = CreateSampleAppVolume(appName);
+            var appDir = volume.ContainerDir;
+            var appOutputDir = "/tmp/app-output";
+            var script = new ShellScriptBuilder()
+                 .AddCommand(GetSnippetToCleanUpExistingInstallation())
+                 .SetEnvironmentVariable(SettingsKeys.EnableDynamicInstall, true.ToString())
+                 .SetEnvironmentVariable(
+                    SdkStorageConstants.SdkStorageBaseUrlKeyName,
+                    SdkStorageConstants.DevSdkStorageBaseUrl)
+                .AddBuildCommand(
+                $"{appDir} --platform {PythonConstants.PlatformName} --platform-version {previewVersion} -o {appOutputDir}")
+                .ToString();
+
+            // Act
+            var result = _dockerCli.Run(new DockerRunArguments
+            {
+                ImageId = _imageHelper.GetGitHubActionsBuildImage(),
+                EnvironmentVariables = new List<EnvironmentVariable> { CreateAppNameEnvVar(appName) },
+                Volumes = new List<DockerVolume> { volume },
+                CommandToExecuteOnRun = "/bin/bash",
+                CommandArguments = new[] { "-c", script }
+            });
+
+            // Assert
+            RunAsserts(
+                () =>
+                {
+                    Assert.True(result.IsSuccess);
+                    Assert.Contains(
+                        $"Python Version: {installationDir}/bin/python3",
+                        result.StdOut);
+                },
+                result.GetDebugInfo());
+        }
+
 
         [Fact]
         public void DynamicInstall_ReInstallsSdk_IfSentinelFileIsNotPresent()
