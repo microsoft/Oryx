95326b5e7a1e0d4c90a1939ab7fb21bfce99cf68
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-07-30 11:53:34 -0700

    update some more platform versions

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -11,9 +11,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
-        public const string Node10Version = "10.21.0";
-        public const string Node12Version = "12.18.2";
-        public const string Node14Version = "14.5.0";
+        public const string Node10Version = "10.22.0";
+        public const string Node12Version = "12.18.3";
+        public const string Node14Version = "14.7.0";
         public const string NodeAppInsightsSdkVersion = "1.8.3";
     }
 }
