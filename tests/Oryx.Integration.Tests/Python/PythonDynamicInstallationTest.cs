33a5701b1b087a629e3699797b9290f4b334c528
Kiran <kichalla@users.noreply.github.com>
2020-07-31 04:06:52 -0700

    Use pip using the '-m' option (#702)

--- tests/Oryx.Integration.Tests/Python/PythonDynamicInstallationTest.cs
+++ tests/Oryx.Integration.Tests/Python/PythonDynamicInstallationTest.cs
@@ -155,6 +155,49 @@ namespace Microsoft.Oryx.Integration.Tests
                 });
         }
 
+        [Fact]
+        public async Task CanBuildAndRunPythonAppWhenUsingPackageDirSwitch()
+        {
+            // Arrange
+            var pythonVersion = "3.7";
+            var appName = "flask-app";
+            var volume = CreateAppVolume(appName);
+            var appDir = volume.ContainerDir;
+            var packagesDir = ".python_packages/lib/python3.7/site-packages";
+            var buildScript = new ShellScriptBuilder()
+               .SetEnvironmentVariable(
+                    SdkStorageConstants.SdkStorageBaseUrlKeyName,
+                    SdkStorageConstants.DevSdkStorageBaseUrl)
+               .AddCommand(
+                $"oryx build {appDir} --platform {PythonConstants.PlatformName} --platform-version {pythonVersion} " +
+                $"-p packagedir={packagesDir}")
+               .AddDirectoryExistsCheck($"{appDir}/{packagesDir}")
+               .ToString();
+            var runScript = new ShellScriptBuilder()
+                .SetEnvironmentVariable(
+                    SdkStorageConstants.SdkStorageBaseUrlKeyName,
+                    SdkStorageConstants.DevSdkStorageBaseUrl)
+                .AddCommand($"oryx create-script -appPath {appDir} -bindPort {ContainerPort}")
+                .AddCommand(DefaultStartupFilePath)
+                .ToString();
+
+            await EndToEndTestHelper.BuildRunAndAssertAppAsync(
+                appName,
+                _output,
+                new[] { volume },
+                _imageHelper.GetGitHubActionsBuildImage(),
+                "/bin/bash", new[] { "-c", buildScript },
+                _imageHelper.GetRuntimeImage("python", "3.7"),
+                ContainerPort,
+                "/bin/bash",
+                new[] { "-c", runScript },
+                async (hostPort) =>
+                {
+                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
+                    Assert.Contains("Hello World!", data);
+                });
+        }
+
         private string GetSnippetToCleanUpExistingInstallation()
         {
             return $"rm -rf {DefaultSdksRootDir}; mkdir -p {DefaultSdksRootDir}";
