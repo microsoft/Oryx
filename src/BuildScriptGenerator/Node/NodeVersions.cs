c582277988026f55ab75a08b49a3147a9145646c
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-08-09 11:50:29 -0700

    update node python and dotnet core (#704)
    * update platform versions
    
    * update platform base image tags
    
    * update some more platform versions
    
    * update node runtime base image tag

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,16 +4,16 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20200730.4";
+        public const string NodeRuntimeBaseTag = "20200731.3";
         public const string YarnCacheBaseTag = "20200529.1";
         public const string YarnVersion = "1.17.3";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
-        public const string Node10Version = "10.21.0";
-        public const string Node12Version = "12.18.2";
-        public const string Node14Version = "14.0.0";
+        public const string Node10Version = "10.22.0";
+        public const string Node12Version = "12.18.3";
+        public const string Node14Version = "14.7.0";
         public const string NodeAppInsightsSdkVersion = "1.8.3";
     }
 }
