beb2628409a49ea1a28d2e6379030539c1de6228
Kiran <kichalla@users.noreply.github.com>
2020-09-03 12:53:17 -0700

    Print message to console when failed to parse yaml or toml configuration files. (#754)

---
+++ tests/Detector.Tests/ParserHelperTest.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using Microsoft.Oryx.Detector.Exceptions;
+using Microsoft.Oryx.Detector.Resources;
+using Xunit;
+
+namespace Microsoft.Oryx.Detector.Tests
+{
+    public class ParserHelperTest
+    {
+        [Fact]
+        public void ThrowsGeneralParserExceptionWhenFailingToParseInvalidTomlFile()
+        {
+            // Arrange
+            var fileName = "test.toml";
+            var expectedMessage = string.Format(Messages.FailedToParseFileExceptionFormat, fileName);
+            var repo = new MemorySourceRepo();
+            repo.AddFile("invaid text", fileName);
+
+            // Act & Assert
+            var exception = Assert.Throws<FailedToParseFileException>(
+                () => ParserHelper.ParseTomlFile(repo, fileName));
+            Assert.Equal(expectedMessage, exception.Message);
+            Assert.Equal(fileName, exception.FilePath);
+        }
+
+        [Fact]
+        public void ThrowsGeneralParserExceptionWhenFailingToParseInvalidYamlFile()
+        {
+            // Arrange
+            var fileName = "test.yaml";
+            var expectedMessage = string.Format(Messages.FailedToParseFileExceptionFormat, fileName);
+            var repo = new MemorySourceRepo();
+            repo.AddFile("\"test", fileName);
+
+            // Act & Assert
+            var exception = Assert.Throws<FailedToParseFileException>(
+                () => ParserHelper.ParseYamlFile(repo, fileName));
+            Assert.Equal(expectedMessage, exception.Message);
+            Assert.Equal(fileName, exception.FilePath);
+        }
+    }
+}
