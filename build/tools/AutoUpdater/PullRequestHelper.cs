6b5706c348b69f0adbfe2a90194a43a055169123
Kiran <kichalla@users.noreply.github.com>
2020-09-12 05:51:49 -0700

    Updated GitHub runners digest sha (#765)

---
+++ build/tools/AutoUpdater/PullRequestHelper.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace AutoUpdater
+{
+    internal class PullRequestHelper
+    {
+        public static string GetDescriptionForCreatingPullRequest(string forkBranchName)
+        {
+            var description = new StringBuilder();
+            description.AppendJoin(
+                separator: @"\n",
+                $"git fetch oryxci {forkBranchName}",
+                $"git checkout {forkBranchName}",
+                "git fetch origin",
+                "git rebase origin/master",
+                $"git push -u origin {forkBranchName}");
+            return description.ToString();
+        }
+    }
+}
