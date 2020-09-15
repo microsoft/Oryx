b4212ebc054c2bbe14fbb9bad9f76bec0ddb8ec1
Stella Qian <zqian@microsoft.com>
2020-09-15 13:38:48 -0700

    update

--- src/BuildScriptGenerator/RubyVersions.cs
+++ src/BuildScriptGenerator/RubyVersions.cs
@@ -10,6 +10,6 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Ruby26Version = "2.6.6";
         public const string Ruby26TarSha256 = "5db187882b7ac34016cd48d7032e197f07e4968f406b0690e20193b9b424841f";
         public const string Ruby25Version = "2.5.8";
-        public const string Ruby25TarSha256 = "6c0bdf07876c69811a9e7dc237c43d40b1cb6369f68e0e17953d7279b524ad9a";
+        public const string Ruby25TarSha256 = "0391b2ffad3133e274469f9953ebfd0c9f7c186238968cbdeeb0651aa02a4d6d";
     }
 }
