a2d862631900f47c8beca29c00ba93fa7ef106d3
Kiran <kichalla@users.noreply.github.com>
2020-09-15 18:20:30 -0700

    Updated Python versions (#772)

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -5,9 +5,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
     public static class PythonVersions
     {
         public const string PipVersion = "20.2";
-        public const string Python27Version = "2.7.17";
-        public const string Python36Version = "3.6.10";
-        public const string Python37Version = "3.7.8";
+        public const string Python27Version = "2.7.18";
+        public const string Python36Version = "3.6.12";
+        public const string Python37Version = "3.7.9";
         public const string Python38Version = "3.8.5";
         public const string Python39Version = "3.9.0b1";
     }
