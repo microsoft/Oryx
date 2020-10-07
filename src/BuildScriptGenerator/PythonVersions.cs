8b39316bb4cf2dfb0144d76bf65119ff4f5aff39
Kiran Challa <kichalla@microsoft.com>
2020-10-07 16:05:13 -0700

    Updated Python version to 3.9.0

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -4,11 +4,11 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
 {
     public static class PythonVersions
     {
-        public const string PipVersion = "20.2";
+        public const string PipVersion = "20.2.3";
         public const string Python27Version = "2.7.18";
         public const string Python36Version = "3.6.12";
         public const string Python37Version = "3.7.9";
         public const string Python38Version = "3.8.5";
-        public const string Python39Version = "3.9.0b1";
+        public const string Python39Version = "3.9.0";
     }
 }
