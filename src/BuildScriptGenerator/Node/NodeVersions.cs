96d7d61482ca29b270933c5e3b15bfbe7e256a96
Kiran <kichalla@microsoft.com>
2020-10-09 10:29:59 -0700

    Updated platform versions

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -6,14 +6,14 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
     {
         public const string NodeRuntimeBaseTag = "20200917.1";
         public const string YarnCacheBaseTag = "20200810.1";
-        public const string YarnVersion = "1.17.3";
+        public const string YarnVersion = "1.22.10";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
-        public const string Node10Version = "10.22.0";
-        public const string Node12Version = "12.18.3";
-        public const string Node14Version = "14.7.0";
+        public const string Node10Version = "10.22.1";
+        public const string Node12Version = "12.19.0";
+        public const string Node14Version = "14.13.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "3.5.1";
     }
--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Python27Version = "2.7.18";
         public const string Python36Version = "3.6.12";
         public const string Python37Version = "3.7.9";
-        public const string Python38Version = "3.8.5";
+        public const string Python38Version = "3.8.6";
         public const string Python39Version = "3.9.0";
     }
 }
