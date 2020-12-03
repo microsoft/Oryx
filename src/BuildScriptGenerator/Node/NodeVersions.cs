e345bba2900723b97f6ca633f163351854ede349
Zixuan Qian <zqian@microsoft.com>
2020-12-02 20:03:56 -0800

    Update Node, Python release versions (#869)

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20201027.2";
+        public const string NodeRuntimeBaseTag = "20201203.2";
         public const string YarnCacheBaseTag = "20200810.1";
         public const string YarnVersion = "1.22.10";
         public const string YarnMinorVersion = "1.17";
@@ -13,7 +13,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
         public const string Node12Version = "12.19.0";
-        public const string Node14Version = "14.15.0";
+        public const string Node14Version = "14.15.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "3.5.1";
     }
