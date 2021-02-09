bb730a516c7c35eedc2f511d3b2eb8b27bd55a14
Stella Qian <zqian@microsoft.com>
2021-02-09 09:40:27 -0800

    update

--- src/BuildScriptGenerator/Ruby/RubyPlatform.cs
+++ src/BuildScriptGenerator/Ruby/RubyPlatform.cs
@@ -108,7 +108,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Ruby
 
             if (!rubyPlatformDetectorResult.GemfileExists && !rubyPlatformDetectorResult.ConfigYmlFileExists)
             {
-                throw new InvalidUsageException($"No Gemfile found at the root of the repo. Please provide a Gemfile" +
+                throw new InvalidUsageException($"No Gemfile found at the root of the repo. Please provide a Gemfile. " +
                 $"For Jekyll apps, make sure it contains a '{RubyConstants.ConfigYmlFileName}' file and set it as a static web app");
             }
 
