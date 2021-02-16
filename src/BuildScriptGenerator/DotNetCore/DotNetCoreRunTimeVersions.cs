50439eacfa160673cc27b7aa8eca5054dc49ce15
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-02-16 14:11:52 -0800

    security updates for dotnet core images

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
@@ -4,27 +4,27 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
 {
     public static class DotNetCoreRunTimeVersions
     {
-        public const string DotNetCoreRuntimeBaseTag = "20210201.3";
+        public const string DotNetCoreRuntimeBaseTag = "20210210.1";
         public const string NetCoreApp10 = "1.0.16";
         public const string NetCoreApp11 = "1.1.13";
         public const string NetCoreApp20 = "2.0.9";
-        public const string NetCoreApp21 = "2.1.24";
-        public const string NetCoreApp21Sha = "afeeb1ee20b312c229dfecff76d9b6233299193e738280907020a953caa9c1c71dddb477044cb57f92d1445c83168a8eb8d0e6a686c689b2373ba798447268a9";
-        public const string AspnetCoreApp21 = "2.1.24";
-        public const string AspnetCoreApp21Sha = "e4b3a1e848d0cdec720946ff46fa1f31016f67f895bceab8ea19ce4c313284b636a6abd0378fb65cb02de3d9355a2663a382335e2e804694bd3ae3123032ddd7";
+        public const string NetCoreApp21 = "2.1.25";
+        public const string NetCoreApp21Sha = "ab93b5e4117b4f7d3267eef5552d5001e5f0974e2cc3994b80cfdc2dfe56d24f62bd49754b8dcdf0061f7e572e424631ca96d60efc63d6b63fdb2109fbf147f8";
+        public const string AspnetCoreApp21 = "2.1.25";
+        public const string AspnetCoreApp21Sha = "fc46e2c23048ad859b01ecd3600f312e6c0eae2a9c88302535a55d0a3d4d22065c53e4bf002fa5f959d488d5aba7dfeb60b15b300a40fae6b118f75ee3ab5918";
         public const string NetCoreApp22 = "2.2.8";
         public const string NetCoreApp22Sha = "954072376698be69acb7e277df2c243f931e10529def21dcbf9ce277609b30d462126bf8b8b3cab36476bec3d63a927b8e44e59e4d4cade23eef45956fba1ffd";
         public const string NetCoreApp30 = "3.0.3";
         public const string NetCoreApp30Sha = "3d1f0ba2f293f43e087a8940a0f4736561f43b9f28bc5beda79a61f887e258543aea4dc71e88414a7c6e271dcda984a989afb62fc659a72223707a179cdde9c5";
         public const string AspnetCoreApp30 = "3.0.3";
         public const string AspnetCoreApp30Sha = "d88366ac20204c5acb4b80bc40396d608d6c067b25baeef54204e77dde48fb515bd463743745a0982ba8c9182fc751306a4e796f91060b2d013ffbea7d9a2dc8";
-        public const string NetCoreApp31 = "3.1.11";
-        public const string NetCoreApp31Sha = "9f04797ae293723dbfe846676abcf5809e94a335b37fdc0c61b940e583b52730ba2539eddbf8edc0901fa6b7765b1ec3946eab381530e03f6d5d9946cc2bfbeb";
-        public const string AspnetCoreApp31 = "3.1.11";
-        public const string AspnetCoreApp31Sha = "626cbf5f4c76c9382403f18cc288a925e62d6bf821249be5c0ef2f58278479dd95ee4bc243377554a835eda0c2409c5d8e529c5f9712ae7475ce0420c8df4b72";
-        public const string NetCoreApp50 = "5.0.2";
-        public const string NetCoreApp50Sha = "e3ce93a74e9c69098e6767ac1b36b53ce71da53bce68c9b84bcd767c2015cf4f818640cb5311c2cf3934e9f39e59562cc83a533956d114ad446420b7c26f53a7";
-        public const string AspnetCoreApp50 = "5.0.2";
-        public const string AspnetCoreApp50Sha = "917978eb091f4af0c403343aee9bb0eac296bc3d67b7a0e2155d211f1c3f332c72f8867f1ea726387854bf26f06b745b8080d2097d4452f69606d01c11af3555";
+        public const string NetCoreApp31 = "3.1.12";
+        public const string NetCoreApp31Sha = "d1a815d26c75d9fe41479efc32626c1c83bdc616cdcde63768ff7480d721c37427a17fd3999366c738aa936afe43c5426162c4b188f923d810762c91230e2d8b";
+        public const string AspnetCoreApp31 = "3.1.12";
+        public const string AspnetCoreApp31Sha = "e6d384a4c05bc6a693a85dc1da5f34e26449ad5d9414dee5f46a56805ac53eb304610be06d6a2a683f2d9e1447f316f47abea71fbfd6ee901dcc9da9d7c4e03b";
+        public const string NetCoreApp50 = "5.0.3";
+        public const string NetCoreApp50Sha = "263dbe260123c3d6d706ed8b5f4d510d9384216422e9af0d293df87ed98e84e1e0ffbf0c7dd543c40c5ccc95bd7cd006c8bbbe9f1cd1f060b1eaa2f7a60fea74";
+        public const string AspnetCoreApp50 = "5.0.3";
+        public const string AspnetCoreApp50Sha = "188618b07cd5c97a34c77efccc7c4eba10a52681592ef711a005c82d243e601891418e0dcc27a22aae7dd6ae33d35e0bb7aa9b9fc022746c6c2414bd25cdb110";
     }
 }
