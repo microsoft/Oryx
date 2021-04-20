40d388e79accce6ff1ac0d6ce9ceee743078b3f0
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-04-20 09:37:27 -0700

    update node base runtime tags

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20210414.5";
+        public const string NodeRuntimeBaseTag = "20210420.2";
         public const string YarnVersion = "1.22.10";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
