215fdd36ae46c7540a61c0491ee6ac3e935936cd
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-04-20 12:20:45 -0700

    add .net 6 apps for tests (#989)

---
+++ tests/SampleApps/DotNetCore/NetCore6PreviewConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore6PreviewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/SampleApps/DotNetCore/NetCore6PreviewMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore6PreviewMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
