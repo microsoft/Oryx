83cf8ad66541432d2db7405396f27106a05daf39
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-04-28 12:44:06 -0700

    update legacy node versions in node runtime tests

--- tests/Oryx.Tests.Common/TestValueGenerator.cs
+++ tests/Oryx.Tests.Common/TestValueGenerator.cs
@@ -21,7 +21,8 @@ namespace Microsoft.Oryx.Tests.Common
         private readonly static List<string> NodeVersions = new List<string>
         {
             "10","10.1", "10.10", "10.14",
-            "12"
+            "12",
+            "14"
         };
 
         private readonly static List<string> PythonVersions = new List<string>
@@ -49,15 +50,25 @@ namespace Microsoft.Oryx.Tests.Common
         {
             var versions = new List<string>
             {
-                "8","8.0", "8.1", "8.2", "8.8", "8.9", "8.11", "8.12",
-                "9.4",
                 "10","10.1", "10.10", "10.14",
-                "12"
+                "12",
+                "14"
             };
 
             return versions.Select(v => new object[] { v });
         }
 
+        public static IEnumerable<object[]> GetLegacyNodeVersions_SupportDebugging()
+        {
+            var versions = new List<string>
+            {
+                "8","8.0", "8.1", "8.2", "8.8", "8.9", "8.11", "8.12",
+                "9.4"
+            };
+
+            return versions.Select(v => new object[] { v });
+        }
+        
         public static IEnumerable<object[]> GetNodeVersions_DoesNotSupportDebugging()
         {
             var versions = new List<string>
