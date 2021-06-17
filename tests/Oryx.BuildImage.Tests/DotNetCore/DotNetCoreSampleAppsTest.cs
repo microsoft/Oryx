4ec9b26dcee55ff30dbdde2350c2491f9c7bd06b
Zixuan Qian <zqian@microsoft.com>
2021-06-17 16:28:35 -0700

    Include .NET 3 SDK to Jamstack build image (#1034)
    * include .NET 3 to jamstack image, fixing .NET 5 function app issue
    
    * adding tests
    
    * update

--- tests/Oryx.BuildImage.Tests/DotNetCore/DotNetCoreSampleAppsTest.cs
+++ tests/Oryx.BuildImage.Tests/DotNetCore/DotNetCoreSampleAppsTest.cs
@@ -888,5 +888,30 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 },
                 result.GetDebugInfo());
         }
+
+        [Fact]
+        public void DotNetCoreSDK31_IsPresentInTheJamStackImage()
+        {
+            // Arrange
+            var versionCommand = "dotnet --version";
+            var expectedVersion = DotNetCoreSdkVersions.DotNetCore31SdkVersion;
+            // Act
+            var result = _dockerCli.Run(new DockerRunArguments
+            {
+                ImageId = _imageHelper.GetAzureFunctionsJamStackBuildImage(),
+                CommandToExecuteOnRun = "/bin/bash",
+                CommandArguments = new[] { "-c", versionCommand },
+            });
+
+            // Assert
+            var actualOutput = result.StdOut.ReplaceNewLine();
+            RunAsserts(
+                () =>
+                {
+                    Assert.True(result.IsSuccess);
+                    Assert.Contains(expectedVersion, actualOutput);
+                },
+                result.GetDebugInfo());
+        }
     }
 }
