6daaf3110840112d1a7eb6533bfa1518dfa1f5e7
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-07-19 13:46:39 -0700

    fix python test failure

--- tests/SampleApps/python/django-app/myproject/settings.py
+++ tests/SampleApps/python/django-app/myproject/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',  # adds PostGIS / GeoDjango functionality
     'boards',
     'uservoice'
 ]
