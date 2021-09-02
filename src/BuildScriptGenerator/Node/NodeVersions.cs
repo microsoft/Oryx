a72ec250bb229f96c55cae8df7800337738fa178
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-09-02 14:40:47 -0700

    update node and npm versions (#1098)
    * bump npm and node version
    
    * update node versions
    
    * update node runtime image base tag
    
    Co-authored-by: Josh Spicer <josh@joshspicer.com>

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,18 +4,18 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20210803.4";
+        public const string NodeRuntimeBaseTag = "20210902.2";
         public const string YarnVersion = "1.22.10";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
-        public const string Node12Version = "12.22.4";
-        public const string Node14Version = "14.17.4";
-        public const string Node16Version = "16.6.1";
+        public const string Node12Version = "12.22.6";
+        public const string Node14Version = "14.17.6";
+        public const string Node16Version = "16.8.0";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
-        public const string NpmVersion = "6.14.14";
+        public const string NpmVersion = "6.14.15";
     }
 }
