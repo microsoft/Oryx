3836d3d39a72796d23972dad15d76a2265a43768
William Hernandez-Limon <williamhe@microsoft.com>
2021-09-12 13:54:20 -0700

    Add Hugo build properties

---
+++ src/BuildScriptGenerator/Hugo/HugoBashBuildSnippetProperties.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Hugo
+{
+    /// <summary>
+    /// Build script template for Hugo in Bash.
+    /// </summary>
+    public class HugoBashBuildSnippetProperties
+    {
+        public string CustomRunBuildCommand { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of commands for the build.
+        /// </summary>
+        public IDictionary<string, string> HugoBuildProperties { get; set; }
+
+    }
+}
