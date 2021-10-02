a41d15c99639182d145ffd5733119a428ebbf211
William Hernandez-Limon <williamhe@microsoft.com>
2021-10-01 19:55:36 -0700

    Add retry method

---
+++ build/__common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+# --------------------------------------------------------------------------------------------
+
+function retry() {
+	# this method will execute a command
+	# and sleep & retry if there's a failure
+	# $1 
+	#	parameter contains the full command to be executed
+	r=0
+	retries=5
+	while [ "$r" -le "$retries" ]
+	do
+	   echo "retry $r"
+	   $1 && break
+	   echo "error executing: $1"
+	   r=$((r+1)) 
+	   sleep 15
+	done
+}
