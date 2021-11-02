a77f112ac327caf6e2ad88cdc6b469a3710dec56
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2021-11-01 17:25:16 -0700

    update

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -6,8 +6,8 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
     {
         public const string PipVersion = "21.2.4";
         public const string Python27Version = "2.7.18";
-        public const string Python36Version = "3.6.14";
-        public const string Python37Version = "3.7.11";
+        public const string Python36Version = "3.6.15";
+        public const string Python37Version = "3.7.12";
         public const string Python38Version = "3.8.12";
         public const string Python39Version = "3.9.7";
         public const string Python310Version = "3.10.0";
