09d967284e91fc176ddf8c14c06103de359426a5
William Hernandez-Limon <williamhe@microsoft.com>
2021-11-17 16:45:53 -0800

    Add new url for getting jdk17

--- platforms/java/getJavaSdk.sh
+++ platforms/java/getJavaSdk.sh
@@ -46,6 +46,23 @@ downloadJavaSdk()
     IFS='.' read -ra VERSION_PARTS <<< "$JDK_VERSION"
     majorVersion="${VERSION_PARTS[0]}"
 
+    # Version 17 has a different url format than rest of the versions, so special casing it.
+    if [ "$majorVersion" == "17" ]; then
+        local versionUpdate="2a2082e5a09d4267845be086888add4f"
+        local buildNumber="12"
+        local url="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk${versionUpdate}-${buildNumber}/OpenJDK8U-jdk_x64_linux_hotspot_${versionUpdate}${buildNumber}.tar.gz"
+        local url="https://download.java.net/java/GA/jdk${JDK_VERSION}/${versionUpdate}/${buildNumber}/GPL/openjdk-${JDK_VERSION}_linux-x64_bin.tar.gz"
+
+        curl -L "$url" -o $tarFileName
+        rm -rf extracted
+        mkdir -p extracted
+        tar -xf $tarFileName --directory extracted
+        cd "extracted/jdk${versionUpdate}-${buildNumber}"
+        tar -zcf "$hostJavaArtifactsDir/$tarFileName" .
+		echo "Version=$JDK_VERSION" >> "$hostJavaArtifactsDir/java-$JDK_VERSION-metadata.txt"
+        return
+    fi
+
     if shouldBuildSdk java $tarFileName || shouldOverwriteSdk || shouldOverwriteJavaSdk; then
         local baseUrl="https://github.com/AdoptOpenJDK/openjdk${majorVersion}-binaries/releases/download"
         if [ "$majorVersion" == "10" ] && [ "$JDK_BUILD_NUMBER" == "13" ]; then
