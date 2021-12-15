ef44fa5327203d8c33685980039ef7e9c6285649
Zixuan Qian <zqian@microsoft.com>
2021-12-15 11:25:24 -0800

    Support php 8.1 and python 3.10 dynamic install (#1184)
    * support php 8.1 and python 3.10 dynamic install
    
    * update
    
    * update runtime
    
    * update runtime tag
    
    Co-authored-by: Stella Qian <zixuan.qian@microsoft.com>

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -4,8 +4,8 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
 {
     public static class PhpVersions
     {
-        public const string PhpRuntimeBaseTag = "20211031.1";
-        public const string PhpFpmRuntimeBaseTag = "20211031.1";
+        public const string PhpRuntimeBaseTag = "20211214.1";
+        public const string PhpFpmRuntimeBaseTag = "20211214.1";
         public const string ComposerVersion = "2.0.8";
         public const string ComposerSetupSha384 = "756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3";
         public const string Composer19Version = "1.9.3";
@@ -14,12 +14,15 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Composer110SetupSha384 = "756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3";
         public const string Composer20Version = "2.0.8";
         public const string Composer20SetupSha384 = "756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3";
-        public const string Php80Version = "8.0.11";
+        public const string Php81Version = "8.1.0";
+        public const string Php81Keys = "528995BFEDFBA7191D46839EF9BA0ADA31CBD89E 39B641343D8C104B2B146DC3F9C39DC0B9698544";
+        public const string Php81TarSha256 = "a1317eff0723a2b3d3122bbfe107a1158570ea2822dc35a5fb360086db0f6bbc";
+        public const string Php80Version = "8.0.13";
         public const string Php80Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 BFDDD28642824F8118EF77909B67A5C12229118F";
-        public const string Php80TarSha256 = "e3e5f764ae57b31eb65244a45512f0b22d7bef05f2052b23989c053901552e16";
-        public const string Php74Version = "7.4.24";
+        public const string Php80TarSha256 = "cd976805ec2e9198417651027dfe16854ba2c2c388151ab9d4d268513d52ed52";
+        public const string Php74Version = "7.4.26";
         public const string Php74Keys = "42670A7FE4D0441C8E4632349E4FDC074A4EF02D 5A52880781F755608BF815FC910DEB46F53EA312";
-        public const string Php74TarSha256 = "ff7658ee2f6d8af05b48c21146af5f502e121def4e76e862df5ec9fa06e98734";
+        public const string Php74TarSha256 = "e305b3aafdc85fa73a81c53d3ce30578bc94d1633ec376add193a1e85e0f0ef8";
         public const string Php73Version = "7.3.27";
         public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
         public const string Php73TarSha256 = "65f616e2d5b6faacedf62830fa047951b0136d5da34ae59e6744cbaf5dca148d";
