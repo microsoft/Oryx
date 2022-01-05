c3780704c3375556fb9211d2211a98a684bf5f16
William Hernandez-Limon <williamhe@microsoft.com>
2022-01-05 14:13:15 -0800

    Remove more integration tests with old php versions

--- tests/Oryx.Integration.Tests/Php/PhpGreetingsAppTest.cs
+++ tests/Oryx.Integration.Tests/Php/PhpGreetingsAppTest.cs
@@ -25,9 +25,6 @@ namespace Microsoft.Oryx.Integration.Tests
         [Theory]
         [InlineData("8.0")]
         [InlineData("7.4")]
-        [InlineData("7.3")]
-        [InlineData("7.2")]
-        [InlineData("7.0")]
         public async Task GreetingsAppTest(string phpVersion)
         {
             // Arrange
@@ -64,8 +61,6 @@ namespace Microsoft.Oryx.Integration.Tests
         [Theory]
         [InlineData("8.0")]
         [InlineData("7.4")]
-        [InlineData("7.3")]
-        [InlineData("7.2")]
         public async Task PhpFpmGreetingsAppTest(string phpVersion)
         {
             // Arrange
