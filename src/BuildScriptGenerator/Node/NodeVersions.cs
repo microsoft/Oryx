d75a000a4963a05038a1557bc39dc1a29376ed38
Stella Qian <zixuan.qian@microsoft.com>
2022-01-28 10:34:39 -0800

    update node and npm versions

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -16,6 +16,6 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node16Version = "16.13.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
-        public const string NpmVersion = "8.1.2";
+        public const string NpmVersion = "6.14.15";
     }
 }
