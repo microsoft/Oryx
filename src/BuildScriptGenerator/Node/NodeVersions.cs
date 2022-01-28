b879f2c30cdd013d663b2d7064386e2ff5d50127
Stella Qian <zixuan.qian@microsoft.com>
2022-01-28 11:26:09 -0800

    update image tags

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
 {
     public static class NodeVersions
     {
-        public const string NodeRuntimeBaseTag = "20211208.1";
+        public const string NodeRuntimeBaseTag = "20220128.5";
         public const string YarnVersion = "1.22.15";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
@@ -12,7 +12,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
         public const string Node12Version = "12.22.6";
-        public const string Node14Version = "14.17.6";
+        public const string Node14Version = "14.18.3";
         public const string Node16Version = "16.13.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
