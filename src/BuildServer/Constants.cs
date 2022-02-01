7004bc626ecf0356fafe63316d29a1089898b699
William Hernandez-Limon <williamhe@microsoft.com>
2022-02-01 15:57:37 -0800

    Remove unnecessary code

--- src/BuildServer/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.Oryx.BuildServer
-{
-    public class Constants
-    {
-        public static readonly string StorePath = "/store";
-        public static readonly string HomePath = "/home";
-
-    }
-}
