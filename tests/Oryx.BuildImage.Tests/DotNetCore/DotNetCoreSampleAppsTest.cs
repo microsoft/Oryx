cd8494ac94eeff3dea4348c1576101c2b8d7a718
REDMOND\williamhe <williamhe@microsoft.com>
2022-03-12 17:34:39 -0800

    Update to un-skip tests

--- tests/Oryx.BuildImage.Tests/DotNetCore/DotNetCoreSampleAppsTest.cs
+++ tests/Oryx.BuildImage.Tests/DotNetCore/DotNetCoreSampleAppsTest.cs
@@ -312,7 +312,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
         }
 
         // This test is necessary once .NET 6 preview 5 come out.
-        [Fact(Skip = "1492709")]
+        [Fact]
         public void Builds_Net6BlazorWasmApp_RunsAOTCompilationInstallCommands()
         {
             // Arrange
@@ -984,7 +984,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 result.GetDebugInfo());
         }
 
-        [Fact(Skip = "1492709")]
+        [Fact]
         public void JamstackImage_CanBuild_Dotnet5_Isolated_apps()
         {
             // Arrange
