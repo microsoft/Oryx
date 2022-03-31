0fe074df6a32e555f46681559419c7f3f023684d
william-msft <86327553+william-msft@users.noreply.github.com>
2022-03-31 16:58:13 -0700

    Add golang 1.18 (#1323)

--- src/BuildScriptGenerator/GolangVersions.cs
+++ src/BuildScriptGenerator/GolangVersions.cs
@@ -8,5 +8,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Golang16TarSha256 = "7fe7a73f55ba3e2285da36f8b085e5c0159e9564ef5f63ee0ed6b818ade8ef04";
         public const string Golang17Version = "1.17";
         public const string Golang17TarSha256 = "6bf89fc4f5ad763871cf7eac80a2d594492de7a818303283f1366a7f6a30372d";
+        public const string Golang18Version = "1.18";
+        public const string Golang18TarSha256 = "e85278e98f57cdb150fe8409e6e5df5343ecb13cebf03a5d5ff12bd55a80264f";
     }
 }
