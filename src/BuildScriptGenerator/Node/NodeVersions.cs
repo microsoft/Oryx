ba41b35649e97684a8413da721e0fa082d943f22
Samruddhi Khandale <skhandale@microsoft.com>
2022-04-12 22:43:52 +0000

    test fix : attempt

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
         public const string Node12Version = "12.22.6";
-        public const string Node14Version = "14.18.3";
+        public const string Node14Version = "14.19.1";
         public const string Node16Version = "16.13.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
