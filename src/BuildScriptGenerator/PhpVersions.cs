642430964b0f0b0b69e8e855595c85fb36c903c9
Samruddhi Khandale <skhandale@microsoft.com>
2022-04-26 16:45:49 +0000

    fix tests

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -12,9 +12,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Composer110Version = "1.10.19";
         public const string Composer20Version = "2.0.8";
         public const string Composer22Version = "2.2.9";
-        public const string Php81Version = "8.1.4";
+        public const string Php81Version = "8.1.0";
         public const string Php81Keys = "528995BFEDFBA7191D46839EF9BA0ADA31CBD89E 39B641343D8C104B2B146DC3F9C39DC0B9698544";
-        public const string Php81TarSha256 = "05a8c0ac30008154fb38a305560543fc172ba79fb957084a99b8d3b10d5bdb4b";
+        public const string Php81TarSha256 = "a1317eff0723a2b3d3122bbfe107a1158570ea2822dc35a5fb360086db0f6bbc";
         public const string Php80Version = "8.0.17";
         public const string Php80Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 BFDDD28642824F8118EF77909B67A5C12229118F";
         public const string Php80TarSha256 = "bdbd792901c156c4d1710c9d266732d3c17f6ff63850d6660b9d8d3411188424";
--- tests/Oryx.BuildImage.Tests/VersionInformationTest.cs
+++ tests/Oryx.BuildImage.Tests/VersionInformationTest.cs
@@ -551,7 +551,13 @@ namespace Microsoft.Oryx.BuildImage.Tests
         public void PhpAlias_UsesPhpLatestVersion_ByDefault_WhenNoExplicitVersionIsProvided(string buildImageName)
         {
             // Arrange
-            var expectedOutput = $"PHP {PhpVersions.Php81Version} (cli) ";
+            var phpVersion = PhpVersions.Php73Version
+            if (buildImageName == Settings.VsoUbuntuBuildImageName)
+            {
+                phpVersion = PhpVersions.Php81Version
+            }
+
+            var expectedOutput = $"PHP {phpVersion} (cli) ";
 
             // Arrange & Act
             var result = _dockerCli.Run(new DockerRunArguments
