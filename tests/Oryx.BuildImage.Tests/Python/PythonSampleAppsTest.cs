010ef1279881495b51afb172984a55dfe3b108ca
Samruddhi Khandale <skhandale@microsoft.com>
2022-04-27 02:41:16 +0000

    fix test

--- tests/Oryx.BuildImage.Tests/Python/PythonSampleAppsTest.cs
+++ tests/Oryx.BuildImage.Tests/Python/PythonSampleAppsTest.cs
@@ -758,7 +758,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
             {
-                ImageId = _imageHelper.GetVsoBuildImage("ltsversions"),
+                ImageId = _imageHelper.GetVsoBuildImage("lts-versions"),
                 EnvironmentVariables = new List<EnvironmentVariable> { CreateAppNameEnvVar(appName) },
                 Volumes = new List<DockerVolume> { volume },
                 CommandToExecuteOnRun = "/bin/bash",
