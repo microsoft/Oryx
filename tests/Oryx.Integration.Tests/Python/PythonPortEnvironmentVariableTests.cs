8bca886edc2e0366ef87d9d778ec530d6a5414dd
Samruddhi Khandale <skhandale@microsoft.com>
2022-04-28 22:24:10 +0000

    chnage flask to django-app

--- tests/Oryx.Integration.Tests/Python/PythonPortEnvironmentVariableTests.cs
+++ tests/Oryx.Integration.Tests/Python/PythonPortEnvironmentVariableTests.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Oryx.Integration.Tests
         public async Task PythonStartupScript_UsesPortEnvironmentVariableValue()
         {
             // Arrange
-            var appName = "flask-app";
+            var appName = "django-app";
             var volume = CreateAppVolume(appName);
             var appDir = volume.ContainerDir;
             var appOutputDirVolume = CreateAppOutputDirVolume();
@@ -59,8 +59,17 @@ namespace Microsoft.Oryx.Integration.Tests
                 },
                 async (hostPort) =>
                 {
-                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
-                    Assert.Contains("Hello World!", data);
+                    var data = await GetResponseDataAsync($"http://localhost:{hostPort}/staticfiles/css/boards.css");
+                    Assert.Contains("CSS file from Boards app module", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/staticfiles/css/uservoice.css");
+                    Assert.Contains("CSS file from UserVoice app module", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/boards/");
+                    Assert.Contains("Hello, World! from Boards app", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/uservoice/");
+                    Assert.Contains("Hello, World! from Uservoice app", data);
                 });
         }
 
@@ -68,7 +77,7 @@ namespace Microsoft.Oryx.Integration.Tests
         public async Task PythonStartupScript_UsesSuppliedBindingPort_EvenIfPortEnvironmentVariableValue_IsPresent()
         {
             // Arrange
-            var appName = "flask-app";
+            var appName = "django-app";
             var volume = CreateAppVolume(appName);
             var appDir = volume.ContainerDir;
             var appOutputDirVolume = CreateAppOutputDirVolume();
@@ -103,8 +112,17 @@ namespace Microsoft.Oryx.Integration.Tests
                 },
                 async (hostPort) =>
                 {
-                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
-                    Assert.Contains("Hello World!", data);
+                    var data = await GetResponseDataAsync($"http://localhost:{hostPort}/staticfiles/css/boards.css");
+                    Assert.Contains("CSS file from Boards app module", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/staticfiles/css/uservoice.css");
+                    Assert.Contains("CSS file from UserVoice app module", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/boards/");
+                    Assert.Contains("Hello, World! from Boards app", data);
+
+                    data = await GetResponseDataAsync($"http://localhost:{hostPort}/uservoice/");
+                    Assert.Contains("Hello, World! from Uservoice app", data);
                 });
         }
     }
