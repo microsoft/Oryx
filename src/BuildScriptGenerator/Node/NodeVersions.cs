f15b4429184a7c564c4ed89b7092fd84d57df76c
Cormac McCarthy <corm@microsoft.com>
2022-05-19 10:47:55 -0700

    Update node 12 and 16 runtimes

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
-        public const string Node12Version = "12.22.6";
+        public const string Node12Version = "12.22.11";
         public const string Node14Version = "14.19.1";
         public const string Node16Version = "16.14.2";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
