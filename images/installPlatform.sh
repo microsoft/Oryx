8558263a05946ef6d01147ba3c5dbe81aad2399e
Stella Qian <zixuan.qian@microsoft.com>
2022-05-24 14:49:30 -0700

    Revert "support python bullseye SDK"
    This reverts commit c7f63da7013722f5d7784d5f6d8c974527399cdf.

--- images/installPlatform.sh
+++ images/installPlatform.sh
@@ -52,8 +52,11 @@ fi
 if [ -z "$debianFlavor" ] || [ "$debianFlavor" == "stretch" ]; then
   # Use default sdk file name
 	fileName="$PLATFORM_NAME-$VERSION.tar.gz"
-else
+elif [ "$debianFlavor" == "buster" ] || [ "$debianFlavor" == "focal-scm" ]; then
   fileName="$PLATFORM_NAME-$debianFlavor-$VERSION.tar.gz"
+else
+  # Bullseye SDKs are not supported so using buster version for now.
+  fileName="$PLATFORM_NAME-buster-$VERSION.tar.gz"
 fi
 
 platformDir="/opt/$PLATFORM_NAME"
