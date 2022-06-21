e4189dfba8067cf40305ee47eda700120b73c66e
Stella Qian <zixuan.qian@microsoft.com>
2022-06-20 17:05:16 -0700

    updating tests

--- tests/Oryx.Integration.Tests/Node/NodeDynamicInstallationTest.cs
+++ tests/Oryx.Integration.Tests/Node/NodeDynamicInstallationTest.cs
@@ -123,6 +123,56 @@ namespace Microsoft.Oryx.Integration.Tests
                 });
         }
 
+        [Theory]
+        [InlineData(NodeVersions.Node18Version), Trait("category", "node-18")]
+        public async Task CanBuildAndRunApp_UsingScriptCommand(string nodeVersion)
+        {
+            // Arrange
+            var appName = "webfrontend";
+            var volume = CreateAppVolume(appName);
+            var appDir = volume.ContainerDir;
+            var appOutputDirVolume = CreateAppOutputDirVolume();
+            var appOutputDir = appOutputDirVolume.ContainerDir;
+            var buildScript = new ShellScriptBuilder()
+                .AddCommand(GetSnippetToCleanUpExistingInstallation())
+                .AddDefaultTestEnvironmentVariables()
+                .AddCommand(
+                $"oryx build {appDir} -i /tmp/int -o {appOutputDir} " +
+                $"--platform {NodeConstants.PlatformName} --platform-version {nodeVersion}")
+                .ToString();
+            var runScript = new ShellScriptBuilder()
+                .AddDefaultTestEnvironmentVariables()
+                .SetEnvironmentVariable(SettingsKeys.EnableDynamicInstall, true.ToString())
+                .AddCommand($"oryx create-script -appPath {appOutputDir} -bindPort {ContainerPort}")
+                .AddCommand(DefaultStartupFilePath)
+                .ToString();
+
+            await EndToEndTestHelper.BuildRunAndAssertAppAsync(
+                appName,
+                _output,
+                new[] { volume, appOutputDirVolume },
+                _imageHelper.GetGitHubActionsBuildImage("github-actions-bullseye"),
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    buildScript
+                },
+                _imageHelper.GetRuntimeImage("node", "18"),
+                ContainerPort,
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    runScript
+                },
+                async (hostPort) =>
+                {
+                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
+                    Assert.Contains("Say It Again", data);
+                });
+        }
+
         private string GetSnippetToCleanUpExistingInstallation()
         {
             return $"rm -rf {DefaultSdksRootDir}; mkdir -p {DefaultSdksRootDir}";
