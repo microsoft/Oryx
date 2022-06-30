31372ee5e99a0dad59243704e81c13e1b9237e0a
Wali Bhuiyan <mbhuiyan@microsoft.com>
2022-06-30 12:54:37 -0400

    changes

--- tests/Oryx.Integration.Tests/DotNetCore/DotNetCoreRuntimeVersion60Tests.cs
+++ tests/Oryx.Integration.Tests/DotNetCore/DotNetCoreRuntimeVersion60Tests.cs
@@ -6,6 +6,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Oryx.BuildScriptGenerator;
 using Microsoft.Oryx.BuildScriptGenerator.Common;
 using Microsoft.Oryx.BuildScriptGenerator.DotNetCore;
 using Microsoft.Oryx.BuildScriptGeneratorCli;
@@ -68,6 +69,60 @@ namespace Microsoft.Oryx.Integration.Tests
                 {
                     var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
                     Assert.Contains("Welcome to ASP.NET Core MVC!", data);
+                });
+        }
+
+        [Fact]
+        public async Task CanBuildAndRun_NetCore60MvcApp_WithCustomizedRunCommand()
+        {
+            // Arrange
+            var tempScriptPath = Path.Combine(Path.GetTempPath(), "run.sh");
+            var dotnetcoreVersion = DotNetCoreRunTimeVersions.NetCoreApp60;
+            var hostDir = Path.Combine(_hostSamplesDir, "DotNetCore", NetCoreApp60MvcApp);
+            var appsvcFilePath = Path.Combine(hostDir, "appsvc.yaml");
+            BuildConfigurationFIle buildConfigFile = BuildConfigurationFIle.Create(File.ReadAllText(appsvcFilePath));
+            var volume = DockerVolume.CreateMirror(hostDir);
+            var appDir = volume.ContainerDir;
+            var appOutputDirVolume = CreateAppOutputDirVolume();
+            var appOutputDir = appOutputDirVolume.ContainerDir;
+            var buildImageScript = new ShellScriptBuilder()
+               .AddDefaultTestEnvironmentVariables()
+               .AddCommand(
+                $"oryx build {appDir} -i /tmp/int --platform {DotNetCoreConstants.PlatformName} " +
+                $"--platform-version {dotnetcoreVersion} -o {appOutputDir}")
+               .ToString();
+            var runtimeImageScript = new ShellScriptBuilder()
+                .AddCommand(
+                $"oryx create-script -appPath {appOutputDir} -bindPort {ContainerPort}")
+                .AddCommand(DefaultStartupFilePath)
+                .ToString();
+
+            await EndToEndTestHelper.BuildRunAndAssertAppAsync(
+                NetCoreApp60MvcApp,
+                _output,
+                new DockerVolume[] { volume, appOutputDirVolume },
+                _imageHelper.GetGitHubActionsBuildImage(),
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    buildImageScript
+                },
+                _imageHelper.GetRuntimeImage("dotnetcore", "6.0"),
+                ContainerPort,
+                "/bin/sh",
+                new[]
+                {
+                    "-c",
+                    runtimeImageScript
+                },
+                async (hostPort) =>
+                {
+                    var data = await _httpClient.GetStringAsync($"http://localhost:{hostPort}/");
+                    Assert.NotNull(buildConfigFile);
+                    Assert.NotNull(buildConfigFile.Run);
+                    Assert.NotEmpty(buildConfigFile.Run);
+                    Assert.Contains(buildConfigFile.Run, data);
                 });
         }
 
