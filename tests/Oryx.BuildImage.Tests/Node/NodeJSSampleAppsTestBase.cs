df1c751c2375989edd2d6afc86941077f1814e8b
Wali Bhuiyan <mbhuiyan@microsoft.com>
2022-06-30 11:29:59 -0400

    addressed PR comments

--- tests/Oryx.BuildImage.Tests/Node/NodeJSSampleAppsTestBase.cs
+++ tests/Oryx.BuildImage.Tests/Node/NodeJSSampleAppsTestBase.cs
@@ -43,8 +43,8 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 $"{appDir} -o {appOutputDir}")
                 .ToString();
             // Regex will match:
-            // "yyyy-mm-dd hh:mm:ss"|ERROR|node: errors parsing node.mod
-            Regex regex = new Regex(@"""[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])""\|ERROR\|node:\serrors\sparsing\snode\.mod.*");
+            // "yyyy-mm-dd hh:mm:ss"|WARNING|node.
+            Regex regex = new Regex(@"""[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])""\|WARNING\|node.*");
 
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
