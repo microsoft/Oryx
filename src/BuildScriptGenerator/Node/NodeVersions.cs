44fbcf77979212d45237642dc8a75c992ff32b13
Stella Qian <zixuan.qian@microsoft.com>
2022-07-07 15:29:15 -0700

    update constant files

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -10,15 +10,11 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string YarnVersion = "1.22.15";
         public const string YarnMinorVersion = "1.17";
         public const string YarnMajorVersion = "1";
-        public const string Node6Version = "6.17.1";
-        public const string Node8Version = "8.17.0";
-        public const string Node10Version = "10.23.0";
-        public const string Node12Version = "12.22.11";
         public const string Node14Version = "14.19.1";
         public const string Node16Version = "16.14.2";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
         public const string NpmVersion = "6.14.15";
-        public static readonly List<string> RuntimeVersions = new List<string> { "4.4", "4.5", "4.8", "6", "6.2", "6.6", "6.9", "6.10", "6.11", "8", "8.0", "8.1", "8.2", "8.8", "8.9", "8.11", "8.12", "9.4", "10", "10.1", "10.10", "10.12", "10.14", "12", "14", "16" };
+        public static readonly List<string> RuntimeVersions = new List<string> { "14", "16" };
     }
 }
