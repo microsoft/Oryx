fb49ade0d616d536da52f6b653d77301bb78b4b2
Stella Qian <zixuan.qian@microsoft.com>
2022-07-07 16:18:52 -0700

    Revert "update constant files"
    This reverts commit 4d62f63bd73958a79555729e213d88ab71fa052b.

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -24,6 +24,18 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Php74Version = "7.4.28";
         public const string Php74Keys = "42670A7FE4D0441C8E4632349E4FDC074A4EF02D 5A52880781F755608BF815FC910DEB46F53EA312";
         public const string Php74TarSha256 = "a04014cd1646b90547907e2e0ac5371594533960de317b6c7ac70bcb42db92fb";
-         public static readonly List<string> RuntimeVersions = new List<string> { "7.4", "8.0", "8.1" };
+        public const string Php73Version = "7.3.27";
+        public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
+        public const string Php73TarSha256 = "65f616e2d5b6faacedf62830fa047951b0136d5da34ae59e6744cbaf5dca148d";
+        public const string Php72Version = "7.2.34";
+        public const string Php72Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 B1B44D8F021E4E2D6021E995DC9FF8D3EE5AF27F";
+        public const string Php72TarSha256 = "409e11bc6a2c18707dfc44bc61c820ddfd81e17481470f3405ee7822d8379903";
+        public const string Php70Version = "7.0.33";
+        public const string Php70Keys = "1A4E8B7277C42E53DBA9C7B9BCAA30EA9C0D5763 6E4F6AB321FDC07F2C332E3AC2BF0BC433CFC8B3";
+        public const string Php70TarSha256 = "ab8c5be6e32b1f8d032909dedaaaa4bbb1a209e519abb01a52ce3914f9a13d96";
+        public const string Php56Version = "5.6.40";
+        public const string Php56Keys = "0BD78B5F97500D450838F95DFE857D9A90D90EC1 6E4F6AB321FDC07F2C332E3AC2BF0BC433CFC8B3";
+        public const string Php56TarSha256 = "1369a51eee3995d7fbd1c5342e5cc917760e276d561595b6052b21ace2656d1c";
+        public static readonly List<string> RuntimeVersions = new List<string> { "5.6", "7.0", "7.2", "7.3", "7.4", "8.0", "8.1" };
     }
 }
