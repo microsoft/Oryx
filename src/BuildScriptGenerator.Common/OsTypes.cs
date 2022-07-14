49dbc3754b4e2c0a10aa6e8f3d6039e19d3b53b6
Paul Dorsch <pauldorsch@microsoft.com>
2022-07-14 14:28:06 -0400

    added constants that relate to the different debian flavors we may see

--- src/BuildScriptGenerator.Common/OsTypes.cs
+++ src/BuildScriptGenerator.Common/OsTypes.cs
@@ -1,10 +1,12 @@
-﻿namespace Microsoft.Oryx.BuildScriptGenerator.Common
+// This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Common
 {
     public static class OsTypes
     {
         public const string DebianStretch = "stretch";
-        public const string DebianBuster = "buster";
         public const string DebianBullseye = "bullseye";
-        public const string DebianVsoFocal = "vso-focal";
+        public const string DebianBuster = "buster";
+        public const string UbuntuFocalScm = "focal-scm";
     }
 }
