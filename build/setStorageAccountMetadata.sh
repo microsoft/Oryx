c19d6d3444b8b0709c40fb9f00d7b99e70e2222a
Paul Dorsch <pauldorsch@microsoft.com>
2022-08-03 09:43:35 -0400

    added some logic

--- build/setStorageAccountMetadata.sh
+++ build/setStorageAccountMetadata.sh
@@ -7,18 +7,20 @@ xml=$(curl -k --silent "$storageAccount/$container?restype=container&comp=list")
 
 nonStretchRegex="$container-(focal-scm|bullseye|buster)-(.*?).tar.gz"
 stretchRegex="$container-(.*?).tar.gz"
-grep -oPm1 "(?<=<Name>)[^<]+" <<< "$xml" | while read -r line ; do
-    ostype=$(echo $line | awk "{match(\$0,/$nonStretchRegex/,a);print a[1]}" )
-    version=$(echo $line | awk "{match(\$0,/$nonStretchRegex/,a);print a[2]}")
+grep -oPm1 "(?<=<Name>)[^<]+" <<< "$xml" | while read -r fileName ; do
+    ostype=$(echo $fileName | awk "{match(\$0,/$nonStretchRegex/,a);print a[1]}" )
+    version=$(echo $fileName | awk "{match(\$0,/$nonStretchRegex/,a);print a[2]}")
     if [[ -z $ostype ]] || [[ -z $version ]] ; then
-        version=$(echo $line | awk "{match(\$0,/$stretchRegex/,a);print a[1]}")
+        version=$(echo $fileName | awk "{match(\$0,/$stretchRegex/,a);print a[1]}")
         if [[ ! -z $version ]] ; then
             ostype="stretch"
         fi
     fi
     if [[ -z $ostype ]] || [[ -z $version ]] ; then
-        echo "No match found for $line"
-    else
-        echo "ostype: $ostype     version: $version"
+        echo "No match found for $fileName. Skipping..."
+        continue
     fi
+    metadata=$(curl -I --silent "$storageAccount/$container/$fileName?comp=metadata" | grep x-ms-meta)
+    echo "Match found.     ostype: $ostype     version: $version       metadata: $metadata"
+
 done
