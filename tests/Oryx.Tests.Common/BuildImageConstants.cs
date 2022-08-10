46267955443f1d5e2eb524ea08330658f576c128
Paul Dorsch <pauldorsch@microsoft.com>
2022-08-10 07:07:27 -0400

    starting a constants file for images to test on

---
+++ tests/Oryx.Tests.Common/BuildImageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Oryx.Tests.Common
+{
+    public static class BuildImageConstants
+    {
+        public const string BuildRepo = "build";
+        public const string CliRepo = "cli";
+        public const string CliBusterRepo = "cli-buster";
+
+
+    }
+}
