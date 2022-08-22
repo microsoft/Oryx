175c5ceddd014a5588c78a85246a4d8e7927ef98
Zixuan Qian <zqian@microsoft.com>
2022-08-22 15:40:14 -0700

    Add more tests and sample app

--- tests/Oryx.BuildImage.Tests/Node/NodeDynamicInstallationTest.cs
+++ tests/Oryx.BuildImage.Tests/Node/NodeDynamicInstallationTest.cs
@@ -369,7 +369,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
             {
-                ImageId = _imageHelper.GetBuildImage(imageTag),
+                ImageId = _imageHelper.GetBuildImage(buildImageName),
                 Volumes = new List<DockerVolume> { volume },
                 CommandToExecuteOnRun = "/bin/bash",
                 CommandArguments = new[] { "-c", script }
