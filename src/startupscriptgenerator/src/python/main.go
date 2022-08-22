03b0ed0c38271d2b26beb30ee63ce08a762eeba6
Cormac McCarthy <corm@microsoft.com>
2022-08-22 13:32:04 -0700

    Remove setupEnv call for Python 3.10 which now builds with the same commands

--- src/startupscriptgenerator/src/python/main.go
+++ src/startupscriptgenerator/src/python/main.go
@@ -111,7 +111,7 @@ func main() {
 			fmt.Sprintf("echo %s/lib > /etc/ld.so.conf.d/python.conf\n", pythonInstallationRoot))
 		scriptBuilder.WriteString("ldconfig\n")
 
-		if strings.HasPrefix(buildManifest.PythonVersion, "3.") && !strings.HasPrefix(buildManifest.PythonVersion, "3.10") {
+		if strings.HasPrefix(buildManifest.PythonVersion, "3.") && !strings.HasPrefix(buildManifest.PythonVersion, "3.11") {
 			scriptBuilder.WriteString(
 				fmt.Sprintf("cd %s/bin\n", pythonInstallationRoot))
 			scriptBuilder.WriteString("rm -f python\n")
