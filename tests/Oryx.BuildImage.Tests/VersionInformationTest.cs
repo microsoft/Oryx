916367df7e385d06dc363555c9f4446942cb9bfa
Paul Dorsch <pauldorsch@microsoft.com>
2022-08-23 11:59:31 -0400

    resolve vso focal issue

--- tests/Oryx.BuildImage.Tests/VersionInformationTest.cs
+++ tests/Oryx.BuildImage.Tests/VersionInformationTest.cs
@@ -581,7 +581,7 @@ namespace Microsoft.Oryx.BuildImage.Tests
 
             // Arrange
             var phpVersion = PhpVersions.Php73Version;
-            if (buildImageName.Contains("oryxdevmcr.azurecr.io/public/oryx/build:vso-focal"))
+            if (buildImageName.Contains("oryxdevmcr.azurecr.io/public/oryx/build:ubuntu-vso-focal"))
             {
                 phpVersion = PhpVersions.Php81Version;
             }
