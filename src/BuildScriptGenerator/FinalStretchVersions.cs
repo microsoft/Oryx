7695943165758661aa5d4b199daa9d587b227021
Paul Dorsch <107068277+pauld-msft@users.noreply.github.com>
2022-08-30 09:07:41 -0400

    Final stretch version constants for dotnet 3.1 and 6.0 (#1539)
    * added final stretch versions to mark end of uploading stretch sdks
    
    * attempt to fix tests
    
    * fix lts versions dockerfile
    
    * saved file to fix final test
    
    * pr feedback: renamed netcore31 runtime and fixed test to specific version

---
+++ src/BuildScriptGenerator/FinalStretchVersions.cs
@@ -0,0 +1,14 @@
+// This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Common
+{
+    public static class FinalStretchVersions
+    {
+        public const string FinalStretchDotNetCore31SdkVersion = "3.1.420";
+        public const string FinalStretchDotNetCoreApp31RunTimeVersion = "3.1.26";
+        public const string FinalStretchAspnetCoreApp31RunTimeVersion = "3.1.26";
+        public const string FinalStretchDotNet60SdkVersion = "6.0.301";
+        public const string FinalStretchDotNetCoreApp60RunTimeVersion = "6.0.6";
+        public const string FinalStretchAspnetCoreApp60RunTimeVersion = "6.0.6";
+    }
+}
