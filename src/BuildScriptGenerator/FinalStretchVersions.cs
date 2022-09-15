f2ee0fafad3bc52750d8beee8b06f0b948e14bea
Sneha Parthasarathy <snehapar@microsoft.com>
2022-09-15 11:56:32 -0700

    Updated final stretch constant name

--- src/BuildScriptGenerator/FinalStretchVersions.cs
+++ src/BuildScriptGenerator/FinalStretchVersions.cs
@@ -7,8 +7,11 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string FinalStretchDotNetCore31SdkVersion = "3.1.420";
         public const string FinalStretchDotNetCoreApp31RunTimeVersion = "3.1.26";
         public const string FinalStretchAspnetCoreApp31RunTimeVersion = "3.1.26";
-        public const string FinalStretchDotNet60SdkVersion = "7.0.100-preview.6.22352.1";
-        public const string FinalStretchDotNetCoreApp60RunTimeVersion = "7.0.0-preview.6.22324.4";
-        public const string FinalStretchAspnetCoreApp60RunTimeVersion = "7.0.0-preview.6.22330.3";
+        public const string FinalStretchDotNet60SdkVersion = "6.0.301";
+        public const string FinalStretchDotNetCoreApp60RunTimeVersion = "6.0.6";
+        public const string FinalStretchAspnetCoreApp60RunTimeVersion = "6.0.6";
+        public const string FinalStretchDotNet70SdkVersion = "7.0.100-preview.6.22352.1";
+        public const string FinalStretchDotNetCoreApp70RunTimeVersion = "7.0.0-preview.6.22324.4";
+        public const string FinalStretchAspnetCoreApp70RunTimeVersion = "7.0.0-preview.6.22330.3";
     }
 }
