9231b6695513ee2019fa6bfe8698df491b2a1a9d
REDMOND\williamhe <williamhe@microsoft.com>
2022-09-23 09:20:11 -0700

    Add license header

--- build/tools/Automation/HttpClientHelper.cs
+++ build/tools/Automation/HttpClientHelper.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
