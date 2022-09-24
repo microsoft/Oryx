0efc278a5896e0eb346c2461f1483dfe72f4d883
waliMSFT <105239794+waliMSFT@users.noreply.github.com>
2022-09-23 21:33:44 -0400

    reverted base image to Buster for Node (#1575)

--- images/runtime/node/generateDockerfiles.sh
+++ images/runtime/node/generateDockerfiles.sh
@@ -13,25 +13,34 @@ source $REPO_DIR/build/__nodeVersions.sh
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 declare -r DOCKERFILE_TEMPLATE="$DIR/template.Dockerfile"
 declare -r RUNTIME_BASE_IMAGE_TAG_PLACEHOLDER="%RUNTIME_BASE_IMAGE_TAG%"
-declare -r NODE_BULLSEYE_VERSION_ARRAY=($NODE18_VERSION $NODE16_VERSION $NODE14_VERSION)
+declare -r NODE_BULLSEYE_VERSION_ARRAY=($NODE18_VERSION)
+declare -r NODE_BUSTER_VERSION_ARRAY=($NODE16_VERSION $NODE14_VERSION)
 
-echo "$1"
+ImageDebianFlavor="$1"
+echo "node baseimage type: $ImageDebianFlavor"
+
+VERSIONS_DIRECTORY=()
+
+if [ "$ImageDebianFlavor" == "bullseye" ];then
+    VERSIONS_DIRECTORY=("${NODE_BULLSEYE_VERSION_ARRAY[@]}")
+elif [ "$ImageDebianFlavor" == "buster" ];then
+    VERSIONS_DIRECTORY=("${NODE_BUSTER_VERSION_ARRAY[@]}")
+fi
 
 cd $DIR
-if [ "$1" == "bullseye" ];then
-	for NODE_BULLSEYE_VERSION in "${NODE_BULLSEYE_VERSION_ARRAY[@]}"
-	do
-		IFS='.' read -ra SPLIT_VERSION <<< "$NODE_BULLSEYE_VERSION"
-		VERSION_DIRECTORY="${SPLIT_VERSION[0]}"
 
-		echo "Generating Dockerfile for bullseye based image $VERSION_DIRECTORY..."
+for NODE_VERSION_DIRECTORY  in "${VERSIONS_DIRECTORY[@]}"
+do
+	IFS='.' read -ra SPLIT_VERSION <<< "$NODE_VERSION_DIRECTORY"
+	VERSION_DIRECTORY="${SPLIT_VERSION[0]}"
 
-		TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/$1.Dockerfile"
-		cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
+	echo "Generating Dockerfile for $ImageDebianFlavor based image $VERSION_DIRECTORY..."
 
-		echo "Generating Dockerfile for bullseye based images..."
-		# Replace placeholders
-		RUNTIME_BASE_IMAGE_TAG="node-$VERSION_DIRECTORY-$NODE_RUNTIME_BASE_TAG"
-		sed -i "s|$RUNTIME_BASE_IMAGE_TAG_PLACEHOLDER|$RUNTIME_BASE_IMAGE_TAG|g" "$TARGET_DOCKERFILE"
-	done
-fi
+	TARGET_DOCKERFILE="$DIR/$VERSION_DIRECTORY/$1.Dockerfile"
+	cp "$DOCKERFILE_TEMPLATE" "$TARGET_DOCKERFILE"
+
+	echo "Generating Dockerfile for $ImageDebianFlavor based images..."
+	# Replace placeholders
+	RUNTIME_BASE_IMAGE_TAG="node-$VERSION_DIRECTORY-$NODE_RUNTIME_BASE_TAG"
+	sed -i "s|$RUNTIME_BASE_IMAGE_TAG_PLACEHOLDER|$RUNTIME_BASE_IMAGE_TAG|g" "$TARGET_DOCKERFILE"
+done
