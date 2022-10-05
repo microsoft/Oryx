988b4f143367a60fe79d2068377e55dafa133b4d
Zixuan Qian <zqian@microsoft.com>
2022-10-05 03:32:25 -0700

    Update DefaultDockerfileGenerator.cs

--- src/BuildScriptGenerator/DefaultDockerfileGenerator.cs
+++ src/BuildScriptGenerator/DefaultDockerfileGenerator.cs
@@ -69,7 +69,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator
                 {
                     createScriptArguments.Add("bindPort", this.commonOptions.BindPort);
                 }
-                
+
                 if (!string.IsNullOrEmpty(this.commonOptions.BindPort2))
                 {
                     createScriptArguments.Add("bindPort2", this.commonOptions.BindPort2);
