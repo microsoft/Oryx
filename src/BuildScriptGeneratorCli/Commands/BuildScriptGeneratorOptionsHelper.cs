3399b2d82037d2b9cf8075bc25025ec52277883d
Paul Dorsch <pauldorsch@microsoft.com>
2022-10-26 16:51:34 -0400

    add support for debian flavor to build-script

--- src/BuildScriptGeneratorCli/Commands/BuildScriptGeneratorOptionsHelper.cs
+++ src/BuildScriptGeneratorCli/Commands/BuildScriptGeneratorOptionsHelper.cs
@@ -6,7 +6,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Oryx.BuildScriptGenerator;
+using Microsoft.Oryx.BuildScriptGenerator.Common;
 using Microsoft.Oryx.BuildScriptGenerator.Exceptions;
 
 namespace Microsoft.Oryx.BuildScriptGeneratorCli
@@ -79,6 +81,29 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
             if (scriptOnly.HasValue)
             {
                 options.ScriptOnly = scriptOnly.Value;
+            }
+
+            // For debian flavor, we first check for existance of an environment variable
+            // which contains the os type. If this does not exist, parse the
+            // FilePaths.OsTypeFileName file for the correct flavor
+            if (string.IsNullOrWhiteSpace(options.DebianFlavor))
+            {
+                var ostypeFilePath = Path.Join("/opt", "oryx", FilePaths.OsTypeFileName);
+                if (File.Exists(ostypeFilePath))
+                {
+                    // these file contents are in the format <OS_type>|<Os_version>, e.g. DEBIAN|BULLSEYE
+                    // we want the Os_version part only, as all lowercase
+                    var fullOsTypeFileContents = File.ReadAllText(ostypeFilePath);
+                    options.DebianFlavor = fullOsTypeFileContents.Split("|").TakeLast(1).SingleOrDefault().Trim().ToLowerInvariant();
+                }
+                else
+                {
+                    // If we cannot resolve the debian flavor, error out as we will not be able to determine
+                    // the correct SDKs to pull
+                    var errorMessage = $"Error: Image debian flavor not found in DEBIAN_FLAVOR environment variable or the " +
+                        $"{Path.Join("/opt", "oryx", FilePaths.OsTypeFileName)} file. Exiting...";
+                    throw new InvalidUsageException(errorMessage);
+                }
             }
 
             options.Properties = ProcessProperties(properties);
