ca0c90b53289ac9906f29b217aa75bd07024ad28
Wali Bhuiyan <mbhuiyan@microsoft.com>
2022-10-31 15:31:46 -0400

    private binaries changes

--- src/BuildScriptGenerator.Common/SdkStorageConstants.cs
+++ src/BuildScriptGenerator.Common/SdkStorageConstants.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string TestingSdkStorageUrlKeyName = "ORYX_TEST_SDK_STORAGE_URL";
         public const string DevSdkStorageBaseUrl = "https://oryxsdksdev.blob.core.windows.net";
         public const string SandboxSdkStorageBaseUrl = "https://oryxsdkssandbox.blob.core.windows.net";
+        public const string PrivateSdkStorageBaseUrl = "https://oryxsdksprivate.blob.core.windows.net";
         public const string ProdSdkStorageBaseUrl = "https://oryxsdksprod.blob.core.windows.net";
         public const string ProdBackupSdkStorageBaseUrl = "https://oryxsdksprodbackup.blob.core.windows.net";
         public const string ProdSdkCdnStorageBaseUrl = "https://oryx-cdn.microsoft.io";
