85eff71a6c55550a36c56d49fee828111a3ce680
Paul Dorsch <pauldorsch@microsoft.com>
2022-11-23 08:23:21 -0500

    updated php 7.4 version to most recent

--- src/BuildScriptGenerator/PhpVersions.cs
+++ src/BuildScriptGenerator/PhpVersions.cs
@@ -21,9 +21,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Php80Version = "8.0.22";
         public const string Php80Keys = "1729F83938DA44E27BA0F4D3DBDB397470D12172 BFDDD28642824F8118EF77909B67A5C12229118F";
         public const string Php80TarSha256 = "130937c0fa3050cd33d6c415402f6ccbf0682ae83eb8d39c91164224ddfe57f1";
-        public const string Php74Version = "7.4.30";
+        public const string Php74Version = "7.4.33";
         public const string Php74Keys = "42670A7FE4D0441C8E4632349E4FDC074A4EF02D 5A52880781F755608BF815FC910DEB46F53EA312";
-        public const string Php74TarSha256 = "ea72a34f32c67e79ac2da7dfe96177f3c451c3eefae5810ba13312ed398ba70d";
+        public const string Php74TarSha256 = "924846abf93bc613815c55dd3f5809377813ac62a9ec4eb3778675b82a27b927";
         public const string Php73Version = "7.3.27";
         public const string Php73Keys = "CBAF69F173A0FEA4B537F470D66C9593118BCCB6 F38252826ACD957EF380D39F2F7956BC5DA04B5D";
         public const string Php73TarSha256 = "65f616e2d5b6faacedf62830fa047951b0136d5da34ae59e6744cbaf5dca148d";
