ef265dfe5173a21557a0daa10100bd8e1aaf07f9
REDMOND\williamhe <williamhe@microsoft.com>
2022-12-14 13:55:02 -0800

    Update filename

--- build/tools/Automation/checkPipelineStatus.sh
+++ build/tools/Automation/checkPipelineStatus.sh
@@ -25,8 +25,8 @@ while [ "$retryCount" -le "$maxRetries" ]
 do
 	echo "retry $retryCount"
 	# status.json contains the buildNumber, used later in the workflow
-	az pipelines runs show --id ${pipelineInvocationId} --organization https://devdiv.visualstudio.com/ --project DevDiv > result.json
-	result=$( cat result.json | jq ".result" | tr -d '"' )
+	az pipelines runs show --id ${pipelineInvocationId} --organization https://devdiv.visualstudio.com/ --project DevDiv > status.json
+	result=$( cat status.json | jq ".result" | tr -d '"' )
 	echo $result
 	if [[ "$result" == "succeeded" ]]; then
 		return
