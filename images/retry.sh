d94a92e5d6b03f66cb9b7ff96857875a8a6c8d35
REDMOND\williamhe <williamhe@microsoft.com>
2022-12-21 17:57:29 -0800

    Revert retry for images

--- images/retry.sh
+++ images/retry.sh
@@ -8,22 +8,13 @@
 # and sleep & retry if there's a failure
 # $1 
 #	parameter contains the full command to be executed
-maxRetries=3
-if [[ -n "${MAX_RETRIES}" ]]; then
-  maxRetries=${MAX_RETRIES}
-fi
-
-timeoutSeconds=10
-if [[ -n "${TIMEOUT_SECONDS}" ]]; then
-  timeoutSeconds=${TIMEOUT_SECONDS}
-fi
-
 retryCount=0
-while [ "$retryCount" -le "$maxRetries" ]
+retries=5
+while [ "$retryCount" -le "$retries" ]
 do
 	echo "retry $retryCount"
 	$1 && break
-	echo "Failed command: $1"
+	echo "error executing: $1"
 	retryCount=$((retryCount+1)) 
-	sleep ${timeoutSeconds}
+	sleep 15
 done
