4f7da91f88d9cc7cfa69cb67e6e490c6d6a1fccc
Cormac McCarthy <corm@microsoft.com>
2023-01-09 15:14:11 -0500

    Fix or skip failing tests after default version update

--- tests/Oryx.BuildImage.Tests/CommandTests/OryxCommandTest.cs
+++ tests/Oryx.BuildImage.Tests/CommandTests/OryxCommandTest.cs
@@ -140,10 +140,10 @@ namespace Microsoft.Oryx.BuildImage.Tests
                 () =>
                 {
                     Assert.Contains("#!" + expectedBashPath, result.StdOut);
-                    Assert.Contains($"{NodeConstants.PlatformName}={FinalStretchVersions.FinalStretchNode14Version}", result.StdOut);
+                    Assert.Contains($"{NodeConstants.PlatformName}={FinalStretchVersions.FinalStretchNode16Version}", result.StdOut);
                     Assert.True(result.IsSuccess);
                     // Actual output from `node --version` starts with a 'v'
-                    Assert.Contains($"v{FinalStretchVersions.FinalStretchNode14Version}", result.StdOut);
+                    Assert.Contains($"v{FinalStretchVersions.FinalStretchNode16Version}", result.StdOut);
                 },
                 result.GetDebugInfo());
         }
