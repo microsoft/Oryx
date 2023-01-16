caa168f331278d131308afe72d6243b86118a063
Sneha Parthasarathy <snehapar@microsoft.com>
2023-01-16 01:01:44 -0800

    Changed image name for cli-stretch

--- tests/Oryx.Tests.Common/Settings.cs
+++ tests/Oryx.Tests.Common/Settings.cs
@@ -17,8 +17,9 @@ namespace Microsoft.Oryx.Tests.Common
         public const string JamStackBullseyeBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/build:azfunc-jamstack-debian-bullseye";
         public const string JamStackBusterBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/build:azfunc-jamstack-debian-buster";
         public const string JamStackBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/build:azfunc-jamstack-debian-stretch";
-        public const string CliBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/cli:debian-stretch";
+        public const string CliBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/cli-stretch:debian-stretch";
         public const string CliBusterBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/cli-buster:debian-buster";
+        public const string CliBullseyeBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/cli-bullseye:debian-buster";
         public const string LtsVerionsBusterBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/build:lts-versions-debian-buster";
         public const string VsoUbuntuBuildImageName = "oryxdevmcr.azurecr.io/public/oryx/build:vso-ubuntu-focal";
 
