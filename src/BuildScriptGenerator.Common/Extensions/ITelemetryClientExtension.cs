ef6d0f29b3c349d34ecbe116d382215b39785758
snehapar9 <snehapar@microsoft.com>
2023-02-02 10:55:12 -0800

    Added wrapper for telemetry client

---
+++ src/BuildScriptGenerator.Common/Extensions/ITelemetryClientExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Common.Extensions
+{
+    public interface ITelemetryClientExtension
+    {
+        TelemetryClient GetTelemetryClient();
+    }
+
+    public class TelemetryClientExtension : ITelemetryClientExtension
+    {
+        private string connectionString;
+
+        public TelemetryClientExtension(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        TelemetryClient ITelemetryClientExtension.GetTelemetryClient()
+        {
+            var config = new TelemetryConfiguration()
+            {
+                ConnectionString = this.connectionString,
+            };
+            return new TelemetryClient(config);
+        }
+    }
+}
