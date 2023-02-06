57fdddecc17666473779f47ee1550ff9711f46f9
Sneha Parthasarathy <snehapar@microsoft.com>
2023-02-06 13:29:06 -0800

    Added Application Insights to service provider builder

--- src/BuildScriptGeneratorCli/ServiceProviderBuilder.cs
+++ src/BuildScriptGeneratorCli/ServiceProviderBuilder.cs
@@ -4,16 +4,10 @@
 // --------------------------------------------------------------------------------------------
 
 using System;
-using System.IO;
-using JetBrains.Annotations;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Oryx.BuildScriptGenerator;
-using Microsoft.Oryx.BuildScriptGenerator.Common;
-using NLog;
-using NLog.Config;
-using NLog.Extensions.Logging;
 
 namespace Microsoft.Oryx.BuildScriptGeneratorCli
 {
@@ -26,21 +20,17 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
 
         public ServiceProviderBuilder(string logFilePath = null, IConsole console = null)
         {
-            LogManager.Configuration = BuildNLogConfiguration(logFilePath);
-            LogManager.ReconfigExistingLoggers();
-
             this.serviceCollection = new ServiceCollection();
+            var connectionString = string.Empty;
             this.serviceCollection
                 .AddBuildScriptGeneratorServices()
                 .AddCliServices(console)
                 .AddLogging(builder =>
                 {
+                    builder.AddApplicationInsights(
+                         configureTelemetryConfiguration: (config) => config.ConnectionString = connectionString,
+                         configureApplicationInsightsLoggerOptions: (options) => { });
                     builder.SetMinimumLevel(Extensions.Logging.LogLevel.Trace);
-                    builder.AddNLog(new NLogProviderOptions
-                    {
-                        CaptureMessageTemplates = true,
-                        CaptureMessageProperties = true,
-                    });
                 });
         }
 
@@ -59,49 +49,6 @@ namespace Microsoft.Oryx.BuildScriptGeneratorCli
         public IServiceProvider Build()
         {
             return this.serviceCollection.BuildServiceProvider();
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Cannot prematurely dispose of Application insights objects.")]
-        private static LoggingConfiguration BuildNLogConfiguration([CanBeNull] string logPath)
-        {
-            var config = new LoggingConfiguration();
-
-            var disableTelemetryEnvVariableValue = Environment.GetEnvironmentVariable(
-                LoggingConstants.OryxDisableTelemetryEnvironmentVariableName);
-            _ = bool.TryParse(disableTelemetryEnvVariableValue, out bool disableTelemetry);
-
-            var aiKey = disableTelemetry ? string.Empty : Environment.GetEnvironmentVariable(
-                LoggingConstants.ApplicationInsightsInstrumentationKeyEnvironmentVariableName);
-            if (!string.IsNullOrWhiteSpace(aiKey))
-            {
-                var aiTarget = new ApplicationInsights.NLogTarget.ApplicationInsightsTarget()
-                {
-                    Name = "ai",
-                    InstrumentationKey = aiKey,
-                };
-                config.AddTarget(aiTarget);
-                config.AddRuleForAllLevels(aiTarget);
-            }
-
-            bool hasLogPath = !string.IsNullOrWhiteSpace(logPath);
-            if (hasLogPath || config.AllTargets.Count == 0)
-            {
-                if (!hasLogPath)
-                {
-                    logPath = LoggingConstants.DefaultLogPath;
-                }
-
-                // Default layout: "${longdate}|${level:uppercase=true}|${logger}|${message}"
-                var fileTarget = new NLog.Targets.FileTarget("file")
-                {
-                    FileName = Path.GetFullPath(logPath),
-                    Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${exception:format=ToString}",
-                };
-                config.AddTarget(fileTarget);
-                config.AddRuleForAllLevels(fileTarget);
-            }
-
-            return config;
         }
     }
 }
