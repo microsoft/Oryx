a21a42ccbbe962f134845e0609071fed61acbbd2
william-msft <86327553+william-msft@users.noreply.github.com>
2023-02-08 09:29:58 -0800

    Williamhe/Python.h error workaround (#1865)
    * Update requirementstxt
    
    * Update requirementstxt
    
    * Add python cffi libary
    
    * Add additional libaries to python
    
    * Remove libffi6, not supported
    
    * Add new library
    
    * Update libraries
    
    * Update pip gcc flags
    
    * Add packages to build all optional python modules
    
    * Add additional python module requirements
    
    * Add integration test
    
    * Update to test version 3.11.1
    
    * Update after running generateConstants.sh script which distributes constants.yaml constants across Oryx repo
    
    * Add python 3.11.1
    
    * Remove bad package causing pipeline failure
    
    * Remove bad package and add comments
    
    * Remove unnecessary flag
    
    * Remove sandbox storage account
    
    * Remove additional packages for runtime
    
    * Update dependencies
    
    * Revert dependencies
    
    * Update python runtime tag
    
    * Update python version
    
    * Update python version
    
    * Add 3.8.16 python back
    
    * Update runtime tag
    
    * Update runtime tag

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
 {
     public static class PythonVersions
     {
-        public const string PythonRuntimeBaseTag = "20221116.1";
+        public const string PythonRuntimeBaseTag = "20230208.7";
         public const string PipVersion = "21.2.4";
         public const string Python27Version = "2.7.18";
         public const string Python36Version = "3.6.15";
@@ -14,7 +14,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Python38Version = "3.8.16";
         public const string Python39Version = "3.9.15";
         public const string Python310Version = "3.10.8";
-        public const string Python311Version = "3.11.0";
+        public const string Python311Version = "3.11.1";
         public static readonly List<string> RuntimeVersions = new List<string> { "2.7", "3.6", "3.8", "3.9", "3.10", "3.11" };
     }
 }
