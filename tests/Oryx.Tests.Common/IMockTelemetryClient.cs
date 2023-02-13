80aa472c0f6c24a006c22f22ee787667873ea2b2
snehapar9 <snehapar@microsoft.com>
2023-02-13 13:06:41 -0800

    Mock telemetry client for testing

---
+++ tests/Oryx.Tests.Common/IMockTelemetryClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+
+public interface ITelemetyClientMock
+{}
+
+public class TelemetryClientMock : ITelemetyClientMock
+{
+    public string connectionString { get; set; }
+    private TelemetryClient telemetryClient;
+    private TelemetryConfiguration telemetryConfigutration = new TelemetryConfiguration();
+    public TelemetryClientMock()
+    {
+        telemetryClient = new TelemetryClient(telemetryConfigutration);
+    }
+}
