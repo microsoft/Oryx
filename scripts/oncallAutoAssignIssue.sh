f00ed5ec7ea6ef89c0a0472e10c61ea5c46d074e
harrli <harrli@microsoft.com>
2023-03-03 17:34:56 -0800

    Address comments

--- scripts/oncallAutoAssignIssue.sh
+++ scripts/oncallAutoAssignIssue.sh
@@ -5,44 +5,52 @@ autoIssue() {
   token=$2
   issueNum=$3
   # Check if the user can be assigned
+  echo "Attempting to assign user $assigneeName to issue $issueNum"
   checkAccess=$(curl --silent --output /dev/null --write-out "%{http_code}" -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/microsoft/Oryx/assignees/$assigneeName)
   if [[ "$checkAccess" =~ ^2 ]]; then
     res=$(curl --silent --output /dev/null --write-out "%{http_code}" -X POST "https://api.github.com/repos/microsoft/Oryx/issues/$issueNum/assignees" -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" -d "{\"assignees\": ["\"$assigneeName\""]}")
     # Check for curl response status
     if [[ "$res" =~ ^2 ]]; then
-      return 0
+      exit 0
     else
       echo "Error: Failed to assign isssue $issueNum. Check token and issue status."
       exit 1
     fi
   else
     echo "Error: Engineer Access denied."
-    exit 1
+    return 1
   fi
 }
 
 tryAssignIssueToCustomOncall() {
-  input=$1
-  today=$2
-  token=$3
-  issue=$4
+  today=$1
+  token=$2
+  issue=$3
+  input="$CUSTOM_ONCALL_ROTATION"
+  input="${input//$'\n'/''}"
+  input="${input//$'\r'/';'}"
   if [ -z "$input" ]; then
     return 1
   fi
   # Custom oncall rotation defined in repo variable. Using format:
-  # "{github-username},{start-date},{end-date}"
-  # Double quote is required.
-  IFS="," read -ra arr <<< "$1"
-  name=${arr[0]}
-  startDate=${arr[1]}
-  endDate=${arr[2]}
-  startDate=$(date -d $startDate +%s)
-  endDate=$(date -d $endDate +%s)
-  if [ "$today" -ge "$startDate" ] && [ "$today" -le "$endDate" ]; then
-    echo "Custom oncall found"
-    autoIssue $name $token $issue
-    exit 0
-  fi
+  # {github-username},{start-date},{end-date}
+
+  # Devide each entry by ";"
+  IFS=';' read -ra arr <<< "$input"
+  for i in "${arr[@]}"
+  do
+    # Devide each field by ","
+    IFS=',' read -ra entry <<< "$i"
+    name=$(echo ${entry[0]})
+    startDate=$(echo ${entry[1]})
+    endDate=$(echo ${entry[2]})
+    startDate=$(date -d $startDate +%s)
+    endDate=$(date -d $endDate +%s)
+    if [ "$today" -ge "$startDate" ] && [ "$today" -le "$endDate" ]; then
+      echo "Custom oncall found for $name"
+      autoIssue $name $token $issue
+    fi
+  done
 }
 
 # https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
@@ -55,10 +63,6 @@ while (( "$#" )); do
     ;;
     --issue) # Issue number flag
     issueNum=$2
-    shift 2
-    ;;
-    --custom) # Custom shift flag
-    customRotation=$2
     shift 2
     ;;
     --)
@@ -82,12 +86,12 @@ d=$(((today - anchorDate)/60/60/24/7))
 pos=`echo "$d%$oncallArrLen" | bc`
 currentOncall=`echo ${ONCALLS[$pos]}`
 
-# Check if custom rotation argument is present or not
-if [[ -v customRotation ]]; then
+# Check if custom rotation env var is present or not
+if [ -n "$CUSTOM_ONCALL_ROTATION" ]; then
   # If yes, try assign it to custom oncall engineer
   # Function exit 0 when successfully assigned it to custom oncall engineer
-  tryAssignIssueToCustomOncall $customRotation $today $authToken $issueNum
+  tryAssignIssueToCustomOncall $today $authToken $issueNum
 fi
 # If failed, then assign to current oncall.
 autoIssue $currentOncall $authToken $issueNum
-exit 0
+exit 1
