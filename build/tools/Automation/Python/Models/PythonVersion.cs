83f4e5337d676c55b04a136a7e022f2aaf831f5a
william-msft <86327553+william-msft@users.noreply.github.com>
2023-03-03 20:47:47 -0800

    Williamhe/1732771 refactor automation (#1888)
    * Add refactored dotnet code
    
    * Add multiple platform support
    
    * Add a dotnet test
    
    * Update invocation
    
    * Update method location
    
    * Rename automation tests
    
    * Update spacing
    
    * Update httpclient to extension
    
    * Update with review feedback changes
    
    * Add error handling
    
    * Add more documentation
    
    * Update null case
    
    * Add writing method to YamlFileService
    
    * Rename to DotNetAutomator
    
    * Rename to DotNetAutomator
    
    * Update PR feedback
    
    * Update PR feedback
    
    * Update with addiontional refactoring
    
    * Update to ignore pre-release
    
    * Add env var
    
    * Remove unnecessary field
    
    * Add unit tests
    
    * Update var names
    
    * Add FileService
    
    * Add error handling
    
    * Update unit test
    
    * Add FileService unit test
    
    * Address PR feedback
    
    * Add tests
    
    * Update with minor change
    
    * Fix spacing
    
    * Update workflow
    
    * Add HttpServiceExtension
    
    * Update unit tests
    
    * Update unit tests
    
    * Rename HttpServiceExtension
    
    * Update with minor change
    
    * Update tests
    
    * Update minor changes
    
    * Add new line
    
    * Update order
    
    * Update with minor changes
    
    * Remove dot
    
    * Add default value
    
    * Update PR feedback
    
    * Update PR feedback
    
    * Update to block pre-release
    
    * Revert "Add runtime build number"
    
    This reverts commit 954eddba895bfdb39b6ee5ce47cfde397bcd6cb6.
    
    * Add blocked list to parameter
    
    * Williamhe/1732771 add python automation (#1896)
    
    * Update azure pipeline trigger
    
    * Add python automation
    
    * Update var name
    
    * Address comments
    
    * Update model
    
    * Address PR feedback
    
    * Update workflow
    
    * Update with minor change
    
    * Test
    
    * Update env vars
    
    * Update env vars
    
    * Update with minor changes
    
    * Add READ.me
    
    * Update python automator to use httpServiceExtension
    
    * Update with minor change
    
    * Update to rename HttpServiceExtension
    
    * Remove test parameter
    
    * Fix pipeline build error
    
    * Add storage account control through env var

---
+++ build/tools/Automation/Python/Models/PythonVersion.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Oryx.Automation.Python.Models
+{
+    public class PythonVersion
+    {
+        public string Version { get; set; } = string.Empty;
+
+        public string GpgKey { get; set; } = string.Empty;
+    }
+}
