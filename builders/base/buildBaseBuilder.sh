ab747e952ca29be63cae8396e482b29c05d9bcd3
Paul Dorsch <pauldorsch@microsoft.com>
2023-03-17 11:52:39 -0400

    add to the rest of the pipelines and pull the cli builder before retagging it

--- builders/base/buildBaseBuilder.sh
+++ builders/base/buildBaseBuilder.sh
@@ -79,6 +79,7 @@ function replaceRepo() {
 
 if [ -z $cliBuilderImage ]; then
     cliBuilderImage="$destinationFqdn/public/oryx/cli:builder-debian-buster-$builderImageVersion"
+    docker pull $cliBuilderImage
     mcrCliBuilderImage=$(replaceRepo "$cliBuilderImage" "$MCR_CLI_IMAGE_REPO")
 fi
 
