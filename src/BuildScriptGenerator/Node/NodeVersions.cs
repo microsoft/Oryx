c0cc6b6593c4dcf557cd1695adbc7c146b559db9
Wali Bhuiyan <mbhuiyan@microsoft.com>
2023-04-11 06:20:59 -0400

    versions update of PHP, Node, GO

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -13,10 +13,10 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node6Version = "6.17.1";
         public const string Node8Version = "8.17.0";
         public const string Node10Version = "10.23.0";
-        public const string Node12Version = "12.22.11";
-        public const string Node14Version = "14.21.2";
-        public const string Node16Version = "16.19.0";
-        public const string Node18Version = "18.14.0";
+        public const string Node12Version = "12.22.12";
+        public const string Node14Version = "14.21.3";
+        public const string Node16Version = "16.20.0";
+        public const string Node18Version = "18.15.0";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
         public const string NpmVersion = "6.14.15";
