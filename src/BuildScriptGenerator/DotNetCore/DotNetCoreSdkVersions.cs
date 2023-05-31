142b9d31f4741e6198434a29ca9352f51e9d5997
Oryx Team <oryx@microsoft.com>
2023-05-31 17:13:33 +0000

    Add new dotnet SDK versions

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string DotNet50SdkVersion = "5.0.408";
         public const string DotNet60SdkVersion = "6.0.408";
         public const string DotNet60RtmSdkVersion = "6-0-100-rtm-21527-11";
-        public const string DotNet70SdkVersion = "7.0.203";
+        public const string DotNet70SdkVersion = "7.0.302";
         public static readonly Dictionary<string, string> RuntimeVersions = new Dictionary<string, string> { { "3.0", "debian-buster" }, { "3.1", "debian-buster" }, { "5.0", "debian-buster" }, { "6.0", "debian-buster" }, { "7.0", "debian-buster" }, { "dynamic", "debian-buster" } };
     }
 }
