5df99452db941bf70dc5035f68af075df492ef47
Wali Bhuiyan <mbhuiyan@microsoft.com>
2023-07-05 22:17:05 -0400

    node version 18.16.1 and 16.20.1 version update

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -15,8 +15,8 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node10Version = "10.23.0";
         public const string Node12Version = "12.22.12";
         public const string Node14Version = "14.21.3";
-        public const string Node16Version = "16.20.0";
-        public const string Node18Version = "18.16.0";
+        public const string Node16Version = "16.20.1";
+        public const string Node18Version = "18.16.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
         public const string NpmVersion = "9.6.4";
