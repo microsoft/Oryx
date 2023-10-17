9d1928c1714116f5396a7e79c0c1c40add469732
Yi Li <109205537+yilims@users.noreply.github.com>
2023-10-17 21:17:31 +0800

    expand jar file to workspace (#2196)
    * expand jar file to workspace
    
    * update image
    
    * check file count for jar file
    
    * unzip jar and war
    
    ---------
    
    Co-authored-by: Yi Li <yili7@microsoft.com>

--- builders/container-apps-internal-registry-demo/startup-script.sh
+++ builders/container-apps-internal-registry-demo/startup-script.sh
@@ -29,6 +29,20 @@ mkdir -p $CNB_APP_DIR
 cd $CNB_APP_DIR
 tar -xzf "$temp_app_source_path"
 
+fileCount=$(ls | wc -l)
+if [ "$fileCount" = "1" ]; then
+  # Find .jar file in the directory
+  jarfile=$(find "$CNB_APP_DIR" -maxdepth 1 -name ".[jw]ar" | head -n 1)
+
+  # unzip it if found
+  if [[ -n $jarfile ]];
+  then 
+    echo "Unzip jar file $jarfile"
+    unzip -qq $jarfile -d $CNB_APP_DIR
+    rm $jarfile
+  fi
+fi
+
 # public cert should be in this env var
 ca_pem_decoded=$(printf "%s" "$REGISTRY_HTTP_TLS_CERTIFICATE" | base64 -d)
 echo "$ca_pem_decoded" >> /usr/local/share/ca-certificates/internalregistry.crt
