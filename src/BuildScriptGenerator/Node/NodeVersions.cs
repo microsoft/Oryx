0383ff4ff05a4ff0f027d884d1e8eb340bf8d79b
harrli <harrli@microsoft.com>
2023-10-24 11:22:40 -0700

    Adding node 20 support for bullseye and bookworm

--- src/BuildScriptGenerator/Node/NodeVersions.cs
+++ src/BuildScriptGenerator/Node/NodeVersions.cs
@@ -17,9 +17,10 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Node
         public const string Node14Version = "14.21.3";
         public const string Node16Version = "16.20.2";
         public const string Node18Version = "18.17.1";
+        public const string Node20Version = "20.8.1";
         public const string NodeAppInsightsSdkVersion = "1.8.7";
         public const string Pm2Version = "4.5.6";
         public const string NpmVersion = "9.6.4";
-        public static readonly List<string> RuntimeVersions = new List<string> { "14-debian-bullseye", "14-debian-buster", "16-debian-bullseye", "16-debian-buster", "18-debian-bullseye", "dynamic-debian-buster" };
+        public static readonly List<string> RuntimeVersions = new List<string> { "14-debian-bullseye", "14-debian-buster", "16-debian-bullseye", "16-debian-buster", "18-debian-bullseye", "20-debian-bullseye", "20-debian-bookworm", "dynamic-debian-buster" };
     }
 }
