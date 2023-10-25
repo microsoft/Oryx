16e57b300c878975bf0996817498d3842726a86a
harrli <harrli@microsoft.com>
2023-10-25 13:14:33 -0700

    Fixed run image base pipeline failure

--- build/buildRunTimeImageBases.sh
+++ build/buildRunTimeImageBases.sh
@@ -168,7 +168,7 @@ for dockerFile in $dockerFiles; do
         --build-arg NODE14_VERSION=$NODE14_VERSION \
         --build-arg NODE16_VERSION=$NODE16_VERSION \
         --build-arg NODE18_VERSION=$NODE18_VERSION \
-        --build-arg NODE20_VERSION=$NODE20_VERSION
+        --build-arg NODE20_VERSION=$NODE20_VERSION \
         --build-arg DEBIAN_FLAVOR=$runtimeImageDebianFlavor \
         --secret id=oryx_sdk_storage_account_access_token,env=ORYX_SDK_STORAGE_ACCOUNT_ACCESS_TOKEN \
         --secret id=dotnet_storage_account_token_id,env=DOTNET_PRIVATE_STORAGE_ACCOUNT_ACCESS_TOKEN \
