676ce3b58569c9a10df5c9c61255d951771a33d6
Oryx Team <oryx@microsoft.com>
2023-10-27 23:58:15 +0000

    Add new python SDK versions

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -11,10 +11,10 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Python27Version = "2.7.18";
         public const string Python36Version = "3.6.15";
         public const string Python37Version = "3.7.15";
-        public const string Python38Version = "3.8.16";
-        public const string Python39Version = "3.9.15";
-        public const string Python310Version = "3.10.8";
-        public const string Python311Version = "3.11.0";
+        public const string Python38Version = "3.8.18";
+        public const string Python39Version = "3.9.18";
+        public const string Python310Version = "3.10.13";
+        public const string Python311Version = "3.11.6";
         public static readonly List<string> RuntimeVersions = new List<string> { "3.7-debian-bullseye", "3.7-debian-buster", "3.8-debian-bullseye", "3.8-debian-buster", "3.9-debian-bullseye", "3.9-debian-buster", "3.10-debian-bullseye", "3.10-debian-buster", "3.11-debian-bullseye", "dynamic-debian-buster" };
     }
 }
