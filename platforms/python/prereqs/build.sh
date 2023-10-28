54451cb882a5ffba5d72f0016060f1bd217d79d9
Wali Bhuiyan <mbhuiyan@microsoft.com>
2023-10-27 23:07:07 -0400

    quick fix

--- platforms/python/prereqs/build.sh
+++ platforms/python/prereqs/build.sh
@@ -171,7 +171,7 @@ echo
 
 # Replace log level in pip's code as a workaround for https://github.com/pypa/pip/issues/6189
 pipReqSetPath=`find $INSTALLATION_PREFIX/lib -path "*site-packages/pip/_internal/req/req_set.py"`
-if [ -n $pipReqSetPath ]; then
+if [ -n "$pipReqSetPath" ]; then
     sed -i 's|logger\.debug('\''Cleaning up\.\.\.'\'')|logger\.info('\''Cleaning up\.\.\.'\'')|' "$pipReqSetPath"
 fi
 
