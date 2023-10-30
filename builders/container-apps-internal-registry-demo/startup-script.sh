5ed30383fc8f477085a1514bd65592993dc2a35e
Paul Dorsch <107068277+pauld-msft@users.noreply.github.com>
2023-10-30 12:09:33 -0400

    Container App Build Image (#2197)
    * added builder for demo of internal registry
    
    * Update capps builder with latest changes
    
    * changes to current capp builder
    
    * updates to script for blob storage
    
    * updated startup script to pull from blob storage
    
    * expand jar file to workspace (#2196)
    
    * expand jar file to workspace
    
    * update image
    
    * check file count for jar file
    
    * unzip jar and war
    
    ---------
    
    Co-authored-by: Yi Li <yili7@microsoft.com>
    
    * minor cleanup
    
    * Update startup-script.sh
    
    * Update stack images referenced, remove -cache-dir arguments
    
    * fix the jar file find regex (#2198)
    
    Co-authored-by: Yi Li <yili7@microsoft.com>
    
    * Update base builder tag to 20231018.1
    
    * cleanup demo app tar.gz
    
    * replace rather than append
    
    ---------
    
    Co-authored-by: Cormac McCarthy <corm@microsoft.com>
    Co-authored-by: Yi Li <109205537+yilims@users.noreply.github.com>
    Co-authored-by: Yi Li <yili7@microsoft.com>

---
+++ builders/container-apps-internal-registry-demo/startup-script.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -e
+
+# Wait for the app source to be uploaded to the blob storage.
+formatted_date=$(date -u +"%a, %d %b %Y %H:%M:%S GMT")
+auth_header="Authorization: Bearer $MI_ACCESS_TOKEN"
+version_header="x-ms-version: 2019-12-12"
+date_header="x-ms-date: $formatted_date"
+file_upload_endpoint="$FILE_UPLOAD_CONTAINER_URL/$FILE_UPLOAD_BLOB_NAME"
+
+temp_app_source_dir="/tmp/appsource"
+temp_app_source_path="$temp_app_source_dir/$FILE_UPLOAD_BLOB_NAME"
+mkdir $temp_app_source_dir
+
+while [[ ! -f "$temp_app_source_path" || ! "$(file $temp_app_source_path)" =~ "compressed data" ]]
+do
+  echo "Waiting for app source to be uploaded. Please upload the app source to the endpoint specified in the Build resource's 'uploadEndpoint' property."
+  curl -H "$auth_header" -H "$version_header" -H "$date_header" -X GET "$file_upload_endpoint" -o "$temp_app_source_path" -s
+  sleep 5
+done
+
+# Extract app code to CNB_APP_DIR directory.
+echo "Found app source at '$temp_app_source_path'. Extracting to $CNB_APP_DIR"
+mkdir -p $CNB_APP_DIR
+cd $CNB_APP_DIR
+tar -xzf "$temp_app_source_path"
+
+fileCount=$(ls | wc -l)
+if [ "$fileCount" = "1" ]; then
+  # Find .jar/war file in the directory
+  artifact=$(find "$CNB_APP_DIR" -maxdepth 1 -name "*.[jw]ar" | head -n 1)
+
+  # unzip it if found
+  if [[ -n $artifact ]];
+  then
+    echo "Unzip file $artifact"
+    unzip -qq $artifact -d $CNB_APP_DIR
+    rm $artifact
+  fi
+fi
+
+# public cert should be in this env var
+ca_pem_decoded=$(printf "%s" "$REGISTRY_HTTP_TLS_CERTIFICATE" | base64 -d)
+echo "$ca_pem_decoded" > /usr/local/share/ca-certificates/internalregistry.crt
+cd /usr/local/share/ca-certificates/
+awk 'BEGIN {c=0;} /BEGIN CERT/{c++} { print > "cert." c ".crt"}' < /usr/local/share/ca-certificates/internalregistry.crt
+update-ca-certificates
+
+cd $CNB_APP_DIR
+token=$(printf "%s" "$REGISTRY_AUTH_USERNAME:$REGISTRY_AUTH_PASSWORD" | base64)
+acr_access_string="Basic $token"
+export CNB_REGISTRY_AUTH='{"'$ACR_RESOURCE_NAME'":"'$acr_access_string'"}'
+
+# Execute the analyze phase
+echo
+echo "======================================="
+echo "===== Executing the analyze phase ====="
+echo "======================================="
+/lifecycle/analyzer \
+  -log-level debug \
+  -run-image mcr.microsoft.com/oryx/builder:stack-run-debian-bullseye-20230926.1 \
+  $APP_IMAGE
+
+# Execute the detect phase
+echo
+echo "======================================"
+echo "===== Executing the detect phase ====="
+echo "======================================"
+/lifecycle/detector \
+  -log-level debug \
+  -app $CNB_APP_DIR
+
+# Execute the restore phase
+echo
+echo "======================================="
+echo "===== Executing the restore phase ====="
+echo "======================================="
+/lifecycle/restorer \
+  -log-level debug \
+  -build-image mcr.microsoft.com/oryx/builder:stack-build-debian-bullseye-20230926.1
+
+# Execute the extend phase
+echo
+echo "======================================"
+echo "===== Executing the extend phase ====="
+echo "======================================"
+/lifecycle/extender \
+  -log-level debug \
+  -app $CNB_APP_DIR
+
+# Execute the export phase
+echo
+echo "======================================"
+echo "===== Executing the export phase ====="
+echo "======================================"
+/lifecycle/exporter \
+  -log-level debug \
+  -app $CNB_APP_DIR \
+  $APP_IMAGE
