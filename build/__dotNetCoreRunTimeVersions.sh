19f662c513fde265824f724f67e37e29a01173bb
Paul Dorsch <pauldorsch@microsoft.com>
2023-11-09 08:47:31 -0500

    updated base runtime tag

--- build/__dotNetCoreRunTimeVersions.sh
+++ build/__dotNetCoreRunTimeVersions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-DOT_NET_CORE_RUNTIME_BASE_TAG='20231108.3'
+DOT_NET_CORE_RUNTIME_BASE_TAG='20231109.5'
 NET_CORE_APP_10='1.0.16'
 NET_CORE_APP_11='1.1.13'
 NET_CORE_APP_20='2.0.9'
