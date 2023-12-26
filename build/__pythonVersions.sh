663dee41a24843871d121db69f9fecf6ae49c0e5
William Hernandez <williamhe@microsoft.com>
2023-12-26 00:41:38 +0000

    Add runtime build number

--- build/__pythonVersions.sh
+++ build/__pythonVersions.sh
@@ -1,6 +1,6 @@
 # This file was auto-generated from 'constants.yaml'. Changes may be overridden.
 
-PYTHON_RUNTIME_BASE_TAG='20231102.1'
+PYTHON_RUNTIME_BASE_TAG='20231226.1'
 PIP_VERSION='21.2.4'
 PYTHON27_VERSION='2.7.18'
 PYTHON36_VERSION='3.6.15'
