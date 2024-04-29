9dbca8d368e2d0e597bc161a27077826802ae279
Akshay Kumar <kumaraksh@microsoft.com>
2024-04-29 15:12:21 +0530

    update npm

--- images/runtime/node/installDependencies.sh
+++ images/runtime/node/installDependencies.sh
@@ -28,7 +28,7 @@ currentNpmVersion=$(npm --version)
 echo "Version of npm: $currentNpmVersion"
 
 # Upgrade npm to the latest available version
-if [[ $nodeVersionMajor -ge 10  ]]; then
+if [[ $nodeVersionMajor -ge 18  ]]; then
     echo "Upgrading npm..."
     npm install npm@$10.5.0 -g -loglevel silent
     echo "Done upgrading npm."
