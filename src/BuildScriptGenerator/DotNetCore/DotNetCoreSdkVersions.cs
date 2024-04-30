ff4cf0a90b3108f3c7aebc34ef1b50abd5d529b5
kumaraksh1 <kumaraksh1@users.noreply.github.com>
2024-04-30 13:39:56 +0000

    [create-pull-request] automated change

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreSdkVersions.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string DotNetCore31SdkVersion = "3.1.426";
         public const string DotNetCore50SdkVersion = "5.0.302";
         public const string DotNet50SdkVersion = "5.0.408";
-        public const string DotNet60SdkVersion = "6.0.417";
+        public const string DotNet60SdkVersion = "6.0.421";
         public const string DotNet60RtmSdkVersion = "6-0-100-rtm-21527-11";
         public const string DotNet70SdkVersion = "7.0.404";
         public const string DotNet80SdkVersion = "8.0.202";
