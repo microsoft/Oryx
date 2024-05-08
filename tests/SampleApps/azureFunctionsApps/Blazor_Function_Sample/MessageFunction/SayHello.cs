4df6edbd6069341a5313723be3249fd211932209
Sarath chandra Bussa <sbussa@microsoft.com>
2024-05-08 13:51:14 +0530

    add the sample app as code instead of submodule

---
+++ tests/SampleApps/azureFunctionsApps/Blazor_Function_Sample/MessageFunction/SayHello.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace MessageFunction
+{
+    public static class SayHello
+    {
+        [FunctionName("SayHello")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Enter your name and click the button for more personal."
+                : $"👋 Hello, {name}.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
