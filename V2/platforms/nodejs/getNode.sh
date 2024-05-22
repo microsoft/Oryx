2b9ffc2950165ecd71f5af45494cc7ffaa71b25e
Sarath chandra Bussa <sbussa@microsoft.com>
2024-05-22 20:18:08 +0530

    nodejs sdks

--- V2/platforms/nodejs/getNode.sh
+++ V2/platforms/nodejs/getNode.sh
@@ -6,17 +6,15 @@
 
 set -ex
 
-declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && cd .. && pwd )
+declare -r REPO_DIR="/tmp"
 
 source $REPO_DIR/platforms/__common.sh
 
 nodePlatformDir="$REPO_DIR/platforms/nodejs"
-hostNodeArtifactsDir="$volumeHostDir/nodejs"
+outputDir="/tmp/sdk"
 debianFlavor="$1"
 sdkStorageAccountUrl="$2"
-mkdir -p "$hostNodeArtifactsDir"
 
-builtNodeImage=false
 getNode() {
 	local version="$1"
 	
@@ -27,14 +25,14 @@ getNode() {
     if [ "$debianFlavor" == "stretch" ]; then
 			# Use default sdk file name
 			tarFileName=nodejs-$version.tar.gz
-			metadataFile="$hostNodeArtifactsDir/nodejs-$version-metadata.txt"
+			metadataFile="$outputDir/nodejs-$version-metadata.txt"
 			# Continue adding the version metadata with the name of Version
 			# which is what our legacy CLI will use
 			sdkVersionMetadataName="$LEGACY_SDK_VERSION_METADATA_NAME"
-			cp "$nodePlatformDir/versions/$debianFlavor/defaultVersion.txt" "$hostNodeArtifactsDir/defaultVersion.txt"
+			cp "$nodePlatformDir/versions/$debianFlavor/defaultVersion.txt" "$outputDir/defaultVersion.txt"
 	else
 			tarFileName=nodejs-$debianFlavor-$version.tar.gz
-			metadataFile="$hostNodeArtifactsDir/nodejs-$debianFlavor-$version-metadata.txt"
+			metadataFile="$outputDir/nodejs-$debianFlavor-$version-metadata.txt"
 			sdkVersionMetadataName="$SDK_VERSION_METADATA_NAME"
 	fi
 
@@ -42,19 +40,7 @@ getNode() {
 		echo "Getting Node version '$version'..."
 		echo
 
-		if ! $builtNodeImage; then
-			docker build \
-				--build-arg DEBIAN_FLAVOR=$debianFlavor \
-				-f "$nodePlatformDir/Dockerfile" \
-				-t $imageName \
-				$REPO_DIR
-			builtNodeImage=true
-		fi
-
-		docker run \
-			-v /$hostNodeArtifactsDir:$volumeContainerDir \
-			$imageName \
-			bash -c "/tmp/scripts/build.sh $version && cp -f /tmp/compressedSdk/* /tmp/sdk"
+		/tmp/scripts/build.sh $version && cp -f /tmp/compressedSdk/* $outputDir
 		
 		echo "$sdkVersionMetadataName=$version" >> $metadataFile
 		echo "$OS_TYPE_METADATA_NAME=$debianFlavor" >> $metadataFile
@@ -66,4 +52,4 @@ echo
 buildPlatform "$nodePlatformDir/versions/$debianFlavor/versionsToBuild.txt" getNode
 
 # Write the default version
-cp "$nodePlatformDir/versions/$debianFlavor/defaultVersion.txt" "$hostNodeArtifactsDir/defaultVersion.$debianFlavor.txt"
+cp "$nodePlatformDir/versions/$debianFlavor/defaultVersion.txt" "$outputDir/defaultVersion.$debianFlavor.txt"
