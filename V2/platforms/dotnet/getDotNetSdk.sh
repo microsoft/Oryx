12cfc098a1e18233240e7f9431f1fe44fde81a62
Sarath chandra Bussa <sbussa@microsoft.com>
2024-05-22 17:11:36 +0530

    sdks

--- V2/platforms/dotnet/getDotNetSdk.sh
+++ V2/platforms/dotnet/getDotNetSdk.sh
@@ -8,9 +8,9 @@ set -e
 
 declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && cd .. && pwd )
 
-source $REPO_DIR/platformsV2/__common.sh
+source $REPO_DIR/platforms/__common.sh
 
-dotNetPlatformDir="$REPO_DIR/platformsV2/dotnet"
+dotNetPlatformDir="$REPO_DIR/platforms/dotnet"
 targetDir="$volumeHostDir/dotnet"
 debianFlavor="$1"
 sdkStorageAccountUrl="$2"
