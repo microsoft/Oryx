28c77c8f36379584f33b24d95323582dc5534f28
Sarath chandra Bussa <sbussa@microsoft.com>
2024-05-23 18:42:02 +0530

    if condition

--- V2/platforms/php/buildPhp.sh
+++ V2/platforms/php/buildPhp.sh
@@ -135,7 +135,7 @@ buildPhpComposer() {
 	fi
 }
 
-if $phpType == "php"; then
+if [ "$phpType" == "php" ]; then
 	echo "Building Php..."
 	echo
 	buildPlatform "$phpPlatformDir/versions/$debianFlavor/versionsToBuild.txt" buildPhp
