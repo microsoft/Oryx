68cb2623e4942d71ff9043d8b0931b600b5ce87e
Akshay Kumar <kumaraksh@microsoft.com>
2024-06-14 10:27:21 +0530

    Add new python versions

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -11,9 +11,9 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Python27Version = "2.7.18";
         public const string Python36Version = "3.6.15";
         public const string Python37Version = "3.7.15";
-        public const string Python38Version = "3.8.18";
-        public const string Python39Version = "3.9.18";
-        public const string Python310Version = "3.10.13";
+        public const string Python38Version = "3.8.19";
+        public const string Python39Version = "3.9.19";
+        public const string Python310Version = "3.10.14";
         public const string Python311Version = "3.11.8";
         public const string Python312Version = "3.12.2";
         public static readonly List<string> RuntimeVersions = new List<string> { "3.7-debian-bullseye", "3.7-debian-buster", "3.8-debian-bookworm", "3.8-debian-bullseye", "3.8-debian-buster", "3.9-debian-bookworm", "3.9-debian-bullseye", "3.9-debian-buster", "3.10-debian-bookworm", "3.10-debian-bullseye", "3.10-debian-buster", "3.11-debian-bookworm", "3.11-debian-bullseye", "3.12-debian-bookworm", "3.12-debian-bullseye", "dynamic-debian-buster" };
