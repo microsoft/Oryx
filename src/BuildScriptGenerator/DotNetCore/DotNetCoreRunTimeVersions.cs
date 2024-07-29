7c0ea333bb3b09be8351faf0622a09a4d15ce464
Saipriya-1144 <saipriyach1144@gmail.com>
2024-07-29 08:07:48 +0530

    Resolved Merge conflicts

--- src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
+++ src/BuildScriptGenerator/DotNetCore/DotNetCoreRunTimeVersions.cs
@@ -34,17 +34,10 @@ namespace Microsoft.Oryx.BuildScriptGenerator.DotNetCore
         public const string NetCoreApp70Sha = "87855297338555a7b577d7e314e5dbf2c2350f8c867a489cd1e535634bad5c123a1871464d37fc9421837ff5d426c2eadecbe0f60bbf3fd32bc2461f47790a40";
         public const string AspnetCoreApp70 = "7.0.20";
         public const string AspnetCoreApp70Sha = "62ed9743972043a72e48d5aa2f7fdf3483cf684a32b051315004d1c778e9712bf66e5e7a97a5a53993fa8e92daf5bacaf2cdb3eae44bb9a9e25532b9a80f4f70";
-<<<<<<< HEAD
-        public const string NetCoreApp80 = "8.0.6";
-        public const string NetCoreApp80Sha = "c0c5e93d4e68e2075c4c63336dc74246efb704ac9663411351efdefc4cc7da5a7750f44b8a23aebe959bb4308575bead443a41b2524ae03b29ac41929d27e0e0";
-        public const string AspnetCoreApp80 = "8.0.6";
-        public const string AspnetCoreApp80Sha = "16cd54c431d80710a06037f8ea593e04764a80cbaad75e1db4225fbe3e7fce4c4d279f40757b9811e1c092436d2a1ca3be64c74cb190ebf78418a9865992ad12";
-=======
         public const string NetCoreApp80 = "8.0.7";
         public const string NetCoreApp80Sha = "88e9ac34ad5ac76eec5499f2eb8d1aa35076518c842854ec1053953d34969c7bf1c5b2dbce245dbace3a18c3b8a4c79d2ef2d2ff105ce9d17cbbdbe813d8b16f";
         public const string AspnetCoreApp80 = "8.0.7";
         public const string AspnetCoreApp80Sha = "c7479dc008fce77c2bfcaa1ac1c9fe6f64ef7e59609fff6707da14975aade73e3cb22b97f2b3922a2642fa8d843a3caf714ab3a2b357abeda486b9d0f8bebb18";
->>>>>>> 161b82f203abe7493a353068dfca40f3a8c211b7
         public const string NetCoreApp90 = "9.0.0-preview.6";
         public const string NetCoreApp90Sha = "09aa8c4e6ae3ada1a265a5cd2b46779a763163e4dd9a1892b44606b89cf147339e10b7c584dbcaf5404af0553f0ef6c5801436c217f4fe1a5d3bdb6d74aef1d1";
         public const string AspnetCoreApp90 = "9.0.0-preview.6";
