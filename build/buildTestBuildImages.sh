a83571532779a9a8eeb1793738b3356f1ae46a9a
Saipriya-1144 <saipriyach1144@gmail.com>
2024-08-08 12:03:53 +0530

    recovered build/buildPlatformBinaries.sh build/buildTestBuildImages.sh files

---
+++ build/buildTestBuildImages.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+# --------------------------------------------------------------------------------------------
+
+set -e
+
+declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
+
+# Load all variables
+source $REPO_DIR/build/__variables.sh
+source $REPO_DIR/build/__functions.sh
+
+echo
+echo "Building build images for tests..."
+
+
+echo "Building buster based GitHub Action image for tests..."
+docker build \
+    -t "$ORYXTESTS_BUILDIMAGE_REPO:github-actions-debian-buster" \
+    --build-arg PARENT_IMAGE_BASE=$ORYX_TEST_IMAGE_BASE:github-actions-debian-buster-$IMAGE_BUILDNUMBER \
+    -f "$ORYXTESTS_GITHUB_ACTIONS_BUILDIMAGE_DOCKERFILE" \
+    .
+
+echo
+echo
+
+echo "Building bullseye based GitHub Action image for tests..."
+docker build \
+    -t "$ORYXTESTS_BUILDIMAGE_REPO:github-actions-debian-bullseye" \
+    --build-arg PARENT_IMAGE_BASE=$ORYX_TEST_IMAGE_BASE:github-actions-debian-bullseye-$IMAGE_BUILDNUMBER \
+    -f "$ORYXTESTS_GITHUB_ACTIONS_BUILDIMAGE_DOCKERFILE" \
+    .
+
+echo
+echo
+
+echo "Building bookworm based GitHub Action image for tests..."
+docker build \
+    -t "$ORYXTESTS_BUILDIMAGE_REPO:github-actions-debian-bookworm" \
+    --build-arg PARENT_IMAGE_BASE=$ORYX_TEST_IMAGE_BASE:github-actions-debian-bookworm-$IMAGE_BUILDNUMBER \
+    -f "$ORYXTESTS_GITHUB_ACTIONS_BUILDIMAGE_DOCKERFILE" \
+    .
+
+echo
+echo
+
+dockerCleanupIfRequested
