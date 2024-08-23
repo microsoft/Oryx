93e4104dbbb01177ef742f22dff75aba8fb7459a
Saipriya-1144 <saipriyach1144@gmail.com>
2024-08-23 09:43:39 +0530

    updated update_constants.sh file

--- monitor_version_scripts/update_constants.sh
+++ monitor_version_scripts/update_constants.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Read the YAML file line by line
+
+constants_FILE=$(cd .. && pwd)/images/constants.yml
+
+echo $constants_FILE
+
 while IFS= read -r line; do
     # Use yq to parse the YAML line and extract the key and value
     key=$(echo "$line" | yq e 'keys' - | sed 's/^[[:space:]]*-*//' | sed 's/^[[:space:]]*//')
@@ -11,12 +16,12 @@ while IFS= read -r line; do
 
     echo "$key"
 
-    if yq eval ".variables | has(\"$key\")" constants.yaml | grep -q 'true'; then
-        yq eval ".variables.$key = \"$value\"" -i constants.yaml
-        echo "Updated $key in constants.yaml"
+    if yq eval ".variables | has(\"$key\")" $constants_FILE | grep -q 'true'; then
+        yq eval ".variables.$key = \"$value\"" -i $constants_FILE
+        echo "Updated $key in constants_FILE"
     else
-        yq eval ".variables.$key = \"$value\"" -i constants.yaml
-        echo "Added $key to constants.yaml"
+        yq eval ".variables.$key = \"$value\"" -i $constants_FILE
+        echo "Added $key to constants_FILE"
     fi
   
 done < <(yq e '.[]' "override_constants.yaml")
@@ -28,13 +33,13 @@ while IFS= read -r line; do
 
     echo "Key: $key, Value: $value,"
 
-    if yq eval ".variables | has(\"$key\")" constants.yaml | grep -q 'true'; then
+    if yq eval ".variables | has(\"$key\")" $constants_FILE | grep -q 'true'; then
         if yq eval ".variables | has(\"$key\") | not" override_constants.yaml | grep -q 'true'; then
-            yq eval ".variables.$key = \"$value\"" -i constants.yaml
+            yq eval ".variables.$key = \"$value\"" -i $constants_FILE
         fi
     else
-        yq eval ".variables.$key = \"$value\"" -i constants.yaml
-        echo "Added $key to constants.yaml"
+        yq eval ".variables.$key = \"$value\"" -i $constants_FILE
+        echo "Added $key to constants_FILE"
     fi
   
 done < <(yq e '.[]' "latest_stack_versions.yaml")
