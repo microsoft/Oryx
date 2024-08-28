9c017937f36cfeca07636f7d3f54297c601d75ab
Saipriya-1144 <saipriyach1144@gmail.com>
2024-08-28 15:21:53 +0530

    updated versions to build files

--- monitor_version_scripts/update_versions_to_build.sh
+++ monitor_version_scripts/update_versions_to_build.sh
@@ -1,33 +1,30 @@
-add_line_to_versions_to_build() {
-    update_file=$1
-    line_to_be_added=$2
+# add_line_to_versions_to_build() {
+#     update_file=$1
+#     line_to_be_added=$2
+    
+#     awk -v line="$line_to_be_added" '
+#     BEGIN { added = 0 }
+#     /^[[:space:]]*$/ { print; next }  # Skip empty lines
+#     /^#/ { print; next }              # Skip comment lines
+#     {
+#         split(line, new_fields, ", ")
+#         split($0, fields, ", ")
 
-    awk -v line="$line_to_be_added" '
-    BEGIN { added = 0 }
-    /^[[:space:]]*$/ { print; next }  # Skip empty lines
-    /^#/ { print; next }              # Skip comment lines
-    {
-        split(line, new_fields, ", ")
-        split($0, fields, ", ")
+#         # Use sort -V to compare versions
+#         cmd = "echo \"" fields[1] "\n" new_fields[1] "\" | sort -V | head -n 1"
+#         cmd | getline result
+#         close(cmd)
 
-        # Use sort -V to compare versions
-        cmd = "echo \"" fields[1] "\n" new_fields[1] "\" | sort -V | head -n 1"
-        cmd | getline result
-        close(cmd)
-
-        print "Comparing: " fields[1] " with " new_fields[1] " -> " result  # Debug print
-
-        if (!added && result == new_fields[1]) {
-                print "Adding line: " line  # Debug print
-                print line
-                added = 1
-        }
-        print
-    }
-    END {
-        if (!added) print line
-    }' "$update_file" > temp && mv temp "$update_file"
-}
+#         if (!added && result == new_fields[1]) {
+#                 print line
+#                 added = 1
+#         }
+#         print
+#     }
+#     END {
+#         if (!added) print line
+#     }' "$update_file" > temp && mv temp "$update_file"
+# }
 
 update_stack_versions_to_build(){
     stack_versionsToBuild_FILE="$1"
@@ -49,26 +46,17 @@ update_stack_versions_to_build(){
 
     if ! $version_found; then
         if [[ "$key" = *"node"* ]]; then
-            line_to_be_added="$value"
-            # echo -e "\n$value" >> "$stack_versionsToBuild_FILE"
-            echo "adding this line (version) to build file"
-            add_line_to_versions_to_build $stack_versionsToBuild_FILE $line_to_be_added
+            echo -e "\n$value" >> "$stack_versionsToBuild_FILE"
         elif [[ "$key" = *"python"* ]]; then
             gpgkeyname="python${version}_GPG_keys"
             gpgkeysvalue=$(yq eval ".variables.$gpgkeyname" override_constants.yaml)
-            line_to_be_added="$value, $gpgkeysvalue"
-            # echo -e "\n$value, $gpgkeysvalue" >> "$stack_versionsToBuild_FILE"
-            echo "adding this line (version, GPG) to build file"
-            add_line_to_versions_to_build $stack_versionsToBuild_FILE $line_to_be_added
+            echo -e "\n$value, $gpgkeysvalue" >> "$stack_versionsToBuild_FILE"
         elif [[ "$key" = *"php"* ]]; then
             gpgkeyname="php${version}_GPG_keys"
             gpgkeysvalue=$(yq eval ".variables.$gpgkeyname" override_constants.yaml)
             phpSHAName="php${version}Version_SHA"
             phpSHAValue=$(yq eval ".variables.$phpSHAName" latest_stack_versions.yaml)
-            line_to_be_added="$value, $phpSHAValue, $gpgkeysvalue"
-            echo "adding this line (version, SHA, GPG) to build file"
-            # echo -e "\n$value, $phpSHAValue, $gpgkeysvalue" >> "$stack_versionsToBuild_FILE"
-            add_line_to_versions_to_build $stack_versionsToBuild_FILE $line_to_be_added
+            echo -e "\n$value, $phpSHAValue, $gpgkeysvalue" >> "$stack_versionsToBuild_FILE"
         fi
     fi
 }
