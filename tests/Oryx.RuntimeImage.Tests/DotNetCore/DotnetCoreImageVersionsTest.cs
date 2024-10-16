d7155693c87bb9c2578323ffdea6a44fbaebd7d6
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-16 18:26:05 +0530

    removed dotnet runtime tests for buster image

--- tests/Oryx.RuntimeImage.Tests/DotNetCore/DotnetCoreImageVersionsTest.cs
+++ tests/Oryx.RuntimeImage.Tests/DotNetCore/DotnetCoreImageVersionsTest.cs
@@ -58,44 +58,6 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
         }
 
         [Theory]
-        [Trait("category", "runtime-buster")]
-        [InlineData("6.0")]
-        public void DotNetCoreBusterRuntimeImage_Contains_VersionAndCommit_Information(string version)
-        {
-            // we cant always rely on gitcommitid as env variable in case build context is not correctly passed
-            // so we should check agent_os environment variable to know if the build is happening in azure devops agent
-            // or locally, locally we need to skip this test
-            var agentOS = Environment.GetEnvironmentVariable("AGENT_OS");
-            Skip.If(string.IsNullOrEmpty(agentOS));
-
-            // Arrange
-            var buildNumber = Environment.GetEnvironmentVariable("IMAGE_BUILDNUMBER");
-            var expectedOryxVersion = string.Concat(Settings.OryxVersion, buildNumber);
-            var gitCommitID = GitHelper.GetCommitID();
-
-            // Act
-            var result = _dockerCli.Run(new DockerRunArguments
-            {
-                ImageId = _imageHelper.GetRuntimeImage("dotnetcore", version, ImageTestHelperConstants.OsTypeDebianBuster),
-                CommandToExecuteOnRun = "oryx",
-                CommandArguments = new[] { "version" }
-            });
-
-            // Assert
-            RunAsserts(
-                () =>
-                {
-                    Assert.True(result.IsSuccess);
-                    Assert.NotNull(result.StdErr);
-                    Assert.DoesNotContain(".unspecified, Commit: unspecified", result.StdOut);
-                    Assert.Contains(gitCommitID, result.StdOut);
-                    Assert.Contains(expectedOryxVersion, result.StdOut);
-
-                },
-                result.GetDebugInfo());
-        }
-
-        [Theory]
         [Trait("category", "runtime-bookworm")]
         [InlineData("8.0")]
         [InlineData("9.0")]
@@ -130,31 +92,6 @@ namespace Microsoft.Oryx.RuntimeImage.Tests
                     Assert.Contains(gitCommitID, result.StdOut);
                     Assert.Contains(expectedOryxVersion, result.StdOut);
 
-                },
-                result.GetDebugInfo());
-        }
-
-        [Theory]
-        [Trait("category", "runtime-buster")]
-        [InlineData("6.0", "Version: " + DotNetCoreRunTimeVersions.NetCoreApp60)]
-        [Trait(TestConstants.Category, TestConstants.Release)]
-        public void RuntimeImage_Buster_HasExecptedDotNetVersion(string version, string expectedOutput)
-        {
-            // Arrange & Act
-            var result = _dockerCli.Run(new DockerRunArguments
-            {
-                ImageId = _imageHelper.GetRuntimeImage("dotnetcore", version, ImageTestHelperConstants.OsTypeDebianBuster),
-                CommandToExecuteOnRun = "dotnet",
-                CommandArguments = new[] { "--info" }
-            });
-
-            // Assert
-            var actualOutput = string.Join("", result.StdOut.ReplaceNewLine().Where(c => !char.IsWhiteSpace(c)));
-            RunAsserts(
-                () =>
-                {
-                    Assert.True(result.IsSuccess);
-                    Assert.Contains(string.Join("", expectedOutput.Where(c => !char.IsWhiteSpace(c))), actualOutput);
                 },
                 result.GetDebugInfo());
         }
