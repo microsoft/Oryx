a3bf07ef420c6cb5d163d4e8d6e58092ec791ef2
Saipriya-1144 <saipriyach1144@gmail.com>
2025-02-27 14:34:52 +0530

    update node build image tests

--- tests/Oryx.BuildImage.Tests/Node/NodeAppOutputDirTest.cs
+++ tests/Oryx.BuildImage.Tests/Node/NodeAppOutputDirTest.cs
@@ -22,7 +22,8 @@ namespace Oryx.BuildImage.Tests.Node
         {
         }
 
-        [Theory, Trait("category", "jamstack")]
+        [Theory, Trait("category", "githubactions")]
+        [Trait("build-image", "github-actions-debian-bullseye")]
         // Temporarily blocking Angular 14 app: Work item 1565890
         // [InlineData("angular14", "dist")]
         // Temporarily blocking next app as next build is failing accross npm
@@ -30,13 +31,30 @@ namespace Oryx.BuildImage.Tests.Node
         // [InlineData("hackernews-nuxtjs", ".nuxt")]
         // Temporarily blocking gastbysample app after node default version bumped to 16: #1715134
         // [InlineData("gatsbysample", "public")]
-        [InlineData("vue-sample", "dist")]
-        [InlineData("create-react-app-sample", "build")]
-        [InlineData("hexo-sample", "public")]
-        public void BuildsApp_AndAddsOutputDirToManifestFile(string appName, string expectedOutputDirPath)
+        [InlineData("angular14", "dist", NodeVersions.Node18Version)]
+        [InlineData("angular14", "dist", NodeVersions.Node20Version)]
+        [InlineData("angular14", "dist", NodeVersions.Node22Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node18Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node20Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node22Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node18Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node20Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node22Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node18Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node20Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node22Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node18Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node20Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node22Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node18Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node20Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node22Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node18Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node20Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node22Version)]
+        public void BuildsApp_AndAddsOutputDirToManifestFile_WithBullseyeBasedImages(string appName, string expectedOutputDirPath, string version)
         {
             // Arrange
-            var version = "20.11.0";
             var volume = DockerVolume.CreateMirror(
                 Path.Combine(_hostSamplesDir, "nodejs", appName));
             var appDir = volume.ContainerDir;
@@ -53,7 +71,7 @@ namespace Oryx.BuildImage.Tests.Node
             // Act
             var result = _dockerCli.Run(new DockerRunArguments
             {
-                ImageId = _imageHelper.GetAzureFunctionsJamStackBuildImage(ImageTestHelperConstants.AzureFunctionsJamStackBullseye),
+                ImageId = _imageHelper.GetAzureFunctionsJamStackBuildImage(ImageTestHelperConstants.GitHubActionsBullseye),
                 Volumes = new List<DockerVolume> { volume },
                 CommandToExecuteOnRun = "/bin/bash",
                 CommandArguments = new[] { "-c", script }
@@ -67,5 +85,71 @@ namespace Oryx.BuildImage.Tests.Node
                 },
                 result.GetDebugInfo());
         }
+
+
+        [Theory, Trait("category", "githubactions")]
+        [Trait("build-image", "github-actions-debian-bookworm")]
+        // Temporarily blocking Angular 14 app: Work item 1565890
+        // [InlineData("angular14", "dist")]
+        // Temporarily blocking next app as next build is failing accross npm
+        // [InlineData("blog-starter-nextjs", ".next")]
+        // [InlineData("hackernews-nuxtjs", ".nuxt")]
+        // Temporarily blocking gastbysample app after node default version bumped to 16: #1715134
+        // [InlineData("gatsbysample", "public")]
+        [InlineData("angular14", "dist", NodeVersions.Node18Version)]
+        [InlineData("angular14", "dist", NodeVersions.Node20Version)]
+        [InlineData("angular14", "dist", NodeVersions.Node22Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node18Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node20Version)]
+        [InlineData("gatsby-sample", "public", NodeVersions.Node22Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node18Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node20Version)]
+        [InlineData("blog-starter-nextjs", ".next", NodeVersions.Node22Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node18Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node20Version)]
+        [InlineData("hackernews-nuxtjs", ".nuxt", NodeVersions.Node22Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node18Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node20Version)]
+        [InlineData("vue-sample", "dist", NodeVersions.Node22Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node18Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node20Version)]
+        [InlineData("create-react-app-sample", "build", NodeVersions.Node22Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node18Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node20Version)]
+        [InlineData("hexo-sample", "public", NodeVersions.Node22Version)]
+        public void BuildsApp_AndAddsOutputDirToManifestFile_WithBookwormBasedImages(string appName, string expectedOutputDirPath, string version)
+        {
+            // Arrange
+            var volume = DockerVolume.CreateMirror(
+                Path.Combine(_hostSamplesDir, "nodejs", appName));
+            var appDir = volume.ContainerDir;
+            var appOutputDir = "/tmp/output";
+            var script = new ShellScriptBuilder()
+                .AddCommand("node -v")
+                .AddBuildCommand($"{appDir} -i /tmp/int -o {appOutputDir} --platform {NodeConstants.PlatformName} --platform-version {version}")
+                .AddFileExistsCheck($"{appOutputDir}/{FilePaths.OsTypeFileName}")
+                .AddStringExistsInFileCheck(
+                $"{NodeManifestFilePropertyKeys.OutputDirPath}=\"{expectedOutputDirPath}\"",
+                $"{appOutputDir}/{FilePaths.BuildManifestFileName}")
+                .ToString();
+
+            // Act
+            var result = _dockerCli.Run(new DockerRunArguments
+            {
+                ImageId = _imageHelper.GetAzureFunctionsJamStackBuildImage(ImageTestHelperConstants.GitHubActionsBookworm),
+                Volumes = new List<DockerVolume> { volume },
+                CommandToExecuteOnRun = "/bin/bash",
+                CommandArguments = new[] { "-c", script }
+            });
+
+            // Assert
+            RunAsserts(
+                () =>
+                {
+                    Assert.True(result.IsSuccess);
+                },
+                result.GetDebugInfo());
+        }
+
     }
 }
