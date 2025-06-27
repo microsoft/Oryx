ac4243149ecda8c6802e61a8a9318b49d6c19d03
Saipriya-1144 <saipriyach1144@gmail.com>
2025-06-27 12:55:44 +0530

    Use storage account url instead of name

--- vsts/scripts/publishFilesToAzureStorage.sh
+++ vsts/scripts/publishFilesToAzureStorage.sh
@@ -9,7 +9,7 @@ set -e
 declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && cd .. && pwd )
 source $REPO_DIR/platforms/__common.sh
 commit=$GIT_COMMIT
-storageAccountName="$1"
+storageAccountUrl="$1"
 
 uploadFiles() {
     local platform="$1"
@@ -48,7 +48,7 @@ uploadFiles() {
             --name $fileName \
             --file "$fileToUpload" \
             --container-name $platform \
-            --account-name $storageAccountName \
+            --account-url $storageAccountUrl \
             --metadata \
                 Buildnumber="$BUILD_BUILDNUMBER" \
                 Commit="$commit" \
@@ -62,7 +62,7 @@ uploadFiles() {
             --name $fileName \
             --file "$fileToUpload" \
             --container-name $platform \
-            --account-name $storageAccountName \
+            --account-url $storageAccountUrl \
             --metadata \
                 Buildnumber="$BUILD_BUILDNUMBER" \
                 Commit="$commit" \
@@ -72,8 +72,6 @@ uploadFiles() {
         fi
     done
 }
-
-storageAccountUrl="https://$storageAccountName.blob.core.windows.net"
 
 platforms=("nodejs" "python" "dotnet" "php" "php-composer" "ruby" "java" "maven" "golang")
 for platform in "${platforms[@]}"
