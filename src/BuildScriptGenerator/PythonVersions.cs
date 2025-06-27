95e51f069564e1992402fcbc0f75f0b1dc2dec65
Saipriya-1144 <saipriyach1144@gmail.com>
2025-06-27 11:04:46 +0530

    Updated Python Versions

--- src/BuildScriptGenerator/PythonVersions.cs
+++ src/BuildScriptGenerator/PythonVersions.cs
@@ -12,11 +12,11 @@ namespace Microsoft.Oryx.BuildScriptGenerator.Common
         public const string Python36Version = "3.6.15";
         public const string Python37Version = "3.7.15";
         public const string Python38Version = "3.8.20";
-        public const string Python39Version = "3.9.22";
-        public const string Python310Version = "3.10.17";
-        public const string Python311Version = "3.11.12";
-        public const string Python312Version = "3.12.10";
-        public const string Python313Version = "3.13.3";
+        public const string Python39Version = "3.9.23";
+        public const string Python310Version = "3.10.18";
+        public const string Python311Version = "3.11.13";
+        public const string Python312Version = "3.12.11";
+        public const string Python313Version = "3.13.5";
         public static readonly List<string> RuntimeVersions = new List<string> { "3.7-debian-bullseye", "3.7-debian-buster", "3.8-debian-bookworm", "3.8-debian-bullseye", "3.8-debian-buster", "3.9-debian-bookworm", "3.9-debian-bullseye", "3.9-debian-buster", "3.10-debian-bookworm", "3.10-debian-bullseye", "3.10-debian-buster", "3.11-debian-bookworm", "3.11-debian-bullseye", "3.12-debian-bookworm", "3.12-debian-bullseye", "3.13-debian-bullseye", "3.13-debian-bookworm", "dynamic-debian-buster" };
     }
 }
