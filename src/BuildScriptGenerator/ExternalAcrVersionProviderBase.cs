ed80ef092d339f4cea9f4b7dfe35e9cdea2d2f5f
sartsharma <sartsharma@microsoft.com>
2026-05-13 16:41:18 +0530

    update timeout

--- src/BuildScriptGenerator/ExternalAcrVersionProviderBase.cs
+++ src/BuildScriptGenerator/ExternalAcrVersionProviderBase.cs
@@ -26,7 +26,7 @@ namespace Microsoft.Oryx.BuildScriptGenerator
     public class ExternalAcrVersionProviderBase
     {
         private const string SocketPath = "/var/sdk-image-sockets/oryx-pull-sdk-image.socket";
-        private const int MaxTimeoutForSocketOperationInSeconds = 300;
+        private const int MaxTimeoutForSocketOperationInSeconds = 240;
 
         private readonly BuildScriptGeneratorOptions commonOptions;
         private readonly ILogger logger;
