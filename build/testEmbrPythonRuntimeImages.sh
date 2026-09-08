1895e1e0c17dd383a39cfe3d0dcf85a62ae7aaf9
Shilpi Rachna <shilpir@microsoft.com>
2026-09-08 15:45:32 +0530

    Updated Python images to include oryx cli and gunicorn

--- build/testEmbrPythonRuntimeImages.sh
+++ build/testEmbrPythonRuntimeImages.sh
@@ -23,14 +23,32 @@ while IFS= read -r image; do
     docker run --rm --env "EXPECTED_VERSION=$expected_version" "$image" sh -ceu '
         actual_version="$(python -c "import sys; print(f\"{sys.version_info.major}.{sys.version_info.minor}\")")"
         test "$actual_version" = "$EXPECTED_VERSION"
-        python -c "import bz2, ctypes, curses, dbm.gnu, lzma, readline, sqlite3, ssl, tkinter, uuid"
+        python -c "import bz2, ctypes, curses, dbm.gnu, lzma, readline, sqlite3, ssl, uuid"
         python -c "import urllib.request; urllib.request.urlopen(\"https://www.python.org/\", timeout=30).close()"
+        python -c "import ctypes; ctypes.CDLL(\"libpq.so.5\"); ctypes.CDLL(\"libmysqlclient.so.24\")"
         test -n "$(find /usr/local/share/ca-certificates -name "azl_*.crt" -print -quit)"
         test ! -x /usr/bin/gcc
         test ! -x /usr/bin/g++
         python -c "import importlib.util; assert importlib.util.find_spec(\"pip\") is None"
-        python -c "import importlib.util; assert importlib.util.find_spec(\"gunicorn\") is None"
+        python -c "import importlib.util; assert importlib.util.find_spec(\"_tkinter\") is None"
+        gunicorn --version | grep -q "^gunicorn (version "
         python -c "import importlib.util; assert importlib.util.find_spec(\"uvicorn\") is None"
+        test -x /opt/oryx/benv
+        test -x /opt/startupcmdgen/startupcmdgen
+        test "$(readlink /usr/local/bin/oryx)" = "/opt/startupcmdgen/startupcmdgen"
+        mkdir -p /tmp/oryx-startup-test
+        oryx create-script \
+            -appPath /tmp/oryx-startup-test \
+            -output /tmp/oryx-startup.sh \
+            -bindPort 8000 \
+            -userStartupCommand "gunicorn wsgiref.simple_server:demo_app --bind 0.0.0.0:8000"
+        test -s /tmp/oryx-startup.sh
+        grep -q "gunicorn wsgiref.simple_server:demo_app" /tmp/oryx-startup.sh
+        EXPECTED_VERSION="$EXPECTED_VERSION" bash -c '"'"'
+            source /opt/oryx/benv "python=$EXPECTED_VERSION"
+            test "$python" = "/opt/python/$EXPECTED_VERSION/bin/python"
+            test "$(python -c "import sys; print(f\"{sys.version_info.major}.{sys.version_info.minor}\")")" = "$EXPECTED_VERSION"
+        '"'"'
     '
 done < "$1"
 
